e1ea36fb00cbc3b33ebac221b40a2b7fd5e35e40
Ola Blissing <ola.blissing@gmail.com>
2016-04-06 14:28:25 +0200

    removed console log

--- viewer/static/js/main.js
+++ viewer/static/js/main.js
@@ -34,9 +34,8 @@ $(function () {
 
   $(document).ready(function () {
     
-    let searchForm = document.getElementById('searchForm');
+    var searchForm = document.getElementById('searchForm');
     if(searchForm && typeof searchForm !== 'undefined') {
-      console.log("found");
       document.getElementById('searchForm').onsubmit = function(e) {
         if (document.getElementById('searchQ').value == '') {
           e.preventDefault();
