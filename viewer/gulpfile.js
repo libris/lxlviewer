51c962a4240a18785901804c82211bd66ef2fdfc
Ola Blissing <ola.blissing@gmail.com>
2016-06-03 16:48:05 +0200

    Gulp + caching (for es6 files only right now)

---
+++ viewer/gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+
+function compile(watch) {
+  var extensions = ['.es6', '.js', '.json'];
+  var presets = ['es2015'];
+
+  var bundler = watchify(browserify('static/js/es6/app.es6', {
+      debug: true,
+      extensions: extensions,
+      cache: {},
+      packageCache: {}
+    }).transform(babelify.configure({
+      extensions: extensions,
+      presets: presets
+    })
+  )
+  );
+
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('app.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./static/build'));
+  }
+
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle();
+    });
+  }
+
+  rebundle();
+}
+
+function watch() {
+  return compile(true);
+};
+
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return watch(); });
+
+gulp.task('default', ['watch']);
