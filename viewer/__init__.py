192c47014ada1c441478d47db03c7f4595d48a3e
Matthias Nilsson <matthias.nilsson@kb.se>
2016-12-12 13:44:56 +0100

    Don't follow 201/204 in Flask code
    It's up to the caller to follow Location headers.

--- viewer/__init__.py
+++ viewer/__init__.py
@@ -490,16 +490,7 @@ def _write_data(request, item=None, query_params=[]):
             if json_data is None:
                 return Response(status=400)
             else:
-                proxy_resp = _whelk_request(request, json_data, query_params)
-                # If the save operation goes well location is returned, then get the item to return to client
-                if ((proxy_resp.status_code == 201 or
-                     proxy_resp.status_code == 204) and
-                    'location' in proxy_resp.headers):
-                    item_id = _get_served_uri(proxy_resp.headers.get('location'), '')
-                    data = things.ldview.get_record_data(item_id)
-                    return Response(json.dumps(data), status=200, headers={'etag': proxy_resp.headers.get('etag'), 'Content-Type': JSONLD_MIMETYPE})
-                else:
-                    return proxy_resp
+                return _proxy_request(request, json_data, query_params)
         else:
             return Response(status=415)
     except Exception, e:
