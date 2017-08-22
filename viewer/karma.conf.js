a990d0c7f2458a316e108f5f05cb4b49f954db21
Ola Blissing <ola.blissing@gmail.com>
2017-08-22 13:26:44 +0200

    Add jquery path to karma config

--- viewer/karma.conf.js
+++ viewer/karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'bower_components/jquery/dist/jquery.js',
       'static/js/**/*.es6',
       'test/unit/**/*.js',
     ],
