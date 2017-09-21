4f7d4bede205b07192cb66b0693b7f175f18b022
Ola Blissing <ola.blissing@gmail.com>
2017-09-21 17:42:49 +0200

    Add extra field adder to empty local-entities

--- viewer/static/js/es6/components/item-local.vue
+++ viewer/static/js/es6/components/item-local.vue
@@ -316,6 +316,7 @@ export default {
         <field-adder v-if="!isLocked" :allowed="allowedProperties" :inner="true" :path="getPath"></field-adder>
         </span>
       </span>
+      <field-adder v-if="!isLocked && isEmpty" :allowed="allowedProperties" :inner="true" :path="getPath"></field-adder>
       <data-node v-show="expanded && k !== '_uid'" v-for="(k,v) in filteredItem" :parent-path="getPath" :entity-type="item['@type']" :is-inner="true" :is-locked="isLocked" :allow-local="true" :is-removable="false" :embedded="true" :parent-key="key" :parent-index="index" :key="k" :value="v" :focus="focus" :show-action-buttons="showActionButtons"></data-node>
     </div>
     <card-component v-if="isExpandedType" :title="getItemLabel" :focus-data="item" :uri="item['@id']" :is-local="true" :is-extractable="isExtractable" :is-locked="isLocked"></card-component>
