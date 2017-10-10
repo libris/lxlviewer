5f04e60df521b3f286a02576f7081184d306990b
Ola Blissing <ola.blissing@gmail.com>
2017-10-10 13:57:24 +0200

    Fix panel styles

--- viewer/static/js/es6/components/result-item-detailed.vue
+++ viewer/static/js/es6/components/result-item-detailed.vue
@@ -51,9 +51,8 @@ export default {
 .result-item-detailed {
   list-style: none;
   margin-bottom: 0.5em;
-  background-color: @neutral-color;
-  border-radius: 2px;
-  box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.2);
+  .panel-mixin(@neutral-color);
+  
   .label {
     color: @black;
     font-weight: bold;
