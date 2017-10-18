5fa3b215ccb4e635e380660395d8f6b826c0961d
Ola Blissing <ola.blissing@gmail.com>
2017-10-18 16:09:13 +0200

    Intro component

--- viewer/static/js/es6/components/intro-component.vue
+++ viewer/static/js/es6/components/intro-component.vue
@@ -36,8 +36,15 @@ export default {
 
 <template>
   <div class="panel panel-default intro-component">
-    <div class="libris-intro-video">
-      <iframe v-if="settings.siteInfo.title == 'libris.kb.se'" src="https://www.youtube-nocookie.com/embed/n5NOBPmIEf8?rel=0&amp;showinfo=0&html5=1" frameborder="0" allowfullscreen></iframe>
+    <div v-if="settings.siteInfo.title == 'libris.kb.se'" class="libris-intro-video-container">
+      <div class="libris-intro-video">
+        <iframe src="https://www.youtube-nocookie.com/embed/n5NOBPmIEf8?rel=0&amp;showinfo=0&html5=1" frameborder="0" allowfullscreen></iframe>
+      </div>
+    </div>
+    <div v-if="settings.siteInfo.title == 'libris.kb.se'" class="libris-intro-text">
+      <span class="header">Text</span>
+      <p>Lorizzle ipsum dolizzle nizzle amizzle, shiznit adipiscing elit. Nullam sapizzle velizzle, bow wow wow volutpizzle, suscipizzle check it out, mammasay mammasa mamma oo sa vel, arcu. Crunk eros. Pizzle izzle dolizzle bling bling turpis tempizzle.</p>
+      <a class="btn btn-primary">Gå till...</a>
     </div>
   </div>
 </template>
@@ -46,17 +53,34 @@ export default {
 @import './_variables.less';
 
 .intro-component {
-  .libris-intro-video {
-  	position: relative;
-  	padding-bottom: 56.25%; /* 16:9 */
-  	padding-top: 25px;
-  	height: 0;
-    iframe {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
+  display: flex;
+  justify-content: space-between;
+  .libris-intro-video-container {
+    flex-basis: 45%;
+    .libris-intro-video {
+      position: relative;
+      padding-bottom: 56.25%; /* 16:9 */
+      padding-top: 25px;
+      height: 0;
+      iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+  .libris-intro-text {
+    padding: 1em;
+    flex-basis: 45%;
+    flex-grow: 1;
+    .header {
+      font-size: 1.1em;
+      font-weight: bold;
+    }
+    a {
+      text-align: center;
     }
   }
 
