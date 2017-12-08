9addca3c34148b5d16ccd178b5af259860244912
Simon Alm Liljevall <simon.alm.liljevall@netlight.com>
2017-12-08 09:32:59 +0100

    Actually fix template rendering

--- viewer/__init__.py
+++ viewer/__init__.py
@@ -361,8 +361,13 @@ def _to_graph(data, base=None):
     return cg
 
 def _get_template_for(data):
-    for rtype in as_iterable(data.get(GRAPH)):
-        template = TYPE_TEMPLATES.get(rtype.get(TYPE))
+    type_key = ""
+    if not data.get(TYPE):
+        type_key = data.get(GRAPH)[1].get(TYPE)
+    else:
+        type_key = data.get(TYPE)
+    for rtype in as_iterable(type_key):
+        template = TYPE_TEMPLATES.get(rtype)
         if template:
             return template
     return 'edit.html'
