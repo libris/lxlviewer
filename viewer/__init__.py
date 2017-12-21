9360ec1d7cc17c608f1818daaff3561314f75409
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2017-12-21 12:56:53 +0100

    Allow POSTing to _merge.

--- viewer/__init__.py
+++ viewer/__init__.py
@@ -435,7 +435,7 @@ def create():
     return _write_data(request, query_params=MultiDict([]))
 
 
-@app.route('/_merge')
+@app.route('/_merge', methods=['GET','POST'])
 def _merge():
     return _proxy_request(request, session, query_params=['id1', 'id2', 'promote_id2'])
 
