ae550bf3db97aa2ff5949d22e28129bcf76b20a5
Sanna Frese <sanna.frese@kb.se>
2018-02-06 14:31:07 +0100

    Move 'link entity' option next to delete button

--- viewer/client/static/js/es6/components/editorcomponents/item-local.vue
+++ viewer/client/static/js/es6/components/editorcomponents/item-local.vue
@@ -344,11 +344,8 @@ export default {
 
 <template>
   <div class="item-local-container" v-bind:class="{'highlight': isNewlyAdded, 'expanded': expanded}">
-    <div class="link-indicator" :class="{'active': showLinkAction && status.inEdit}" v-if="isExtractable" @click="openExtractDialog" @mouseover="showLinkAction = true" @mouseout="showLinkAction = false">
-      <i v-show="showLinkAction && status.inEdit" class="fa fa-link">
-        <tooltip-component :show-tooltip="showLinkAction" tooltip-text="Link entity" translation="translatePhrase"></tooltip-component>
-      </i>
-      <i v-show="!showLinkAction || !status.inEdit" class="fa fa-unlink"></i>
+    <div class="link-indicator" v-if="isExtractable">
+      <i class="fa fa-unlink"></i>
     </div>
     <div v-if="!isExpandedType" class="item-local" :class="{'expanded': expanded, 'distinguish-removal': removeHover}">
       <div class="topbar">
@@ -361,6 +358,10 @@ export default {
         <span class="actions">
           <i v-if="!isLocked" class="fa fa-trash-o chip-action" :class="{'show-icon': showActionButtons}" v-on:click="removeThis(true)" @mouseover="removeHover = true" @mouseout="removeHover = false">
             <tooltip-component :show-tooltip="removeHover" tooltip-text="Remove" translation="translatePhrase"></tooltip-component>
+          </i>
+          <i v-show="status.inEdit" class="fa fa-link"
+          @click="openExtractDialog" @mouseover="showLinkAction = true" @mouseout="showLinkAction = false">
+            <tooltip-component :show-tooltip="showLinkAction" tooltip-text="Link entity" translation="translatePhrase"></tooltip-component>
           </i>
           <field-adder v-if="!isLocked" :entity-type="item['@type']" :allowed="allowedProperties" :inner="true" :path="getPath"></field-adder>
         </span>
@@ -390,10 +391,6 @@ export default {
     align-items: center;
     background: @gray-darker;
     color: @white;
-    &.active {
-      background: lighten(@gray-darker, 15%);
-      cursor: pointer;
-    }
   }
   &.highlight {
     transition: 0s ease;
