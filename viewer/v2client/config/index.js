289e9f27bd9bfe110ef016bbb1fddda564e58ccc
Ola Blissing <ola.blissing@gmail.com>
2018-03-26 15:16:22 +0200

    Add missing slash to public path

--- viewer/v2client/config/index.js
+++ viewer/v2client/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/katalogisering',
+    assetsPublicPath: '/katalogisering/',
 
     /**
      * Source Maps
