0d6663fcbea9f08f935595508a45a0b2b8f419a0
Ola Blissing <ola.blissing@gmail.com>
2018-04-10 14:40:04 +0200

    Remove duplicate import

--- viewer/v2client/src/components/Inspector.vue
+++ viewer/v2client/src/components/Inspector.vue
@@ -5,7 +5,6 @@ import * as DataUtil from '@/utils/data';
 import * as VocabUtil from '@/utils/vocab';
 import * as httpUtil from '@/utils/http';
 // import * as _ from 'lodash';
-import * as VocabUtil from '@/utils/vocab';
 import * as DisplayUtil from '@/utils/display';
 import * as RecordUtil from '@/utils/record';
 import EntityForm from '@/components/inspector/entity-form';
