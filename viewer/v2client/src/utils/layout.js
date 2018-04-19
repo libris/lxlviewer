ef2f97ef3764017fdf9d941e7308759d3ab57470
Ola Blissing <ola.blissing@gmail.com>
2018-04-19 18:05:05 +0200

    Add getPosition helper

--- viewer/v2client/src/utils/layout.js
+++ viewer/v2client/src/utils/layout.js
@@ -107,3 +107,16 @@ export function showPage(vueInstance) {
     vueInstance.initialized = true;
   }, 0);
 }
+
+export function getPosition(element) {
+  var xPosition = 0;
+  var yPosition = 0;
+
+  while(element) {
+      xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+      yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+      element = element.offsetParent;
+  }
+
+  return { x: xPosition, y: yPosition };
+}
