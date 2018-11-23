98cb787e46702c3252296eeecccefbdaf7c108e6
sannana <sanna.frese@kb.se>
2018-11-23 10:01:48 +0100

    Longer duration for transition

--- viewer/v2client/src/components/search/result-list-item.vue
+++ viewer/v2client/src/components/search/result-list-item.vue
@@ -129,7 +129,7 @@ export default {
     &.is-highlighted {
       opacity: 1;
       transform-origin: left center;
-      transition: transform .2s ease-in-out;
+      transition: transform .4s ease-in-out;
       transform: scale(1.05);
       border: 1px solid @brand-primary;
       box-shadow: -5px 5px 10px 0px rgba(0, 0, 0, 0.1), -5px 5px 20px 10px rgba(0, 0, 0, 0.1), 0 10px 5px -10px rgba(0, 0, 0, 0.1);    
