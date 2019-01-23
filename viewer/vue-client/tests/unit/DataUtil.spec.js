e8ccedcb6a589443bc1cf481ba09d8b0ee3a3eb6
Ola Blissing <ola.blissing@gmail.com>
2019-01-23 15:40:33 +0100

    Implement DataUtil.rewriteValueOfKey

---
+++ viewer/vue-client/tests/unit/DataUtil.spec.js
@@ -0,0 +1,71 @@
+
+import * as DataUtil from '@/utils/data';
+
+describe('rewriteValueOfKey()', () => {
+  const beforeObj = {
+    shouldBeReplaced: 'hello',
+    someOther: {
+      shouldBeReplaced: {},
+      someArray: [
+        'test',
+        {
+        },
+        {
+          '@id': 'test',
+        },
+        {
+          '@id': 'test2',
+          shouldBeReplaced: ['testvalue'],
+        },
+      ],
+    },
+  };
+  const key = 'shouldBeReplaced';
+  const replaceValue = 'newValue';
+
+  it('rewrite all keys (if run as deep = true) <key> with value supplied', () => {
+    const afterObj = DataUtil.rewriteValueOfKey(beforeObj, key, replaceValue, true);
+    const expectedObj = {
+      shouldBeReplaced: 'newValue',
+      someOther: {
+        shouldBeReplaced: 'newValue',
+        someArray: [
+          'test',
+          {},
+          {
+            '@id': 'test',
+          },
+          {
+            '@id': 'test2',
+            shouldBeReplaced: 'newValue',
+          },
+        ],
+      },
+    };
+    expect(afterObj).toEqual(expectedObj);
+  });
+  it('rewrite outermost key (if run as deep = false) <key> with value supplied', () => {
+    const afterObj = DataUtil.rewriteValueOfKey(beforeObj, key, replaceValue);
+    const afterObjExplicitFalse = DataUtil.rewriteValueOfKey(beforeObj, key, replaceValue, false);
+    const expectedObj = {
+      shouldBeReplaced: 'newValue',
+      someOther: {
+        shouldBeReplaced: {},
+        someArray: [
+          'test',
+          {
+          },
+          {
+            '@id': 'test',
+          },
+          {
+            '@id': 'test2',
+            shouldBeReplaced: ['testvalue'],
+          },
+        ],
+      },
+    };
+    expect(afterObj).toEqual(expectedObj);
+    expect(afterObjExplicitFalse).toEqual(expectedObj);
+  });
+});
