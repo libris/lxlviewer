fa10454e74f6fe5fb8f546105adc790f744ae4b4
Jesper Engström <jesper.engstrom@kb.se>
2019-02-19 22:20:02 +0100

    Create components for moving holdings

---
+++ viewer/vue-client/src/components/care/holding-mover.vue
@@ -0,0 +1,53 @@
+<script>
+import { mapGetters } from 'vuex';
+import PostPicker from '@/components/care/post-picker';
+
+export default {
+  name: 'holding-mover',
+  components: {
+    'post-picker': PostPicker,
+  },
+  props: {
+  },
+  methods: {
+  },
+  computed: {
+    ...mapGetters([
+      'userCare',
+    ]),
+  },
+};
+</script>
+
+<template>
+  <div class="HoldingMover">
+    <div class="HoldingMover-wrapper">
+      <div class="HoldingMover-label uppercaseHeading">{{ 'Sender' | translatePhrase }}</div>
+      <post-picker :items="userCare" info="Från den avsändande posten flyttar du bestånd eller annan information till den mottagande parten"/>
+    </div>
+    <div class="HoldingMover-wrapper">
+      <div class="HoldingMover-label uppercaseHeading">{{ 'Reciever' | translatePhrase }}</div>
+      <post-picker :items="userCare"/>
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+
+.HoldingMover  {
+  width: 100%;
+  display: flex;
+
+  &-wrapper {
+    max-width: 50%;
+    flex: 1;
+  }
+
+  &-label {
+    padding: 5px 10px;
+    background-color: @gray-lighter;
+    display: inline-block;
+  }
+}
+
+</style>
