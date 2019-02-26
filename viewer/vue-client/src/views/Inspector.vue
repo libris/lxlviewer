a0d9d21aeea517c03657308a5ddbda966f82b31f
Ola Blissing <ola.blissing@gmail.com>
2019-02-26 11:32:54 +0100

    Refactor tag-switch tooltip builder, change "Mark as" to "Flag for" for care

--- viewer/vue-client/src/views/Inspector.vue
+++ viewer/vue-client/src/views/Inspector.vue
@@ -672,7 +672,7 @@ export default {
             </div>
             <div class="Inspector-actions">
               <div class="TagContainer" v-if="recordType === 'Instance'">
-                <tag-switch :document-id="documentId" tag="Directory care" />
+                <tag-switch :document-id="documentId" :action-labels="{ on: 'Flag for', off: 'Unflag for' }" tag="Directory care" />
               </div>
               <reverse-relations 
                 class="Inspector-reverse" 
