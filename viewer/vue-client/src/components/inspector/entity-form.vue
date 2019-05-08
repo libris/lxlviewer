7274ce7b2161155c1e65258d6b8525ec13ebf0de
Jesper Engström <jesper.engstrom@kb.se>
2019-05-08 13:14:40 +0200

    Use focus-within to highlight when tabbing

--- viewer/vue-client/src/components/inspector/entity-form.vue
+++ viewer/vue-client/src/components/inspector/entity-form.vue
@@ -201,6 +201,12 @@ export default {
       }
     }
 
+    .user-is-tabbing &:focus-within { // icon 'hover-effect' when tabbing 
+      & .icon:not(.is-disabled) {
+        color: rgba(0, 0, 0, 0.6);
+      }
+    }
+
     & .icon {
       color: rgba(0, 0, 0, 0.3);
 
