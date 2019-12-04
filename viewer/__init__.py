aec592219902e3bd6f84b767c498a92a2619085a
Olov Ylinenpää <51744858+olovy@users.noreply.github.com>
2019-12-04 10:50:38 +0100

    Handle Etag with -gzip (#535)
    * Handle Etag with -gzip
    
    * Handle missing header

--- viewer/__init__.py
+++ viewer/__init__.py
@@ -578,7 +578,8 @@ app.context_processor(lambda: RDFNS)
 def vocabview(suffix=None):
     voc = daccess.vocab.get_util()
 
-    if request.headers.get('if-none-match') == daccess.vocab.vocab_etag:
+    etag = request.headers.get('if-none-match', default = '').replace('-gzip', '')
+    if etag == daccess.vocab.vocab_etag:
         return Response(status=304)
 
     def link(obj):
