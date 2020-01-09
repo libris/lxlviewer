61b2146cd5f0689aea82f2f8f990f5459685bcf7
Ola Blissing <ola.blissing@gmail.com>
2020-01-09 13:16:11 +0100

    Change item-error to be a table of key/values instead of just json string

--- viewer/vue-client/src/components/inspector/item-error.vue
+++ viewer/vue-client/src/components/inspector/item-error.vue
@@ -7,6 +7,10 @@ export default {
   mixins: [ItemMixin],
   props: {
     item: {},
+    isLocked: {
+      type: Boolean,
+      default: true,
+    },
   },
   data() {
     return {
@@ -21,7 +25,7 @@ export default {
       if (cleanItem.hasOwnProperty('_uid')) {
         delete cleanItem._uid;
       }
-      return JSON.stringify(cleanItem);
+      return cleanItem;
     },
     failedValidations() {
       const failedValidations = [];
@@ -55,7 +59,12 @@ export default {
 
 <template>
   <div class="ItemError" :id="`formPath-${path}`" :class="{ 'has-failed-validations': failedValidations.length > 0 }">
-    <code>{{itemAsJson}}</code>
+    <i v-if="!isLocked" class="fa fa-question-circle" />
+    <table>
+      <tr v-for="(value, key) in itemAsJson" :key="key">
+        <td class="ItemError-key">{{ key }}</td><td class="ItemError-value">{{ value }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -65,11 +74,14 @@ export default {
   line-height: 1.6;
   width: 100%;
   display: inline-block;
+  font-family: monospace;
+  &-key {
+    padding-right: 1em;
+  }
   code {
     color: @black;
     background-color: transparent;
   }
-
   &.has-failed-validations {
     outline: 1px dotted red;
   }
