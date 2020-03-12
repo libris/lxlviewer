511ec4fb5df0bf9dc8e4d7a37bcca2c188c1c36b
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2020-03-12 10:44:29 +0100

    Fix editor not working on templates

--- viewer/vue-client/src/store.js
+++ viewer/vue-client/src/store.js
@@ -518,7 +518,9 @@ const store = new Vuex.Store({
         }
       });
       // Check if we should remove work node (if it went from local to being linked)
-      if (inspectorData.mainEntity.hasOwnProperty('instanceOf') && (inspectorData.mainEntity.instanceOf === null || inspectorData.mainEntity.instanceOf['@id'].indexOf('#work') === -1)) {
+      const noWork = inspectorData.mainEntity.hasOwnProperty('instanceOf') && inspectorData.mainEntity.instanceOf === null;
+      const localWork = inspectorData.mainEntity.instanceOf['@id'] && inspectorData.mainEntity.instanceOf['@id'].indexOf('#work') === -1;
+      if (noWork || localWork) {
         if (state.inspector.data.hasOwnProperty('work')) {
           delete inspectorData.work;
         }
