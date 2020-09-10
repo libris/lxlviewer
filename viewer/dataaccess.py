41775dd3d2c97a7760ebb55888fef832e946ecea
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2020-09-10 13:21:41 +0200

    Add search facet for contribution.agent.@id

--- viewer/dataaccess.py
+++ viewer/dataaccess.py
@@ -137,7 +137,13 @@ sites = {
                     "sort":"value",
                     "sortOrder":"desc",
                     "size":100
+                },
+                "contribution.agent.@id":{
+                    "sort":"value",
+                    "sortOrder":"desc",
+                    "size":20
                 }
+
             }
         """,
         "itemList": [
