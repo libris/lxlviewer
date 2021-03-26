6845c296ecff675c2e6d859c15c082191dce5736
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2021-03-26 19:18:20 +0100

    Add facets for subject and bibliography

--- viewer/dataaccess.py
+++ viewer/dataaccess.py
@@ -148,7 +148,17 @@ sites = {
                     "sort":"value",
                     "sortOrder":"desc",
                     "size":20
-                }
+                },
+                "instanceOf.subject.@id":{
+                    "sort":"value",
+                    "sortOrder":"desc",
+                    "size":100
+                },
+                "meta.bibliography.@id":{
+                    "sort":"value",
+                    "sortOrder":"desc",
+                    "size":100
+                },
             }
         """,
         "itemList": [
