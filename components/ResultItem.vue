d101995beee464782f17646a6fa1dd6fe915b333
Ola Blissing <ola.blissing@gmail.com>
2021-07-02 17:14:05 +0200

    Consuming search results into find view

---
+++ components/ResultItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="ResultItem">
+    <span class="ResultItem-title">{{ itemData['@id'] }}</span>
+    <span class="ResultItem-scheme chip">{{ itemData['inScheme'].titleByLang['sv'] }}</span>
+    <span class="ResultItem-type chip">{{ itemData['@type'] }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false
+    }
+  },
+  props: {
+    itemData: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.ResultItem {
+  border: 1px solid $gray-200;
+  display: flex;
+  flex-direction: row;
+  padding: 0.5em 1em;
+  gap: 1em;
+  .chip {
+    background-color: $gray-100;
+    border-radius: 2em;
+    padding: 0.5em 0.75em;
+  }
+  > span {
+    padding: 0.5em;
+    &:first-child {
+      flex-grow: 1;
+    }
+  }
+}
+
+</style>
--- pages/find/index.vue
+++ pages/find/index.vue
@@ -1,16 +1,45 @@
 <template>
   <div class="container-fluid">
-    <h1>Query: {{ $route.query }}</h1>
+    <div class="row">
+      <div class="CollectionFilters col-md-12">
+        <p>Query: {{ $route.query }}</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="DetailedFilters col-lg-4 col-xl-3 col-xxl-2"></div>
+      <div class="SearchResults col-lg-8 col-xl-9 col-xxl-10">
+        <ResultItem v-for="resultItem in pageData.items" :item-data="resultItem" :key="resultItem['@id']" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import about from '../about.vue'
-  export default {
-  components: { about },
-    // page properties go here
-  }
+export default {
+  data() {
+    return {
+    }
+  },
+  async asyncData({ $config, route, params, $http }) {
+    const query = route.query;
+    let queryString;
+    Object.entries(query).forEach(([key, val]) => queryString += `${key}=${val}&`);
+    const pageData = await $http.$get(`${$config.apiPath}/find.jsonld?${queryString}`)
+    return { pageData }
+  },
+  // call fetch only on client-side
+  fetchOnServer: false
+}
 </script>
 
-<style>
+<style lang="scss">
+.CollectionFilters {
+  background-color: $gray-100;
+  padding: 1em 0;
+  border: solid $gray-200;
+  border-width: 0px 0px 1px 0px;
+}
+.SearchResults {
+
+}
 </style>
