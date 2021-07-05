c8bad1bd685cbdbc85394c19b1ec524fabeb6440
Ola Blissing <ola.blissing@gmail.com>
2021-07-05 18:26:21 +0200

    Dynamic doc articles

---
+++ pages/doc/_article.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container-fluid">
+    <div class="col-12 col-md-8" v-html="pageData['@graph'][1].articleBody"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  async asyncData({ $config, route, params, $http }) {
+    const pageData = await $http.$get(`${$config.apiPath}/doc/${params.article}/data.jsonld`)
+    return { pageData }
+  },
+  // call fetch only on client-side
+  fetchOnServer: false
+}
+</script>
+
+<style>
+</style>
