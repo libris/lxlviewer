45083dd4c07767b8ae16f63b7658eab34c903f5b
Ola Blissing <ola.blissing@gmail.com>
2021-07-06 09:55:46 +0200

    Doc styles

--- pages/doc/_article.vue
+++ pages/doc/_article.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-fluid">
-    <div class="col-12 col-md-8" v-html="pageData['@graph'][1].articleBody"></div>
+    <div class="col-12 col-md-12 col-lg-10 col-xl-8 col-xxl-6 py-4" v-html="pageData['@graph'][1].articleBody"></div>
   </div>
 </template>
 
