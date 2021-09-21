7bdcb0dc56dde96cc98129a8d14a8340ddee1452
Ola Blissing <ola.blissing@gmail.com>
2021-09-21 19:33:27 +0200

    Pagination style cleanup

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -81,7 +81,6 @@ export default {
   justify-content: center;
   &-item {
     border: 1px solid $gray-300;
-    margin-left: 0.5em;
     a {
       width: 100%;
       height: 100%;
@@ -104,6 +103,11 @@ export default {
   }
   ul {
     padding: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 0.5em;
     li {
       display: inline-block;
     }
