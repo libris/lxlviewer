9cddfbd6bf49d0442a20768fb540ed175d8571c6
Ola Blissing <ola.blissing@gmail.com>
2021-10-11 11:08:24 +0200

    Footer offset in vocab

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,8 @@
 <template>
-  <footer class="bd-footer p-2 p-md-3 bg-dark text-light text-center text-sm-start" :class="{'offset-md-4 offset-lg-3 offset-xl-4 offset-xxl-3': $route.name.startsWith('marcframe') }">
+  <footer class="bd-footer p-2 p-md-3 bg-dark text-light text-center text-sm-start" :class="{
+    'offset-md-4 offset-lg-3 offset-xl-4 offset-xxl-3': $route.name.startsWith('marcframe'),
+    'offset-md-4 offset-lg-4 offset-xl-3 offset-xxl-3': $route.name.startsWith('vocab')
+    }">
     <div class="container">
       <div class="row">
         <div class="col-md-2 col-lg-1 text-center">
