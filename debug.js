7b275196e30abf8ebc2d6080a0f05fa8c4b96cc3
Ola Blissing <ola.blissing@gmail.com>
2021-10-21 10:37:12 +0200

    Add log options to debug tool

--- debug.js
+++ debug.js
@@ -1,9 +1,20 @@
+const lxlLogStack = [];
+
+export function lxlLog(...strings) {
+  if (lxlLogStack.indexOf(JSON.stringify(strings.join())) === -1) {
+    lxlLogStack.push(JSON.stringify(strings.join()));
+    return console.log('%c LXL TOOLS ', 'background: #009788; color: #fff;', ...strings);
+  }
+  return false;
+};
+
+
 const lxlWarnStack = [];
 
 export function lxlWarning(...strings) {
   if (lxlWarnStack.indexOf(JSON.stringify(strings.join())) === -1) {
     lxlWarnStack.push(JSON.stringify(strings.join()));
-    return console.warn('%c LXL ', 'background: #009788; color: #fff;', ...strings);
+    return console.warn('%c LXL TOOLS ', 'background: #009788; color: #fff;', ...strings);
   }
   return false;
 };
@@ -13,7 +24,7 @@ const lxlErrorStack = [];
 export function lxlError(...strings) {
   if (lxlErrorStack.indexOf(JSON.stringify(strings.join())) === -1) {
     lxlErrorStack.push(JSON.stringify(strings.join()));
-    return console.error('%c LXL ERROR ', 'background: #a50000; color: #fff;', ...strings);
+    return console.error('%c LXL TOOLS ', 'background: #a50000; color: #fff;', ...strings);
   }
   return false;
 };
