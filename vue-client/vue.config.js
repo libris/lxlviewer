f782463beb13e91cd161c99922aceab52d5ebcd1
Ola Blissing <ola.blissing@gmail.com>
2021-10-23 02:50:27 +0200

    Only follow symlinks if node_env is other than production

--- vue-client/vue.config.js
+++ vue-client/vue.config.js
@@ -32,7 +32,7 @@ module.exports = {
     },
   },
   chainWebpack(config) {
-    config.resolve.symlinks(false);
+    config.resolve.symlinks(process.env.NODE_ENV === 'production' ? false : true);
     config.module
       .rule('vue')
       .use('vue-loader')
