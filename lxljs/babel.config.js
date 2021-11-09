ca9403396d80dc5da36c9a83b93152c1ab04080a
Ola Blissing <ola.blissing@gmail.com>
2021-11-09 11:35:44 +0100

    Add babel preset-env

--- lxljs/babel.config.js
+++ lxljs/babel.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        "targets": {
+          "node": true
+        }
+      }
+    ]
+  ],
   env: {
     test: {
       plugins: ['@babel/plugin-transform-modules-commonjs'],
