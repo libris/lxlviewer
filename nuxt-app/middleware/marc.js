927e47520276a30b4b5f42286b03c817aaf1fc90
Ola Blissing <ola.blissing@gmail.com>
2021-11-15 15:10:16 +0100

    Make the marc middleware redirect conditional depending on presence in vocab

--- nuxt-app/middleware/marc.js
+++ nuxt-app/middleware/marc.js
@@ -2,9 +2,15 @@
 // They are defined as '/marc/termName'
 // But they need to be handled as '/vocab/marc:termName'
 
-export default function ({ route, redirect }) {
+export default function ({ route, store, redirect }) {
   if (route.path.startsWith('/marc/')) {
-    const newPath = route.path.replace('/marc/', '/vocab/marc:');
-    return redirect(newPath);
+    const vocab = store.getters.resources.vocab;
+    const termPath = route.path;
+    const tryVocabPath = `https://id.kb.se${termPath}`;
+    const term = vocab.get(tryVocabPath);
+    if (term) {
+      const newPath = route.path.replace('/marc/', '/vocab/marc:');
+      return redirect(newPath);
+    }
   }
 }
