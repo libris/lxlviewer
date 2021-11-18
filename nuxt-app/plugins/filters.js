8dc4a3b90bde99c837f1035e0d8cbcaa7f51639d
Ola Blissing <ola.blissing@gmail.com>
2021-11-18 11:02:08 +0100

    Remove 2 filters

--- nuxt-app/plugins/filters.js
+++ nuxt-app/plugins/filters.js
@@ -1,17 +1,6 @@
 import Vue from 'vue'
 import envComputer from '@/plugins/env';
 
-Vue.filter('translateUriEnv', (uri) => {
-  return uri.replace('https://id.kb.se', envComputer(process.env.ENV));
-});
-
-Vue.filter('removeBaseUri', (uri) => {
-  const envHost = envComputer(process.env.ENV);
-  if (uri.includes(envHost)) {
-    return uri.replace(envHost, '/');
-  }
-  return uri.replace('https://id.kb.se/', '/');
-});
 Vue.filter('replaceBaseWithApi', (uri) => {
   return uri.replace('https://id.kb.se', process.env.API_PATH);
 });
