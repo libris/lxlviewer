326edfac8ae7545685cdaf43d8664c2258b14ffe
Ola Blissing <ola.blissing@gmail.com>
2021-11-26 11:50:12 +0100

    Remove bg on normal table

--- nuxt-app/components/EntityTable.vue
+++ nuxt-app/components/EntityTable.vue
@@ -198,7 +198,6 @@ export default {
   }
   &-body {
     padding: 0.5rem 0.25rem 0.5rem 1rem;
-    background-color: $white;
     @media (min-width: 768px) {
       padding: 0.5rem 1rem 0.5rem 1.5rem;
     }
