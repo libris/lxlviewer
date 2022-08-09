bbd13eb2b274d1524972c9afa3bd6426e509a9c6
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2022-08-09 09:57:45 +0200

    Use sjcl as a fallback for SHA-256 when crypto.subtle not available

--- lxljs/string.js
+++ lxljs/string.js
@@ -11,11 +11,20 @@ export function removeDomain(string, removableBaseUriArray) {
 }
 
 export async function digestMessage(message) {
-  const msgUint8 = new TextEncoder().encode(message); // encode as (utf-8) Uint8Array
-  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8); // hash the message
-  const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
-  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
-  return hashHex;
+  try {
+    const msgUint8 = new TextEncoder().encode(message); // encode as (utf-8) Uint8Array
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8); // hash the message
+    const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
+    return hashHex;
+  } catch (e) {
+    // crypto.subtle is only available on https sites and localhost but not e.g. http://kblocalhost.kb.se
+    // eslint-disable-next-line
+    const sjcl = await import('sjcl');
+    const hashArray = sjcl.hash.sha256.hash(message);
+    const hashHex = sjcl.codec.hex.fromBits(hashArray);
+    return hashHex;
+  }
 }
 
 export function convertToPrefix(uri, context) {
