49fe217a1265f0459b750ac54bc82e7387db1517
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2022-08-24 17:50:14 +0200

    ¯\_(ツ)_/¯

--- lxljs/display.js
+++ lxljs/display.js
@@ -444,7 +444,9 @@ export function getDisplayObject(item, level, resources, quoted, settings) {
             }
           }
         }
-        const str = s => JSON.stringify(s).replaceAll('"', '');
+        // FIXME: Why did I start getting "TypeError: JSON.stringify(...) is undefined" here ??
+        // const str = s => JSON.stringify(s).replaceAll('"', '');
+        const str = s => s;
         lxlLog(`Computed alternateProperties for ${trueItem['@type']}. Looked for: ${property.alternateProperties.map(str).join(', ')} | Settled on: ${str(foundProperty)}`);
       }
     }
