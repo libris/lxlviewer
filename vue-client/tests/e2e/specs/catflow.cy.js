74020a0a69b864bd48b589c78d9ea66fda3ccf6b
Jesper Engström <jesper.engstrom@kb.se>
2023-11-08 16:27:34 +0100

    Fix failing cypress tests

--- vue-client/tests/e2e/specs/catflow.cy.js
+++ vue-client/tests/e2e/specs/catflow.cy.js
@@ -22,33 +22,31 @@ describe('Cataloging a book', () => {
       cy.get('.Inspector').its('length').should('eql', 1);
     });
 
-    // TODO: Fix test
     it('Add and edit a string field', () => {
       const compareValue = 'this is an example text';
       cy.get('body').type('{alt}f');
-      cy.focused().type('/note'); // no result for /note?
+      cy.focused().type('cataloguersNote');
       cy.get('.FieldAdderPanel-fieldList button').first().focus();
       cy.focused().type('{enter}'); // Choose the prop
-      cy.focused().type('{esc}'); // Close the panel
       cy.focused().type(compareValue);
+      cy.focused().type('{esc}'); // Close the panel
       cy.focused().blur();
-      cy.get('#formPath-mainEntity\\.note textarea').should('have.value', compareValue);
+      cy.get('#formPath-mainEntity\\.cataloguersNote\\[0\\] textarea').should('have.value', compareValue);
     });
 
-    // TODO: Fix test
     it('Remove a local entity', () => {
       cy.get('#formPath-mainEntity\\.hasTitle\\[0\\]').should('exist');
-      cy.get('#formPath-mainEntity\\.hasTitle\\[0\\] .RemoveAction [tabindex]:not([tabindex="-1"])').click(); // no class RemoveAction?
+      // no unique identifier for remove button - get the aria-label (not ideal...)
+      cy.get('#formPath-mainEntity\\.hasTitle\\[0\\] .EntityAction[aria-label="Ta bort"][tabindex]:not([tabindex="-1"])').click();
       cy.get('#formPath-mainEntity\\.hasTitle\\[0\\]').should('not.exist');
     });
 
-    // TODO: Fix test
     it('Add a linked entity', () => {
-      cy.get('#formPath-work\\.subject\\[0\\]').should('not.exist');
-      cy.get('#formPath-work\\.subject .EntityAdder [tabindex]:not([tabindex="-1"])').click(); // no such elm: #formPath-work?
+      cy.get('#formPath-mainEntity\\.instanceOf\\.subject\\[0\\]').should('not.exist');
+      cy.get('#formPath-mainEntity\\.instanceOf\\.subject .EntityAdder [tabindex]:not([tabindex="-1"])').click();
       cy.focused().type('*');
       cy.get('.PanelSearch-listItem').first().find('button').click();
-      cy.get('#formPath-work\\.subject\\[0\\]').should('exist');
+      cy.get('#formPath-mainEntity\\.instanceOf\\.subject\\[0\\]').should('exist');
     });
   });
 });
--- vue-client/tests/e2e/specs/panels.cy.js
+++ vue-client/tests/e2e/specs/panels.cy.js
@@ -20,7 +20,6 @@ describe('Opening and closing of panels', () => {
     });
 
     it('... the entiy-adder panel', () => {
-      cy.gotoNewDocument();
       cy.get('#formPath-mainEntity\\.mediaType .EntityAdder').click();
       cy.get('.PanelComponent.EntityAdderPanel').should('exist');
       cy.get('.PanelComponent .PanelComponent-windowControl i.fa-close').click();
@@ -35,14 +34,14 @@ describe('Opening and closing of panels', () => {
 
     // TODO: fix test
     it('... the search-window panel', () => {
-      cy.gotoNewDocument();
-      cy.get('#formPath-mainEntity\\.instanceOf .ItemSibling-action.LinkAction [tabindex]:not([tabindex="-1"])').click(); // No itemSibling?
+      // no unique identifier for link button - get the aria-label (not ideal...)
+      cy.get('#formPath-mainEntity\\.instanceOf > .ItemLocal-heading .EntityAction[aria-label="Skapa/länka"][tabindex]:not([tabindex="-1"])')
+        .as('LinkAction');
+      cy.get('@LinkAction').click();
       cy.get('.PanelComponent.SearchWindowPanel').should('exist');
       cy.get('.PanelComponent .PanelComponent-windowControl i.fa-close').click();
       cy.get('.PanelComponent.SearchWindowPanel').should('not.exist');
-      cy.get('#formPath-mainEntity\\.instanceOf .ItemSibling-action.LinkAction [tabindex]:not([tabindex="-1"])').as('LinkAction');
-      cy.get('@LinkAction').focus();
-      cy.get('@LinkAction').trigger('keyup', { keyCode: 13, which: 13 });
+      cy.get('@LinkAction').type('{enter}', { force: true });
       cy.get('.PanelComponent.SearchWindowPanel').should('exist');
       cy.get('body').trigger('keydown', { keyCode: 27, which: 27 });
       cy.get('.PanelComponent.SearchWindowPanel').should('not.exist');
