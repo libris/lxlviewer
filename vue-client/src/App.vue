cd3c89146efcb09b0e5396e8763d72a7a9a4ad86
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2024-01-09 11:13:34 +0100

    Try guarding mocked helpfile import

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -321,7 +321,7 @@ export default {
       };
     },
     fetchHelpDocs() {
-      if (this.settings.mockHelp === true) {
+      if (import.meta.env.DEV && this.settings.mockHelp === true) {
         window.lxlInfo('🎭 MOCKING HELP FILE - Using file from local lxl-helpdocs repository');
         // eslint-disable-next-line import/no-extraneous-dependencies
         import('@/../../../lxl-helpdocs/build/help.json').then((module) => {
