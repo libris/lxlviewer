de0973ad87b12803a0bb08b22aa9af5438239b6b
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-01-11 08:06:36 +0100

    Remove redundant page type annotations

--- lxl-web/src/routes/(app)/[[lang=lang]]/[fnurgel=fnurgel]/+page.server.ts
+++ lxl-web/src/routes/(app)/[[lang=lang]]/[fnurgel=fnurgel]/+page.server.ts
@@ -1,10 +1,9 @@
-import type { PageServerLoad } from './$types';
 import { API_URL } from '$env/static/private';
 
-export const load = (async ({ params, fetch }) => {
+export const load = async ({ params, fetch }) => {
 	const iri = `${API_URL}/${params.fnurgel}`;
 	const response = await fetch(iri, { headers: { Accept: 'application/ld+json' } });
 	const doc = await response.json();
 
 	return { fnurgel: params.fnurgel, iri: iri, lang: params.lang ?? 'sv', doc: doc };
-}) satisfies PageServerLoad;
+};
