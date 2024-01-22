492c718f65f59dc46c9e31e2b7a4769d84378f62
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-01-22 16:40:30 +0100

    Add basic documentation

--- lxl-web/src/lib/components/DecoratedData.svelte
+++ lxl-web/src/lib/components/DecoratedData.svelte
@@ -20,7 +20,7 @@
 		'_contentAfter'
 	];
 
-	const flattenProperties = ['_display', '@value'];
+	const flattenedProperties = ['_display', '@value'];
 
 	function getFilteredEntries(data: Record<string, ResourceData>) {
 		return Object.entries(data).filter(([key]) => !hiddenProperties.includes(key));
@@ -56,6 +56,10 @@
 	}
 </script>
 
+<!-- 
+  @component
+	Component used for rendering decorated data.
+-->
 {#if data && typeof data === 'object'}
 	{#if Array.isArray(data)}
 		{#each data as arrayItem}
@@ -64,7 +68,7 @@
 	{:else}
 		{data?._contentBefore || ''}
 		{#each getFilteredEntries(data) as [key, value]}
-			{#if flattenProperties.includes(key)}
+			{#if flattenedProperties.includes(key)}
 				<svelte:self data={value} />
 			{:else}
 				<svelte:element this={getElementType(value)} {...getElementAttributes({ key, value })}>
