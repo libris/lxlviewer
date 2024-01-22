9791a895a397ea41afcea60b8bcf2ea62b574498
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-01-22 16:41:20 +0100

    Remove test files

--- lxl-web/src/routes/(test)/decorated/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import DecoratedData from '$lib/components/DecoratedData.svelte';
-	import exampleDecorated from './exampleDecorated.json';
-</script>
-
-<DecoratedData data={exampleDecorated} />
