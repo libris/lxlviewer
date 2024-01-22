e7eff1321236992b70de9e2242d82f640c05b23e
Johan Bisse Mattsson <johan@johanbissemattsson.se>
2024-01-22 16:43:46 +0100

    Add component for rendering decorated data (#938)
    Merging this now so we can test it together with the helper functions creating the decorated data.
    
    * Add component for rendering decorated data
    
    * Add support for adding custom decorated value component
    
    * Skip rendering spans for _display property
    
    * Rename property element type prop
    
    * Skip rendering @value as property
    
    * Restructure code
    
    * Add support for rendering custom components
    
    * Simplify code and remove custom components for now
    
    * Add support for decorated links
    
    * Fix links
    
    * Get link from _hint and @id
    
    * Add basic documentation
    
    * Remove test files

---
+++ lxl-web/src/lib/components/DecoratedData.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+	type ResourceData =
+		| null
+		| boolean
+		| string
+		| number
+		| ResourceData[]
+		| undefined
+		| { [key: string]: ResourceData };
+
+	export let data: ResourceData;
+
+	const hiddenProperties = [
+		'@context',
+		'@type',
+		'@id',
+		'_hint',
+		'_style',
+		'_contentBefore',
+		'_contentAfter'
+	];
+
+	const flattenedProperties = ['_display', '@value'];
+
+	function getFilteredEntries(data: Record<string, ResourceData>) {
+		return Object.entries(data).filter(([key]) => !hiddenProperties.includes(key));
+	}
+
+	function getObjectProperty(value: ResourceData, name: string) {
+		if (value && typeof value === 'object' && !Array.isArray(value) && name in value) {
+			return value[name];
+		}
+		return undefined;
+	}
+
+	function getLink(value: ResourceData) {
+		const hints = getObjectProperty(value, '_hint');
+		if (Array.isArray(hints) && hints.includes('link')) {
+			return value?.['@id' as keyof typeof value];
+		}
+		return undefined;
+	}
+
+	function getElementType(value: ResourceData) {
+		if (getLink(value)) {
+			return 'a';
+		}
+		return 'span';
+	}
+
+	function getElementAttributes({ key, value }: { key: string; value: ResourceData }) {
+		return {
+			'data-property': key,
+			href: getLink(value)
+		};
+	}
+</script>
+
+<!-- 
+  @component
+	Component used for rendering decorated data.
+-->
+{#if data && typeof data === 'object'}
+	{#if Array.isArray(data)}
+		{#each data as arrayItem}
+			<svelte:self data={arrayItem} />
+		{/each}
+	{:else}
+		{data?._contentBefore || ''}
+		{#each getFilteredEntries(data) as [key, value]}
+			{#if flattenedProperties.includes(key)}
+				<svelte:self data={value} />
+			{:else}
+				<svelte:element this={getElementType(value)} {...getElementAttributes({ key, value })}>
+					<svelte:self data={value} />
+				</svelte:element>
+			{/if}
+		{/each}
+		{data?._contentAfter || ''}
+	{/if}
+{:else}
+	{data}
+{/if}
