ce0e94a0fcc5c669efe316ef445295c559401bfb
Jesper Engström <jesper.engstrom@kb.se>
2024-02-10 00:02:09 +0100

    Extend site header, remove extra layout

--- lxl-web/src/lib/components/Logo.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import logo from '$lib/assets/img/logo-brown-flare.svg';
-</script>
-
-<img alt="Libris logo" src={logo} />
--- lxl-web/src/routes/(app)/[[lang=lang]]/+layout.svelte
+++ lxl-web/src/routes/(app)/[[lang=lang]]/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	import SiteHeader from './SiteHeader.svelte';
+	import '../../../app.css';
+	export let data;
 </script>
 
+<svelte:head>
+	<title>Libris</title>
+	<base href={data.base} />
+</svelte:head>
 <SiteHeader />
 <slot />
 <div id="floating-elements-container" />
