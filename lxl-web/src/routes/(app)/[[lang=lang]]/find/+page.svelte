d7b1b658117e8d37d28256c97a82fcb2da0e1d67
Jesper Engström <jesper.engstrom@kb.se>
2024-02-26 15:10:32 +0100

    Cleanup code

--- lxl-web/src/routes/(app)/[[lang=lang]]/find/+page.svelte
+++ lxl-web/src/routes/(app)/[[lang=lang]]/find/+page.svelte
@@ -6,7 +6,9 @@
 	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
 
-	$: numHits = $page.data.searchResult.totalItems;
+	$: searchResult = $page.data.searchResult;
+	$: numHits = searchResult.totalItems;
+
 	const sortOrder = $page.url.searchParams.get('_sort');
 	const sortOptions = [
 		{ value: '', label: 'Relevans' },
@@ -20,97 +22,6 @@
 		searchParams.set('_sort', value);
 		goto(`find?${searchParams.toString()}`, { invalidateAll: true });
 	}
-
-	$: ({ first, last, next, totalItems, itemsPerPage, itemOffset } = $page.data.searchResult);
-	$: pagination = {
-		get prev() {
-			return {
-				elem: itemOffset > 0 ? 'a' : null,
-				href: setOffsetParam(itemOffset - itemsPerPage > 0 ? itemOffset - itemsPerPage : 0),
-				label: '←',
-				ariaLabel: 'föregående'
-			};
-		},
-		get firstPage() {
-			return {
-				elem: first && this.currentPage.number > 1 ? 'a' : null,
-				href: first?.['@id'],
-				number: 1
-			};
-		},
-		get ellipsisFirst() {
-			return {
-				elem: this.currentPage.number > 2 ? 'span' : null,
-				label: '...'
-			};
-		},
-		get prevPage() {
-			return {
-				...this.prev,
-				elem:
-					this.currentPage.number === this.lastPage.number && this.currentPage.number > 2
-						? 'a'
-						: null,
-				href: setOffsetParam(itemOffset - itemsPerPage),
-				number: this.currentPage.number - 1
-			};
-		},
-		get currentPage() {
-			return {
-				elem: 'span',
-				number: Math.floor(itemOffset / itemsPerPage) + 1
-			};
-		},
-		get nextPage() {
-			return {
-				elem: next ? 'a' : null,
-				href: next?.['@id'],
-				number: this.currentPage.number + 1
-			};
-		},
-		get secondNextPage() {
-			return {
-				elem: this.currentPage.number === 1 && totalItems > itemsPerPage * 2 ? 'a' : null,
-				href: setOffsetParam(itemOffset + itemsPerPage * 2),
-				number: this.nextPage.number + 1
-			};
-		},
-		get ellipsisLast() {
-			return {
-				...this.ellipsisFirst,
-				elem: this.lastPage.number - this.nextPage.number > 1 ? 'span' : null
-			};
-		},
-		get lastPage() {
-			return {
-				elem:
-					last &&
-					this.currentPage.number !== Math.ceil(totalItems / itemsPerPage) &&
-					this.nextPage.number !== Math.ceil(totalItems / itemsPerPage)
-						? 'a'
-						: null,
-				href: last?.['@id'],
-				number: Math.ceil(totalItems / itemsPerPage)
-			};
-		},
-		get next() {
-			return {
-				elem: next ? 'a' : null,
-				href: next?.['@id'],
-				label: '→',
-				ariaLabel: 'Nästa'
-			};
-		}
-	};
-
-	function setOffsetParam(offset: number) {
-		const params = $page.url.searchParams;
-		params.set('_offset', offset.toString());
-		return `find?${params.toString()}`;
-	}
-	console.log($page.url);
-	console.log($page.data.searchResult);
-	$: searchResult = $page.data.searchResult;
 </script>
 
 <SeachMapping mapping={searchResult.mapping} />
