e0d4fce44ac83443a6c5901f80bc3f3a339c8ffd
Jesper Engström <jesper.engstrom@kb.se>
2024-02-27 11:40:59 +0100

    Use new decorated functionality on seach cards & mapping

--- lxl-web/src/routes/(app)/[[lang=lang]]/find/SeachMapping.svelte
+++ lxl-web/src/routes/(app)/[[lang=lang]]/find/SeachMapping.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import DecoratedData from '$lib/components/DecoratedData.svelte';
+	import { ShowLabelsOptions } from '$lib/types/DecoratedData';
 	import type { DisplayMapping } from './search';
 	export let mapping: DisplayMapping[];
 
@@ -12,7 +13,9 @@
 			<li class="justify-center rounded-md bg-positive-inv px-4 py-2">
 				<span class="mr-2">
 					<span class="text-secondary-inv">{filter.label}</span>
-					<span class="font-bold text-primary-inv"><DecoratedData data={filter.display} /></span>
+					<span class="font-bold text-primary-inv"
+						><DecoratedData data={filter.display} showLabels={ShowLabelsOptions['Never']} /></span
+					>
 				</span>
 				{#if 'up' in filter}
 					<a class="text-secondary-inv" href={filter.up?.['@id']}>x</a>
