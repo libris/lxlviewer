0c3c37b16c0a77665caaddd40c7fec0c160a11f3
Jesper Engström <jesperengstrom@users.noreply.github.com>
2024-03-25 09:35:21 +0100

    Publication year on search card (#981)

--- lxl-web/src/routes/(app)/[[lang=lang]]/find/SearchCard.svelte
+++ lxl-web/src/routes/(app)/[[lang=lang]]/find/SearchCard.svelte
@@ -1,12 +1,40 @@
 <script lang="ts">
+	import jmespath from 'jmespath';
 	import { relativizeUrl } from '$lib/utils/http';
 	import DecoratedData from '$lib/components/DecoratedData.svelte';
 	import type { ResourceData } from '$lib/types/ResourceData';
 	import { ShowLabelsOptions } from '$lib/types/DecoratedData';
 	export let item: { '@id': string; 'card-heading': ResourceData; 'card-body': ResourceData };
+
+	function getInstanceData(instances: ResourceData) {
+		if (typeof instances === 'object') {
+			let years: string = '';
+			let count = 1;
+			let query = '_display[].publication[].*[][?year].year[]';
+
+			if (Array.isArray(instances)) {
+				count = instances.length;
+				query = '[]._display[].publication[].*[][?year].year[]';
+			}
+
+			let res = jmespath.search(instances, query) as string[] | null;
+			if (res) {
+				years = res
+					.filter((el, i, arr) => !isNaN(parseInt(el)) && arr.indexOf(el) === i)
+					.sort()
+					.filter((el, i, arr) => i === 0 || i === arr.length - 1)
+					.join('-');
+			}
+			return { count, years };
+		}
+		return null;
+	}
 </script>
 
-<li class="flex gap-8 rounded-md border-b border-b-primary/16 bg-cards p-6" data-testid="search-card">
+<li
+	class="flex gap-8 rounded-md border-b border-b-primary/16 bg-cards p-6"
+	data-testid="search-card"
+>
 	<div class="flex h-[6.5rem] w-20 shrink-0 items-center justify-center rounded-sm bg-[lightgrey]">
 		Image
 	</div>
@@ -23,7 +51,14 @@
 			{#each item['card-body']?._display as obj}
 				<div class="rounded-md bg-pill/4 p-2">
 					{#if 'hasInstance' in obj}
-						<span>{obj.hasInstance.length ? `${obj.hasInstance.length} utgåvor` : `1 utgåva`}</span>
+						{@const instances = getInstanceData(obj.hasInstance)}
+						{#if instances}
+							<span>
+								{instances?.count}
+								{instances.count > 1 ? 'utgåvor' : 'utgåva'}
+								{instances?.years && `(${instances.years})`}
+							</span>
+						{/if}
 					{:else}
 						<DecoratedData data={obj} showLabels={ShowLabelsOptions.Never} block truncate />
 					{/if}
