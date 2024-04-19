212ca153dc024fbd76430b447a97bb573af87536
Jesper Engström <jesper.engstrom@kb.se>
2024-04-19 11:42:48 +0200

    cache response when redirecting

--- lxl-web/src/routes/(app)/[[lang=lang]]/(find)/+layout.server.ts
+++ lxl-web/src/routes/(app)/[[lang=lang]]/(find)/+layout.server.ts
@@ -12,7 +12,10 @@ import { type apiError } from '$lib/types/API.js';
 import { asResult, type PartialCollectionView, type SearchResult } from './search.js';
 import getAtPath from '$lib/utils/getAtPath';
 
+let cachedSearchResult: null | PartialCollectionView = null;
+
 export const load = async ({ params, url, locals, fetch, isDataRequest }) => {
+	console.log('has cache', !!cachedSearchResult);
 	const displayUtil: DisplayUtil = locals.display;
 	const locale = getSupportedLocale(params?.lang);
 
@@ -68,21 +71,46 @@ export const load = async ({ params, url, locals, fetch, isDataRequest }) => {
 			redirect(303, `/`); // redirect to home page if no search params are given
 		}
 
-		let searchParams = new URLSearchParams(url.searchParams.toString());
+		let result;
 
-		if (shouldFindRelations && resourceId) {
-			searchParams.set('_o', resourceId);
-			searchParams = getSortedSearchParams(addDefaultSearchParams(searchParams));
+		if (cachedSearchResult && cachedSearchResult['@id'] === `/find${url.search}`) {
+			// we have a cached result with matching query
+			console.log('picking up from cache');
+			result = cachedSearchResult;
+			cachedSearchResult = null;
+		} else {
+			let searchParams = new URLSearchParams(url.searchParams.toString());
+
+			if (shouldFindRelations && resourceId) {
+				searchParams.set('_o', resourceId);
+				searchParams = getSortedSearchParams(addDefaultSearchParams(searchParams));
+			}
+
+			console.log('fetching', searchParams.toString());
+			const recordsRes = await fetch(`${env.API_URL}/find.jsonld?${searchParams.toString()}`);
+
+			if (!recordsRes.ok) {
+				const err = (await recordsRes.json()) as apiError;
+				throw error(err.status_code, err.status);
+			}
+
+			result = (await recordsRes.json()) as PartialCollectionView;
 		}
 
-		const recordsRes = await fetch(`${env.API_URL}/find.jsonld?${searchParams.toString()}`);
+		console.log(
+			'matching',
+			result['@id'],
+			`/find${url.search}`,
+			result['@id'] === `/find${url.search}`
+		);
 
-		if (!recordsRes.ok) {
-			const err = (await recordsRes.json()) as apiError;
-			throw error(err.status_code, err.status);
+		if (isFindRoute && `/find${url.search}` !== result['@id']) {
+			// recieved a redirected api response
+			// redirect and cache response for next load
+			cachedSearchResult = result;
+			console.log('redirecting');
+			redirect(308, result['@id']);
 		}
-
-		const result = (await recordsRes.json()) as PartialCollectionView;
 
 		// Hide zero results from resource page
 		if (result.totalItems > 0 || isFindRoute) {
