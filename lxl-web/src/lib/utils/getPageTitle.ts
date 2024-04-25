15d6ff35034d591114e1e6cf566ba7d4fc8093bc
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-04-25 09:19:50 +0200

    Add note about future site config

--- lxl-web/src/lib/utils/getPageTitle.ts
+++ lxl-web/src/lib/utils/getPageTitle.ts
@@ -1,6 +1,9 @@
 /**
  * Adds title separator and suffix to title string
+ *
+ * Future: the suffix should be set in the site config
  */
+
 function getPageTitle(title?: string) {
 	if (title) {
 		return `${title} | Libris`;
