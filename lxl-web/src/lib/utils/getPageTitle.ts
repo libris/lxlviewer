2f7e68e42a764dc3a930fdb855a1220c31f264d2
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-04-25 10:16:14 +0200

    Truncate title if total length is greater than max length

--- lxl-web/src/lib/utils/getPageTitle.ts
+++ lxl-web/src/lib/utils/getPageTitle.ts
@@ -1,14 +1,23 @@
+import { TITLE_SEPARATOR, TITLE_SUFFIX, TITLE_MAX_LENGTH } from '$lib/constants/pageTitle';
+
 /**
  * Adds title separator and suffix to title string
  *
- * Future: the suffix should be set in the site config
+ * Future: the constants should be set in a site config file / environment variables
  */
 
 function getPageTitle(title?: string) {
 	if (title) {
-		return `${title} | Libris`;
+		const separatorAndSuffix = TITLE_SEPARATOR + TITLE_SUFFIX;
+
+		// Truncate the title and add an ellipsis if the total length is greater than the max length
+		if (title.length > TITLE_MAX_LENGTH - separatorAndSuffix.length) {
+			return `${title.substring(0, TITLE_MAX_LENGTH - separatorAndSuffix.length - 1) + '…'}${separatorAndSuffix}`;
+		}
+
+		return `${title}${separatorAndSuffix}`;
 	}
-	return 'Libris';
+	return TITLE_SUFFIX;
 }
 
 export default getPageTitle;
