decc1215f8d9cee8e9e95117a456e563b93325fe
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-05-06 12:42:49 +0200

    Update copy

--- lxl-web/src/lib/components/Error.svelte
+++ lxl-web/src/lib/components/Error.svelte
@@ -1,32 +1,37 @@
 <script>
 	import { page } from '$app/stores';
 	import { getSupportedLocale } from '$lib/i18n/locales';
-
 	export let showLogo = false;
 
 	const locale = getSupportedLocale($page.url.pathname.split('/')[1]);
 
 	/** Currently using hard-coded localized strings here as we cannot access the translator/t function in $page.data from the root error page)... there probably is a much cleaner way to do this? */
-	const localizedErrors = {
+	const errorMessages = {
 		sv: {
-			notFound: 'Sidan kunde inte hittas',
-			faultyLink: 'Klickade du på en länk i Libris som inte fungerade?',
+			somethingWentWrong: 'Något gick fel',
+			notFound: 'Sidan hittades inte',
+			wrongLink: 'Klickade du på en länk i Libris som inte fungerade?',
 			sendEmail: 'Skicka e-post till',
 			customerService: 'Libris kundservice',
 			followUp: ', så undersöker vi felet.',
 			backToStartPage: 'Tillbaka till startsidan',
-			mailSubject: 'Felaktig länk'
+			mailSubject: 'Felaktig länk',
+			mailBody: 'Felaktig referens till sidan'
 		},
 		en: {
-			notFound: 'The page could not be found',
-			faultyLink: "Did you click on a link that didn't work?",
+			somethingWentWrong: 'Somethingh went wrong',
+			notFound: 'Page not found',
+			wrongLink: "Did you click on a link that didn't work?",
 			sendEmail: 'Send an e-mail to',
 			customerService: 'Libris customer service',
 			followUp: ', and we will investigate the error.',
 			backToStartPage: 'Back to the home page',
-			mailSubject: 'Incorrect link'
+			mailSubject: 'Incorrect link',
+			mailBody: 'Incorrect reference to the page'
 		}
 	};
+
+	const localizedErrors = errorMessages[locale];
 </script>
 
 <div class="m-auto flex flex-col p-8 text-center">
@@ -39,19 +44,21 @@
 	{/if}
 	<h1 class="text-6-cond-extrabold">{$page.status}</h1>
 	{#if $page.status === 404}
-		<h2 class="pb-4">{localizedErrors[locale].notFound}</h2>
-		<p>{localizedErrors[locale].faultyLink}</p>
+		<h2 class="pb-4">{localizedErrors.notFound}</h2>
+		<p>{localizedErrors.wrongLink}</p>
 		<p>
-			{localizedErrors[locale].sendEmail}
+			{localizedErrors.sendEmail}
 			<a
-				href="mailto:libris@kb.se?subject=Felaktig länk&body=Felaktig referens till sidan {$page.url
-					.href}">{localizedErrors[locale].customerService}</a
-			>{localizedErrors[locale].followUp}
+				href="mailto:libris@kb.se?subject={encodeURIComponent(
+					localizedErrors.mailSubject
+				)}&body={encodeURIComponent(localizedErrors.mailBody)} {$page.url.href}"
+				>{localizedErrors.customerService}</a
+			>{localizedErrors.followUp}
 		</p>
 		<p class="pt-4">
-			<a href={locale === 'en' ? '/en' : '/'}>{localizedErrors[locale].backToStartPage}</a>
+			<a href={locale === 'en' ? '/en' : '/'}>{localizedErrors.backToStartPage}</a>
 		</p>
-	{:else}
-		<p class="text-secondary">{$page.error?.message}</p>
+	{:else if $page.error?.message}
+		<p class="text-secondary">{$page.error.message}</p>
 	{/if}
 </div>
