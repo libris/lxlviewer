b2002cf15e98a9b6493a13f7d4f8bcb457e75f07
Jesper Engström <jesper.engstrom@kb.se>
2024-05-16 16:59:51 +0200

    Fix beta badge

--- lxl-web/src/routes/(app)/[[lang=lang]]/SiteHeader.svelte
+++ lxl-web/src/routes/(app)/[[lang=lang]]/SiteHeader.svelte
@@ -21,14 +21,14 @@
 >
 	{#if !isLandingPage}
 		<div class="flex items-center">
-			<a
-				href={$page.data.base}
-				class="text-[1.4rem] font-extrabold leading-tight text-primary no-underline sm:text-[1.6rem] md:text-[2.1rem]"
-				>Libris
+			<a href={$page.data.base} class="flex flex-col text-primary no-underline sm:flex-row">
+				<span class="text-[1.4rem] font-extrabold leading-tight sm:text-[1.6rem] md:text-[2.1rem]">
+					Libris</span
+				>
 				<sup
-				class="sr-only self-baseline rounded-sm bg-positive-dark/16 uppercase text-2-cond-bold sm:not-sr-only sm:ml-1 sm:px-2"
-				>Beta</sup
-			>
+					class="top-0 -rotate-6 self-baseline rounded-sm bg-positive-dark/16 px-2 uppercase text-2-cond-bold sm:rotate-0"
+					>Beta</sup
+				>
 			</a>
 		</div>
 	{/if}
@@ -57,12 +57,13 @@
 			{/if}
 		</div>
 		{#if isLandingPage}
-			<div class="flex w-full flex-col items-center px-12 pb-[4.5rem] pt-6">
+			<div class="flex w-full flex-col items-center gap-2 px-12 pb-[4.5rem] pt-6">
 				<h1 class="flex text-3xl font-extrabold text-primary sm:text-[5.5rem] sm:font-bold">
 					Libris
 					<sup
-					class="self-center rounded-sm bg-positive-dark/16 px-2 uppercase text-2-cond-bold sm:text-3-cond-bold"
-					>Beta</sup>
+						class="self-center rounded-sm bg-positive-dark/16 px-2 uppercase text-2-cond-bold sm:text-3-cond-bold"
+						>Beta</sup
+					>
 				</h1>
 				<div class="w-full max-w-3xl">
 					<Search placeholder={$page.data.t('home.searchPlaceholder')} autofocus />
