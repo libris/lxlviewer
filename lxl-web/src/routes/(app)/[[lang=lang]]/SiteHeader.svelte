963521cff8f0c3faf8cc5ea1f352736834f98163
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-05-16 13:01:05 +0200

    Adjust padding again

--- lxl-web/src/routes/(app)/[[lang=lang]]/SiteHeader.svelte
+++ lxl-web/src/routes/(app)/[[lang=lang]]/SiteHeader.svelte
@@ -15,7 +15,7 @@
 </script>
 
 <header
-	class="flex gap-4 bg-site-header p-4 sm:py-6 md:grid md:gap-12"
+	class="flex gap-4 bg-site-header p-4 sm:p-6 md:grid md:gap-12"
 	class:md:grid-cols-find={!isLandingPage}
 >
 	{#if !isLandingPage}
