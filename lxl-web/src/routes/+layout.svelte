71af23e5c52b00bb35db8f2d12e38d4f736180c1
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-05-21 15:49:27 +0200

    Use Inter font

--- lxl-web/src/routes/+layout.svelte
+++ lxl-web/src/routes/+layout.svelte
@@ -2,6 +2,15 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<link
+		rel="preload"
+		as="font"
+		href="/fonts/InterVariable.woff2"
+		type="font/woff2"
+		crossorigin="anonymous"
+	/>
+</svelte:head>
 <header>
 	<nav>
 		<a class="home" href="/">Libris</a>
