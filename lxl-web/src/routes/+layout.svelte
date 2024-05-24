dbba72cbb3d58b85d4472d3f7693c7174226abf6
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-05-24 09:43:01 +0200

    Add SiteHeader component

--- lxl-web/src/routes/+layout.svelte
+++ lxl-web/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import '../app.css';
+	import SiteHeader from './SiteHeader.svelte';
 </script>
 
 <svelte:head>
@@ -11,66 +12,7 @@
 		crossorigin="anonymous"
 	/>
 </svelte:head>
-<header>
-	<nav>
-		<a class="home" href="/">Libris</a>
-		<form class="search" action="find">
-			<input type="search" placeholder="Sök på titel, författare, ämne" />
-		</form>
-		<ul class="actions">
-			<li><a href="/">A</a></li>
-			<li><a href="/">B</a></li>
-			<li><a href="/">C</a></li>
-		</ul>
-	</nav>
-</header>
+<SiteHeader />
 <main>
 	<slot />
 </main>
-
-<style>
-	header nav {
-		display: grid;
-		grid-template-areas:
-			'home actions'
-			'search search';
-		align-items: center;
-		padding: var(--padding-page);
-
-		@media screen and (min-width: 640px) {
-			grid-template-columns: 1fr 2fr 1fr;
-			grid-template-areas: 'home search actions';
-		}
-	}
-
-	.home {
-		grid-area: home;
-		color: inherit;
-		text-decoration: none;
-	}
-
-	.search {
-		grid-area: search;
-
-		& > input {
-			width: 100%;
-			min-height: var(--height-input);
-		}
-	}
-
-	.actions {
-		display: flex;
-		grid-area: actions;
-		justify-content: flex-end;
-		align-items: center;
-		margin: 0;
-
-		& a {
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			width: var(--height-input);
-			height: var(--height-input);
-		}
-	}
-</style>
