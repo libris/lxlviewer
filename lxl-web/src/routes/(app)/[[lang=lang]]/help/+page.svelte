b9719a54af8dfd1a3dc3f6b2ec0d6c9c204e1ace
Johan Bisse Mattsson <johan@johanbissemattsson.se>
2024-05-27 10:23:11 +0200

    LWS-117: Add help page (#1035)
    * Add help page
    
    * Use article element
    
    * Use article on frontpage too
    
    * Get help content using MDsveX/markdown
    
    * Add MarkdownContent component with layout wrapping mdsvex document
    
    * Update styling
    
    ---------
    
    Co-authored-by: Jesper Engström <jesper.engstrom@kb.se>

---
+++ lxl-web/src/routes/(app)/[[lang=lang]]/help/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import getPageTitle from '$lib/utils/getPageTitle';
+	import { page } from '$app/stores';
+	import SvContent from './sv.md';
+	import EnContent from './en.md';
+
+	export let data;
+</script>
+
+<svelte:head>
+	<title>{getPageTitle($page.data.t('header.help'))}</title>
+</svelte:head>
+
+<article class="container-fluid mb-12 mt-8 max-w-3xl page-padding">
+	{#if data.locale === 'en'}
+		<EnContent />
+	{:else}
+		<SvContent />
+	{/if}
+</article>
--- lxl-web/svelte.config.js
+++ lxl-web/svelte.config.js
@@ -1,13 +1,16 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
 import trimSiblingWhitespaces from './src/lib/preprocessors/trimSiblingWhitespaces.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess(), trimSiblingWhitespaces({ filenames: ['DecoratedData.svelte'] })],
-
+	extensions: ['.svelte', '.md'],
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({ extensions: ['.md'], layout: '/src/lib/components/MarkdownContent.svelte' }),
+		trimSiblingWhitespaces({ filenames: ['DecoratedData.svelte'] })
+	],
 	kit: {
 		adapter: adapter(),
 		paths: {
