207aeac9b92aa98a6eeb7cda6d9d72325640b924
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-05-27 10:30:00 +0200

    Add container query for search card

--- lxl-web/src/routes/(app)/[[lang=lang]]/(find)/SearchCard.svelte
+++ lxl-web/src/routes/(app)/[[lang=lang]]/(find)/SearchCard.svelte
@@ -8,35 +8,55 @@
 	$: footerId = `card-footer-${item['@id']}`;
 </script>
 
-<article class="search-card">
-	<!-- svelte-ignore a11y-missing-content -->
-	<!-- (content shouldn't be needed as we're using aria-labelledby, see: https://github.com/sveltejs/svelte/issues/8296) -->
-	<a class="card-link" href="/" aria-labelledby={titleId} aria-describedby={`${bodyId} ${footerId}`}
-	></a>
-	<div class="card-image"></div>
-	<header class="card-header" id={titleId}>
-		<hgroup>
-			<h1 class="text-4-cond-bold">title</h1>
-			<p>transliteration</p>
-		</hgroup>
-		<p><a href="/test">original title</a></p>
-	</header>
-	<div class="card-body" id={bodyId}>body</div>
-	<footer class="card-footer" id={footerId}>footer</footer>
-</article>
+<div class="search-card-container">
+	<article class="search-card">
+		<!-- svelte-ignore a11y-missing-content -->
+		<!-- (content shouldn't be needed as we're using aria-labelledby, see: https://github.com/sveltejs/svelte/issues/8296) -->
+		<a
+			class="card-link"
+			href="/"
+			aria-labelledby={titleId}
+			aria-describedby={`${bodyId} ${footerId}`}
+		></a>
+		<div class="card-image"></div>
+		<header class="card-header" id={titleId}>
+			<hgroup>
+				<h1 class="text-4-cond-bold">title</h1>
+				<p>transliteration</p>
+			</hgroup>
+			<p><a href="/test">original title</a></p>
+		</header>
+		<div class="card-body" id={bodyId}>body</div>
+		<footer class="card-footer" id={footerId}>footer</footer>
+	</article>
+</div>
 
 <style lang="postcss">
+	.search-card-container {
+		container-type: inline-size;
+	}
+
 	.search-card {
+		@apply border-b border-b-primary/16;
+
 		display: grid;
+		width: 100%;
 		position: relative;
+		background: theme(backgroundColor.cards);
+		border-radius: theme(borderRadius.md);
+		padding: theme(padding.4);
 		grid-template-areas:
 			'image header'
 			'image body'
 			'image footer';
-		grid-template-columns: 128px 1fr;
+		grid-template-columns: 64px 1fr;
 
 		&:has(a:hover) {
 			@apply shadow-xl;
+		}
+
+		@container (min-width: theme(screens.sm)) {
+			grid-template-columns: 128px 1fr;
 		}
 	}
 
