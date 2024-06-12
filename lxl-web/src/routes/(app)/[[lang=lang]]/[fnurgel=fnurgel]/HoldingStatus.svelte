02d96773417b9df955e10b65ab541c0e243303cb
Jesper Engström <jesper.engstrom@kb.se>
2024-06-12 11:43:48 +0200

    Create component

---
+++ lxl-web/src/routes/(app)/[[lang=lang]]/[fnurgel=fnurgel]/HoldingStatus.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	// import { onMount } from "svelte";
+
+	// export let data;
+
+	// onMount(() => {
+	//   console.log('mounted!', data)
+	// })
+
+	// async function getHolddingStatus(){
+	//   console.log(data);
+	// }
+</script>
+
+<slot />
+<div>Hello</div>
