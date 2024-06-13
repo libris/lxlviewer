6fa59bc1a7a6b85812b736a4de78303963290564
Jesper Engström <jesper.engstrom@kb.se>
2024-06-13 12:25:50 +0200

    Get holding status working

--- lxl-web/src/lib/types/api.ts
+++ lxl-web/src/lib/types/api.ts
@@ -5,3 +5,25 @@ export type ApiError = {
 	status_code: NumericRange<400, 599>;
 	status: string;
 };
+
+export interface HoldingStatus {
+	item_information: ItemInformation;
+}
+
+interface ItemInformation {
+	library_code: string;
+	count: number;
+	error?: string;
+	items: HoldingItem[] | [];
+}
+
+interface HoldingItem {
+	Item_No: string;
+	UniqueItemId: string;
+	Location: string;
+	Call_No: string;
+	Status: string;
+	Status_Date_Description: string;
+	Status_Date: string;
+	Loan_Policy: string;
+}
