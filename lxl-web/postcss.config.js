b0f1c892f6e4c3f523e2c939b1e3a3d4947f3b26
Johan Bisse Mattsson <johan@johanbissemattsson.se>
2024-06-18 09:38:47 +0200

    LWS-207: Bump dependencies (#1066)
    * Bump @playwright/test and @axe-core/playwright and
    
    * Bump @floating-ui/dom
    
    * Bump svelte and sveltekit related dependencies
    
    * Bump tailwindcss
    
    * Bump prettier, prettier-plugin-svelte and prettier-plugin-tailwindcss
    
    * Bump mdsvex
    
    * Bump prettier-plugin-tailwindcss to latest
    
    * Bump postcss and postcss-import
    
    * Bump autoprefixer
    
    * Bump cssnano
    
    * Bump husky and lint-staged
    
    * Bump unplugin-icons
    
    * Bump typescript and tslib
    
    * Bump vitest
    
    * Bump vite
    
    * Bump and migrate to ESLint 9
    
    Following how things are setup when creating a new Sveltekit app using `npm create svelte@latest`

--- lxl-web/postcss.config.js
+++ lxl-web/postcss.config.js
@@ -4,5 +4,6 @@ export default {
 		'tailwindcss/nesting': {},
 		tailwindcss: {},
 		autoprefixer: {}
+		// ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}) // we have currently disabled cssnano as it doesn't play along with container queries. This is however fixed in Svelte 5 (see: https://github.com/sveltejs/svelte/issues/10369#issuecomment-1983169720)
 	}
 };
