08473983fe0ae0c9ea673649c5b96814e34058fe
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-06-25 11:32:30 +0200

    Move fonts to lib folder

--- lxl-web/src/routes/+layout.svelte
+++ lxl-web/src/routes/+layout.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
-	import '../app.css';
+	import '$lib/styles/app.css';
 	import SiteHeader from './SiteHeader.svelte';
+	import InterVariable from '$lib/fonts/InterVariable.woff2';
 </script>
 
 <svelte:head>
-	<link
-		rel="preload"
-		as="font"
-		href="/fonts/InterVariable.woff2"
-		type="font/woff2"
-		crossorigin="anonymous"
-	/>
+	<link rel="preload" as="font" href={InterVariable} type="font/woff2" crossorigin="anonymous" />
 </svelte:head>
-<SiteHeader />
-<main>
-	<slot />
-</main>
+<div class="app">
+	<SiteHeader />
+	<main>
+		<slot />
+	</main>
+</div>
+
+<style>
+	.app {
+		display: grid;
+		grid-template-rows: min-content 1fr;
+		min-height: 100vh;
+	}
+</style>
