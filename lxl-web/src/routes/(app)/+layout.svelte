28bd5214e09f644a1db3eb0a98b0bfc6f727cf25
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-07-02 14:52:48 +0200

    Add app group layout again

---
+++ lxl-web/src/routes/(app)/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import AppHeader from './AppHeader.svelte';
+	const { children } = $props();
+</script>
+
+<div class="app">
+	<AppHeader />
+	<main>
+		{@render children()}
+	</main>
+</div>
+
+<style>
+	.app {
+		display: grid;
+		grid-template-rows: min-content 1fr;
+		min-height: 100vh;
+	}
+</style>
--- lxl-web/src/routes/+layout.svelte
+++ lxl-web/src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import '$lib/styles/app.css';
-	import AppHeader from './AppHeader.svelte';
+	import './styles.css';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import { i18n } from '$lib/i18n';
 
@@ -8,18 +7,5 @@
 </script>
 
 <ParaglideJS {i18n}>
-	<div class="app">
-		<AppHeader />
-		<main>
-			{@render children()}
-		</main>
-	</div>
+	{@render children()}
 </ParaglideJS>
-
-<style>
-	.app {
-		display: grid;
-		grid-template-rows: min-content 1fr;
-		min-height: 100vh;
-	}
-</style>
