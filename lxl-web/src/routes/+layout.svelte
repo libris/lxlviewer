e498dd126743a2e15f5d925615911a26830ae1c4
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-07-02 14:45:04 +0200

    Migrate layout file to Svelte 5

--- lxl-web/src/routes/+layout.svelte
+++ lxl-web/src/routes/+layout.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
 	import '$lib/styles/app.css';
 	import AppHeader from './AppHeader.svelte';
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import { i18n } from '$lib/i18n';
+
+	const { children } = $props();
 </script>
 
-<div class="app">
-	<AppHeader />
-	<main>
-		<slot />
-	</main>
-</div>
+<ParaglideJS {i18n}>
+	<div class="app">
+		<AppHeader />
+		<main>
+			{@render children()}
+		</main>
+	</div>
+</ParaglideJS>
 
 <style>
 	.app {
