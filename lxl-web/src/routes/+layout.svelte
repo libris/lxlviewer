18c616f083c50d2fc079d31050f04b252c91b8da
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-07-09 12:00:31 +0200

    Add page title to root layout file

--- lxl-web/src/routes/+layout.svelte
+++ lxl-web/src/routes/+layout.svelte
@@ -3,11 +3,13 @@
 	import InterVariable from '$lib/fonts/InterVariable.woff2';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import { i18n } from '$lib/i18n';
+	import getPageTitle from '$lib/utils/getPageTitle';
 
 	const { children } = $props();
 </script>
 
 <svelte:head>
+	<title>{getPageTitle()}</title>
 	<link rel="preload" href={InterVariable} as="font" type="font/woff2" crossorigin="anonymous" />
 </svelte:head>
 <ParaglideJS {i18n}>
