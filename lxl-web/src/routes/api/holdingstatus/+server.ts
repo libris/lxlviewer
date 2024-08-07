037241301b5254d8618ce3697c9736774e4abbdd
Jesper Engström <jesper.engstrom@kb.se>
2024-08-07 15:03:24 +0200

    Simplify api GET method

--- lxl-web/src/routes/api/holdingstatus/+server.ts
+++ lxl-web/src/routes/api/holdingstatus/+server.ts
@@ -1,10 +1,6 @@
-import { json } from '@sveltejs/kit';
 import { env } from '$env/dynamic/private';
-import type { HoldingStatus } from '$lib/types/api';
 
 export async function GET({ url }) {
 	const res = await fetch(`${env.HOLDING_STATUS_URL}?${url.searchParams.toString()}&output=json`);
-	const data = (await res.json()) as HoldingStatus;
-
-	return json(data);
+	return res;
 }
