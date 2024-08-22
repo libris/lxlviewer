f6af9d0e8a0c9a2c92824770abab89702c9c6aba
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-08-22 15:41:07 +0200

    Add suggestion types

---
+++ lxl-web/src/lib/types/suggestions.ts
@@ -0,0 +1,12 @@
+export type PropertySuggestion = {
+	'@id': string;
+	key: string;
+	labelByLang: { [key: string]: string };
+};
+
+export type PartSuggestion = {
+	'@id': string;
+	'@type': string;
+	labelByLang: { [key: string]: string };
+	descriptionByLang: { [key: string]: string };
+} & ({ keyByLang: { key: string } } | { [key: string]: string });
