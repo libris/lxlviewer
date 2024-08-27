c620663d208a1b813527578732ccd29efe3bc83e
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-08-27 10:05:51 +0200

    Update util comments

--- lxl-web/src/lib/utils/codemirror/getEditedRange.ts
+++ lxl-web/src/lib/utils/codemirror/getEditedRange.ts
@@ -2,13 +2,19 @@ import type { EditorState } from '@codemirror/state';
 import getMainSelection from './getMainSelection';
 
 /**
- * Gets the edited range from the editorState
- * Note that we cannot use the [wordAt](https://codemirror.net/docs/ref/#state.EditorState.wordAt) as we want to match
- * all characters except whitespaces (and not only word characters as in `wordAt`)
+ * Gets the edited range from the editor state
+ * (including the characters surrounding the main selection up until the closest whitespaces)
  */
+
 function getEditedRange(editorState: EditorState) {
+	/**
+	 * Note that we cannot use [wordAt](https://codemirror.net/docs/ref/#state.EditorState.wordAt) as we
+	 * want to match all characters except whitespaces (and not only word characters as in `wordAt`)
+	 */
+
 	const mainSelection = getMainSelection(editorState);
-	const doc = editorState.doc.toString(); // we cannot use editorState.wordAt(mainSelection.anchor) as we want to match all characters except whitespaces (not only word characters as in wordAt)
+
+	const doc = editorState.doc.toString();
 
 	return {
 		from: doc.lastIndexOf(doc.slice(0, mainSelection.anchor).split(/\s+/).pop()!),
--- lxl-web/src/lib/utils/codemirror/getMainSelection.ts
+++ lxl-web/src/lib/utils/codemirror/getMainSelection.ts
@@ -1,6 +1,10 @@
 import type { EditorState } from '@codemirror/state';
 import type { Selection } from '$lib/components/CodeMirror.svelte';
 
+/**
+ * Gets the main selection from the editor state
+ */
+
 function getMainSelection(editorState: EditorState): Selection {
 	const mainSelection = editorState.selection.main;
 	return { anchor: mainSelection.anchor, head: mainSelection.head };
