306150b23b7b3af38856c2de722f41511571f146
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-09-05 12:45:18 +0200

    Add ranges and add tests

---
+++ lxl-web/src/lib/utils/codemirror/getEditedParts.test.ts
@@ -0,0 +1,91 @@
+import { describe, it, expect } from 'vitest';
+import getEditedParts from './getEditedParts';
+
+describe('getEditedParts', () => {
+	it('returns edited word', () => {
+		expect(getEditedParts({ q: 'astrid', cursor: 6 })).toEqual({
+			word: 'astrid',
+			wordRange: { from: 0, to: 6 },
+			phrase: null,
+			phraseRange: null,
+			qualifierLikeName: null,
+			qualifierLikeValue: null
+		});
+		expect(getEditedParts({ q: 'astrid', cursor: 3 })).toEqual({
+			word: 'astrid',
+			wordRange: { from: 0, to: 6 },
+			phrase: null,
+			phraseRange: null,
+			qualifierLikeName: null,
+			qualifierLikeValue: null
+		});
+	});
+	it('returns edited word and phrase if two or more words', () => {
+		expect(getEditedParts({ q: 'astrid lindgren', cursor: 15 })).toEqual({
+			word: 'lindgren',
+			wordRange: { from: 7, to: 15 },
+			phrase: 'astrid lindgren',
+			phraseRange: { from: 0, to: 15 },
+			qualifierLikeName: null,
+			qualifierLikeValue: null
+		});
+		expect(getEditedParts({ q: 'astrid lindgren', cursor: 6 })).toEqual({
+			word: 'astrid',
+			wordRange: { from: 0, to: 6 },
+			phrase: 'astrid lindgren',
+			phraseRange: { from: 0, to: 15 },
+			qualifierLikeName: null,
+			qualifierLikeValue: null
+		});
+	});
+	it('returns matched qualifier-like names and values', () => {
+		expect(getEditedParts({ q: 'astrid lindgren hasTitle:', cursor: 25 })).toEqual({
+			word: 'hasTitle:',
+			wordRange: { from: 16, to: 25 },
+			phrase: null,
+			phraseRange: null,
+			qualifierLikeName: 'hasTitle',
+			qualifierLikeValue: null
+		});
+		expect(getEditedParts({ q: 'astrid lindgren hasTitle:Pippi', cursor: 30 })).toEqual({
+			word: 'hasTitle:Pippi',
+			wordRange: { from: 16, to: 30 },
+			phrase: null,
+			phraseRange: null,
+			qualifierLikeName: 'hasTitle',
+			qualifierLikeValue: 'Pippi'
+		});
+	});
+	it('works with quoted qualifiers', () => {
+		expect(getEditedParts({ q: 'astrid lindgren "rdf:type":Text', cursor: 23 })).toEqual({
+			word: '"rdf:type":Text',
+			wordRange: { from: 16, to: 31 },
+			phrase: null,
+			phraseRange: null,
+			qualifierLikeName: '"rdf:type"',
+			qualifierLikeValue: 'Text'
+		});
+		expect(
+			getEditedParts({ q: 'astrid lindgren "rdf:type":Text itemHeldBy:"sigel:S"', cursor: 50 })
+		).toEqual({
+			word: 'itemHeldBy:"sigel:S"',
+			wordRange: { from: 32, to: 52 },
+			phrase: null,
+			phraseRange: null,
+			qualifierLikeName: 'itemHeldBy',
+			qualifierLikeValue: '"sigel:S"'
+		});
+	});
+	it('gets relevant part between qualfiers', () => {
+		expect(
+			getEditedParts({ q: 'YEAR:2002 astrid lind "rdf:type":Text lejonhjärta', cursor: 21 })
+		).toEqual({
+			word: 'lind',
+			wordRange: { from: 17, to: 21 },
+			phrase: 'astrid lind',
+			phraseRange: { from: 10, to: 21 },
+			qualifierLikeName: null,
+			qualifierLikeValue: null
+		});
+	});
+});
--- lxl-web/src/lib/utils/codemirror/getEditedParts.ts
+++ lxl-web/src/lib/utils/codemirror/getEditedParts.ts
@@ -1,5 +1,7 @@
+import type { EditedRange } from '$lib/components/CodeMirror.svelte';
+
 const QUALIFIER_REGEX = RegExp(/((")?[0-9a-zA-ZaåöAÅÖ:]+\2):((")?[0-9a-zA-ZaåöAÅÖ:]+\4?)?/);
-const PHRASE_REGEX = RegExp(/(")?[0-9a-zA-ZaåöAÅÖ:]+\1:(")?[0-9a-zA-ZaåöAÅÖ:]+\2?\s?|$/);
+const PHRASE_REGEX = RegExp(/(^|\s)((")?[0-9a-zA-ZaåöAÅÖ:]+\3:(")?[0-9a-zA-ZaåöAÅÖ:]+\4?)(\s?|$)/);
 
 /**
  * Gets the edited parts of a query the cursor position.
@@ -7,14 +9,17 @@ const PHRASE_REGEX = RegExp(/(")?[0-9a-zA-ZaåöAÅÖ:]+\1:(")?[0-9a-zA-ZaåöA�
 
 function getEditedParts({ q, cursor }: { q: string; cursor: number }): {
 	word: string; // a single word/string (e.g. Astrid)
-	phrase?: string; // a group of words/strings (e.g. Astrid Lindgren)
-	qualifierLikeName?: string; // a string that could be a valid qualifier name
-	qualifierLikeValue?: string; // a string that could be the value of a valid qualifier
+	wordRange: EditedRange;
+	phrase: string | null; // a group of words/strings (e.g. Astrid Lindgren)
+	phraseRange: EditedRange | null;
+	qualifierLikeName: string | null; // a string that could be a valid qualifier name
+	qualifierLikeValue: string | null; // a string that could be the value of a valid qualifier
 } {
 	const wordFromIndex = q.lastIndexOf(q.slice(0, cursor).split(/\s+/).pop()!);
 	const wordToIndex = cursor + q.slice(cursor).split(/\s+/)[0].length || 0;
-
 	const word = q.slice(wordFromIndex, wordToIndex);
+	const wordRange = { from: wordFromIndex, to: wordToIndex };
+
 	const qualifierMatch = word.match(QUALIFIER_REGEX);
 
 	if (qualifierMatch) {
@@ -23,16 +28,28 @@ function getEditedParts({ q, cursor }: { q: string; cursor: number }): {
 
 		return {
 			word,
+			wordRange,
+			phrase: null,
+			phraseRange: null,
 			qualifierLikeName: name,
-			qualifierLikeValue: value || ''
+			qualifierLikeValue: value || null
 		};
 	} else {
 		const phraseBefore = q.slice(0, cursor).split(PHRASE_REGEX).pop() || ''; // get last string before cursor which isn't a qualifier
 		const phraseAfter = q.slice(cursor).split(PHRASE_REGEX)?.[0] || ''; // get first string after cursor which isn't a qualifier
 
+		const phrase = phraseBefore + phraseAfter;
+		const phraseFrom = q.slice(0, cursor).lastIndexOf(phraseBefore);
+		const phraseTo = phraseFrom + phrase.length;
+		const phraseRange = { from: phraseFrom, to: phraseTo };
+
 		return {
 			word,
-			phrase: phraseBefore + phraseAfter
+			wordRange,
+			phrase: (phrase !== word && phrase) || null,
+			phraseRange: (phrase !== word && phraseRange) || null,
+			qualifierLikeName: null,
+			qualifierLikeValue: null
 		};
 	}
 }
