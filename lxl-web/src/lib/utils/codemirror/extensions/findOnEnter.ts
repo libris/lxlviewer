59b9816da7554456d66045b70b5d974ef0d2a471
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-10-02 14:51:46 +0200

    Find on shift-enter

--- lxl-web/src/lib/utils/codemirror/extensions/findOnEnter.ts
+++ lxl-web/src/lib/utils/codemirror/extensions/findOnEnter.ts
@@ -1,5 +1,5 @@
 import { Prec } from '@codemirror/state';
-import { keymap } from '@codemirror/view';
+import { EditorView, keymap } from '@codemirror/view';
 import { goto } from '$app/navigation';
 import addDefaultSearchParams from '$lib/utils/addDefaultSearchParams';
 
@@ -7,28 +7,34 @@ import addDefaultSearchParams from '$lib/utils/addDefaultSearchParams';
  * CodeMirror extension which submits the closest form element on enter keypresses
  */
 
+function find(editorView: EditorView) {
+	const closestForm = editorView.dom?.closest('form');
+	if (closestForm) {
+		const _q = new FormData(closestForm).get('_q')?.toString();
+		if (_q) {
+			goto(
+				'/find?' +
+					addDefaultSearchParams(
+						new URLSearchParams({
+							_q
+						})
+					).toString()
+			);
+		}
+		return true; // return true to prevent further to be tried
+	}
+	return false;
+}
+
 const findOnEnter = Prec.highest(
 	keymap.of([
 		{
 			key: 'Enter',
-			run: (editorView) => {
-				const closestForm = editorView.dom?.closest('form');
-				if (closestForm) {
-					const _q = new FormData(closestForm).get('_q')?.toString();
-					if (_q) {
-						goto(
-							'/find?' +
-								addDefaultSearchParams(
-									new URLSearchParams({
-										_q
-									})
-								).toString()
-						);
-					}
-					return true; // return true to prevent further to be tried
-				}
-				return false;
-			}
+			run: find
+		},
+		{
+			key: 'Shift-Enter',
+			run: find
 		}
 	])
 );
