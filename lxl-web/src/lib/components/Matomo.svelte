d9c935603b40062fc7af29e0777e00a42e6e3d14
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-10-15 14:54:11 +0200

    Move writable to setMatomoTracker

--- lxl-web/src/lib/components/Matomo.svelte
+++ lxl-web/src/lib/components/Matomo.svelte
@@ -1,37 +1,15 @@
 <script lang="ts">
 	import { env } from '$env/dynamic/public';
 	import { afterNavigate } from '$app/navigation';
-	import { writable } from 'svelte/store';
-	import { type MatomoTracker } from '$lib/types/matomo';
-	import { setMatomoTracker } from '$lib/contexts/matomo';
+	import { setMatomoTracker, getMatomoTracker } from '$lib/contexts/matomo';
 
 	const URL: string = env.PUBLIC_MATOMO_URL;
-	const MATOMO_ID: number = +env.PUBLIC_MATOMO_ID;
 
-	const tracker = writable<MatomoTracker>();
-	setMatomoTracker(tracker);
+	setMatomoTracker();
+	const tracker = getMatomoTracker();
 
-	async function initMatomo() {
-		const matomo = window.Matomo;
-
-		if (matomo && MATOMO_ID) {
-			const track = matomo.getTracker(`${URL}/matomo.php`, MATOMO_ID);
-
-			if (track) {
-				track.disableCookies(); // TODO - remove when cookie consent implemented
-				track.enableLinkTracking();
-
-				// add to matomo context store
-				tracker.set(track);
-				track.trackPageView();
-			}
-		}
-	}
-
-	afterNavigate(async ({ to, from, type }) => {
-		if (type === 'enter' || !$tracker) {
-			await initMatomo();
-		} else {
+	afterNavigate(async ({ to, from }) => {
+		if ($tracker) {
 			if (from?.url.href) {
 				$tracker.setReferrerUrl(from.url.href);
 			}
--- lxl-web/src/lib/contexts/matomo.ts
+++ lxl-web/src/lib/contexts/matomo.ts
@@ -1,9 +1,33 @@
+import { env } from '$env/dynamic/public';
+import { browser } from '$app/environment';
+import { writable } from 'svelte/store';
 import { type Writable } from 'svelte/store';
 import { type MatomoTracker } from '$lib/types/matomo';
 import { setContext, getContext } from 'svelte';
 
-export function setMatomoTracker(tracker: Writable<MatomoTracker>) {
-	setContext('matomo', tracker);
+const MATOMO_ID: number = +env.PUBLIC_MATOMO_ID;
+
+function initMatomo() {
+	if (browser) {
+		const matomo = window.Matomo;
+		if (matomo && MATOMO_ID) {
+			const tracker = matomo.getTracker(`${env.PUBLIC_MATOMO_URL}/matomo.php`, MATOMO_ID);
+
+			if (tracker) {
+				tracker.disableCookies(); // TODO - remove when cookie consent implemented
+				tracker.enableLinkTracking();
+				return tracker;
+			}
+		}
+	}
+}
+
+export function setMatomoTracker() {
+	const initializedMatomoTracker = initMatomo();
+	if (initializedMatomoTracker) {
+		const tracker = writable<MatomoTracker>(initializedMatomoTracker);
+		setContext('matomo', tracker);
+	}
 }
 
 export function getMatomoTracker() {
