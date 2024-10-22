9b201f07f211fe5633c184b3cf3bff002c2787fd
Jesper Engström <jesper.engstrom@kb.se>
2024-10-22 14:01:14 +0200

    Handle quoted qualifier value

--- lxl-web/src/lib/utils/codemirror/getEditedParts.ts
+++ lxl-web/src/lib/utils/codemirror/getEditedParts.ts
@@ -1,6 +1,10 @@
 import type { EditedRange } from '$lib/components/CodeMirror.svelte';
 
-const QUALIFIER_REGEX = RegExp(/((")?[0-9a-zA-ZåäöÅÄÖ:]+\2):((")?[0-9a-zA-ZåäöÅÄÖ:%#-.]+\4?)?/);
+// const QUALIFIER_REGEX = RegExp(/((")?[0-9a-zA-ZåäöÅÄÖ:]+\2):((")?[0-9a-zA-ZåäöÅÄÖ:%#-.]+\4?)?/);
+
+const QUALIFIER_REGEX = RegExp(
+	/^([0-9a-zA-ZåäöÅÄÖ:]*?):\s*(".*?"|[^"\s]+)(?:\s+[0-9a-zA-ZåäöÅÄÖ:%#-.]*)?$/
+);
 const PHRASE_REGEX = RegExp(/(^|\s)((")?[0-9a-zA-ZåäöÅÄÖ:]+\3:(")?[0-9a-zA-ZåäöÅÄÖ:%#-.]+\4?)/);
 
 /**
@@ -15,16 +19,22 @@ function getEditedParts({ value, cursor }: { value: string; cursor: number }): {
 	qualifierType: string | null;
 	qualifierValue: string | null;
 } {
-	const wordFromIndex = value.slice(0, cursor).lastIndexOf(' ') + 1;
-	const wordToIndex = cursor + value.slice(cursor).split(/\s+/)[0].length || 0;
-	const word = value.slice(wordFromIndex, wordToIndex);
+	// word start = last blankspace not in quote from start to cursor
+	const wordFromIndex = blankSpacesOutsideOfQuotes(value.slice(0, cursor)).pop() || 0;
+
+	// word end = first blankspace not in quote from word start to end
+	const wordToIndex =
+		wordFromIndex +
+		(blankSpacesOutsideOfQuotes(value.slice(wordFromIndex)).shift() || value.length);
+
+	const word = value.slice(wordFromIndex, wordToIndex).trim();
 	const wordRange = { from: wordFromIndex, to: wordToIndex };
 
 	const qualifierMatch = word.match(QUALIFIER_REGEX);
 
 	if (qualifierMatch) {
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		const [match, qualifierType, delimiter, qualifierValue] = qualifierMatch;
+		const [match, qualifierType, qualifierValue] = qualifierMatch;
 
 		return {
 			word,
@@ -35,6 +45,7 @@ function getEditedParts({ value, cursor }: { value: string; cursor: number }): {
 			qualifierValue
 		};
 	} else {
+		// todo: phrases does not yet take quotations in qualifiers into account
 		const phraseBefore = value.slice(0, cursor).split(PHRASE_REGEX).pop() || ''; // get last string before cursor which isn't a qualifier
 		const phraseAfter = value.slice(cursor).split(PHRASE_REGEX)?.[0] || ''; // get first string after cursor which isn't a qualifier
 		const phrase = (phraseBefore + phraseAfter).trim();
@@ -51,6 +62,27 @@ function getEditedParts({ value, cursor }: { value: string; cursor: number }): {
 			qualifierValue: null
 		};
 	}
+}
+
+/**
+ * Returns the index positions of blank spaces outside of quotes in a string
+ */
+function blankSpacesOutsideOfQuotes(str: string) {
+	const resultArr = [];
+	let inQuotes = false;
+
+	for (let i = 0; i < str.length; i++) {
+		const char = str[i];
+		if (char === '"') {
+			inQuotes = !inQuotes;
+		} else if (char === ' ' && !inQuotes) {
+			if (i > 0) {
+				// ignore opening whitespace
+				resultArr.push(i);
+			}
+		}
+	}
+	return resultArr;
 }
 
 export default getEditedParts;
