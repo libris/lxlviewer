59bbcf19c064535723239cebd11a629025cb79dc
Lars Rosenström <lars.rosenstrom@kb.se>
2024-10-24 15:11:22 +0200

    Unlock itemOf and heldBy when editing bulkchanges

--- cataloging/src/settings.js
+++ cataloging/src/settings.js
@@ -102,6 +102,10 @@ export default {
     'generationProcess',
     'recordStatus',
   ],
+  unlockableProperties: [
+    'itemOf',
+    'heldBy',
+  ],
   defaultExpandedProperties: [
     'hasComponent',
     '@reverse/reproductionOf',
