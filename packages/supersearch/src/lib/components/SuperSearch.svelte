2a2867da54edb1408489014c906e5bd0b9f8d614
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-11-04 09:29:44 +0100

    feat(supersearch): Init supersearch library

---
+++ packages/supersearch/src/lib/components/SuperSearch.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import CodeMirror, { type ChangeCodeMirrorEvent } from '$lib/components/CodeMirror.svelte';
+	import { placeholder } from '@codemirror/view';
+
+	let value = $state('');
+
+	function handleChangeCodeMirror(event: ChangeCodeMirrorEvent) {
+		value = event.value;
+	}
+</script>
+
+<CodeMirror {value} extensions={[placeholder('Search')]} onchange={handleChangeCodeMirror} />
