591f4441b30574cf78e7690c9f7e14d6df0c2266
Jesper Engström <jesper.engstrom@kb.se>
2024-11-07 23:45:54 +0100

    Reset grammar & strip parser config

--- packages/codemirror-lang-lxlquery/src/index.ts
+++ packages/codemirror-lang-lxlquery/src/index.ts
@@ -1,16 +1,11 @@
-import {parser} from "./syntax.grammar"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
-import {styleTags, tags as t} from "@lezer/highlight"
+import { parser } from "./syntax.grammar"
+import { LRLanguage, LanguageSupport } from "@codemirror/language"
+import { styleTags, tags as t } from "@lezer/highlight"
 
 export const lxlQueryLanguage = LRLanguage.define({
+  name: "LXL query",
   parser: parser.configure({
     props: [
-      indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
-      }),
-      foldNodeProp.add({
-        Application: foldInside
-      }),
       styleTags({
         Identifier: t.variableName,
         String: t.string,
