1d21a5740dfcd1dfb5b2d880518bbd9099d0b9d4
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-11-11 13:02:22 +0100

    chore(lxlquery): Format and lint all existing files

--- packages/codemirror-lang-lxlquery/src/index.ts
+++ packages/codemirror-lang-lxlquery/src/index.ts
@@ -1,22 +1,22 @@
-import { parser } from "./syntax.grammar"
-import { LRLanguage, LanguageSupport } from "@codemirror/language"
-import { styleTags, tags as t } from "@lezer/highlight"
+import { parser } from './syntax.grammar';
+import { LRLanguage, LanguageSupport } from '@codemirror/language';
+import { styleTags, tags as t } from '@lezer/highlight';
 
 export const lxlQueryLanguage = LRLanguage.define({
-  name: "Libris XL query",
-  parser: parser.configure({
-    props: [
-      styleTags({
-        Identifier: t.variableName,
-        String: t.string,
-        CompareOperator: t.compareOperator,
-        "( )": t.paren
-      })
-    ]
-  }),
-  languageData: {}
-})
+	name: 'Libris XL query',
+	parser: parser.configure({
+		props: [
+			styleTags({
+				Identifier: t.variableName,
+				String: t.string,
+				CompareOperator: t.compareOperator,
+				'( )': t.paren
+			})
+		]
+	}),
+	languageData: {}
+});
 
 export function lxlQuery() {
-  return new LanguageSupport(lxlQueryLanguage)
+	return new LanguageSupport(lxlQueryLanguage);
 }
--- packages/codemirror-lang-lxlquery/test/test.js
+++ packages/codemirror-lang-lxlquery/test/test.js
@@ -1,18 +1,18 @@
-import { lxlQueryLanguage } from "../dist/index.js"
-import { fileTests } from "@lezer/generator/dist/test"
+import { lxlQueryLanguage } from '../dist/index.js';
+import { fileTests } from '@lezer/generator/dist/test';
 import { describe, it } from 'vitest';
 
-import * as fs from "fs"
-import * as path from "path"
+import * as fs from 'fs';
+import * as path from 'path';
 import { fileURLToPath } from 'url';
-let caseDir = path.dirname(fileURLToPath(import.meta.url))
+let caseDir = path.dirname(fileURLToPath(import.meta.url));
 
 for (let file of fs.readdirSync(caseDir)) {
-  if (!/\.txt$/.test(file)) continue
+	if (!/\.txt$/.test(file)) continue;
 
-  let name = /^[^\.]*/.exec(file)[0]
-  describe(name, () => {
-    for (let {name, run} of fileTests(fs.readFileSync(path.join(caseDir, file), "utf8"), file))
-      it(name, () => run(lxlQueryLanguage.parser))
-  })
+	let name = /^[^.]*/.exec(file)[0];
+	describe(name, () => {
+		for (let { name, run } of fileTests(fs.readFileSync(path.join(caseDir, file), 'utf8'), file))
+			it(name, () => run(lxlQueryLanguage.parser));
+	});
 }
