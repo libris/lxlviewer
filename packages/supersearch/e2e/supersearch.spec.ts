7ce6174bc3d2a34dc75b984ce0296745f9fccee2
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-11-12 16:11:34 +0100

    Add test

--- packages/supersearch/e2e/supersearch.spec.ts
+++ packages/supersearch/e2e/supersearch.spec.ts
@@ -5,7 +5,24 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('submits closest form on enter key press', async ({ page }) => {
-  await page.locator('form').getByRole('textbox').locator('div').fill('hello world')
-  await page.keyboard.press('Enter');
-  await expect(page).toHaveURL('/find?q=hello+world')
+	await page.locator('form').getByRole('textbox').locator('div').fill('hello world');
+	await page.keyboard.press('Enter');
+	await expect(page).toHaveURL('/find?q=hello+world');
+});
+
+test('prevents new line characters (e.g. when pasting multi-lined text', async ({
+	page,
+	context
+}) => {
+	await context.grantPermissions(['clipboard-read', 'clipboard-write']);
+	await page.locator('form').getByRole('textbox').locator('div').click();
+	await page.evaluate(() =>
+		navigator.clipboard.writeText(`One
+two
+three`)
+	);
+	await page.keyboard.press('Meta+v');
+	await expect(page.locator('form').getByRole('textbox').locator('div')).toHaveText(
+		'One two three'
+	);
 });
