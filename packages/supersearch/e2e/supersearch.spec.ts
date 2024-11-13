4663bd7e09b109e542312f544d913990e9a47292
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-11-13 11:10:47 +0100

    Fix test

--- packages/supersearch/e2e/supersearch.spec.ts
+++ packages/supersearch/e2e/supersearch.spec.ts
@@ -5,15 +5,23 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('submits closest form on enter key press', async ({ page }) => {
-  await page.locator('[data-test-id="test1"]').getByRole('textbox').locator('div').fill('hello world')
-  await page.keyboard.press('Enter');
-  await expect(page).toHaveURL('/test1?q=hello+world')
+	await page
+		.locator('[data-test-id="test1"]')
+		.getByRole('textbox')
+		.locator('div')
+		.fill('hello world');
+	await page.keyboard.press('Enter');
+	await expect(page).toHaveURL('/test1?q=hello+world');
 });
 
 test('submits form identified by form attribute on enter key press', async ({ page }) => {
-  await page.locator('[data-test-id="test2"]').getByRole('textbox').locator('div').fill('hello world')
-  await page.keyboard.press('Enter');
-  await expect(page).toHaveURL('/test2?q=hello+world')
+	await page
+		.locator('[data-test-id="test2"]')
+		.getByRole('textbox')
+		.locator('div')
+		.fill('hello world');
+	await page.keyboard.press('Enter');
+	await expect(page).toHaveURL('/test2?q=hello+world');
 });
 
 test('prevents new line characters (e.g. when pasting multi-lined text', async ({
@@ -21,11 +29,11 @@ test('prevents new line characters (e.g. when pasting multi-lined text', async (
 	context
 }) => {
 	await context.grantPermissions(['clipboard-read', 'clipboard-write']);
-	await page.locator('[data-test-id="test1"]').getByRole('textbox').click();
+	await page.locator('[data-test-id="test1"]').getByRole('textbox').locator('div').click();
 	await page.evaluate(() =>
 		navigator.clipboard.writeText(`One
-two
-three`)
+      two
+      three`)
 	);
 	await page.keyboard.press('Meta+v');
 	await expect(page.locator('[data-test-id="test1"]').getByRole('textbox')).toHaveText(
