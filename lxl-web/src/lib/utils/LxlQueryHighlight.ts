d8122833960eb6258935094524f0c5de00bc63c4
Jesper Engström <jesper.engstrom@kb.se>
2024-11-14 14:06:39 +0100

    Use kebab-case for classes

--- lxl-web/src/lib/utils/LxlQueryHighlight.ts
+++ lxl-web/src/lib/utils/LxlQueryHighlight.ts
@@ -4,12 +4,12 @@ import { tagHighlighter } from '@lezer/highlight';
 
 const highlighter = tagHighlighter([
   { tag: t.Qualifier, class: 'qualifier' },
-  { tag: t.QualifierKey, class: 'qualifierkey' },
-  { tag: t.QualifierValue, class: 'qualifiervalue' },
-  { tag: t.EqualOperator, class: 'equaloperator' },
-  { tag: t.CompareOperator, class: 'compareoperator' },
-  { tag: t.BooleanOperator, class: 'booleanoperator' },
-  { tag: t.BooleanQuery, class: 'booleanquery' },
+  { tag: t.QualifierKey, class: 'qualifier-key' },
+  { tag: t.QualifierValue, class: 'qualifier-value' },
+  { tag: t.EqualOperator, class: 'equal-operator' },
+  { tag: t.CompareOperator, class: 'compare-operator' },
+  { tag: t.BooleanOperator, class: 'boolean-operator' },
+  { tag: t.BooleanQuery, class: 'boolean-query' },
   { tag: t.Wildcard, class: 'wildcard' }
 ], {
   all: 'lxlq',
