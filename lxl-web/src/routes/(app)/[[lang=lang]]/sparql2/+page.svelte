e1f4fbe9e94c20b61b87ce223c98d6f6e2c333e8
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2024-12-09 09:17:44 +0100

    experimental(lxlweb): Use yasgui as sparql editor

---
+++ lxl-web/src/routes/(app)/[[lang=lang]]/sparql2/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import '@triply/yasgui/build/yasgui.min.css';
+
+	onMount(async () => {
+		// only in browser
+		// yasgui module expects 'window' to exist
+		const module = await import('@triply/yasgui');
+		const Yasgui = module.default;
+
+		new Yasgui(document.getElementById('yasgui'), {
+			requestConfig: { endpoint: 'https://libris-qa.kb.se/sparql' },
+			copyEndpointOnNewTab: true
+		});
+	});
+</script>
+
+<div id="yasgui"></div>
