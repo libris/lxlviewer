0bfecb387d77f5bdb9fe8c5f91717ca9a1ee0d6c
Jesper Engström <jesper.engstrom@kb.se>
2024-12-09 17:10:27 +0100

    Migrate search.svelte, add callback fn to Lxlqualifier

---
+++ lxl-web/src/lib/utils/getLabelsFromMapping.svelte.ts
@@ -0,0 +1,51 @@
+import type { DisplayMapping } from '$lib/types/search';
+
+function getLabelFromMappings(
+	key: string,
+	value?: string,
+	pageMapping?: DisplayMapping[],
+	suggestMapping?: DisplayMapping[]
+) {
+	const pageLabels = iterateMapping(key, value, pageMapping);
+	const suggestLabels = iterateMapping(key, value, suggestMapping);
+
+	const keyLabel = suggestLabels.keyLabel || pageLabels.keyLabel;
+	const valueLabel = suggestLabels.valueLabel || pageLabels.valueLabel;
+	// only page data have 'up' links we can use
+	const removeLink = pageLabels.keyLabel ? pageLabels.removeLink : undefined;
+
+	return { keyLabel, valueLabel, removeLink };
+}
+
+function iterateMapping(
+	key: string,
+	value: string | undefined,
+	mapping: DisplayMapping[] | undefined | null
+) {
+	let keyLabel: string | undefined;
+	let valueLabel: string | undefined;
+	let removeLink: string | undefined;
+
+	if (mapping && Array.isArray(mapping)) {
+		_iterate(mapping);
+
+		function _iterate(m: DisplayMapping[]) {
+			m.forEach((el) => {
+				if (el.children) {
+					_iterate(el.children);
+				} else if (el.property === key) {
+					keyLabel = el.label;
+					const isLinked = !!el.display?.['@id'];
+					// only use atomic ranges for linked values
+					if (isLinked && el.displayStr) {
+						valueLabel = el.displayStr;
+					}
+					removeLink = el.up?.['@id'];
+				}
+			});
+		}
+	}
+	return { keyLabel, valueLabel, removeLink };
+}
+
+export default getLabelFromMappings;
