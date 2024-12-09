d27b41af1ba4518ee8f03923847474864802a6da
Jesper Engström <jesper.engstrom@kb.se>
2024-12-09 17:39:27 +0100

    Refactor lxlQualifier extension, add qualifier component, styling

--- packages/supersearch/src/lib/extensions/lxlQualifierPlugin/index.ts
+++ packages/supersearch/src/lib/extensions/lxlQualifierPlugin/index.ts
@@ -9,8 +9,7 @@ import {
 import { EditorState, type Range } from '@codemirror/state';
 import { syntaxTree } from '@codemirror/language';
 import { mount } from 'svelte';
-import QualifierRemove from './QualifierRemove.svelte';
-import QualifierKey from './QualifierKey.svelte';
+import QualifierComponent from './QualifierComponent.svelte';
 import insertQuotes from './insertQuotes.js';
 
 export type Qualifier = {
@@ -19,143 +18,159 @@ export type Qualifier = {
 	operator: string;
 };
 
-class RemoveWidget extends WidgetType {
-	constructor(
-		readonly range: { from: number; to: number },
-		readonly atomic: boolean
-	) {
-		super();
-	}
-	eq(other: RemoveWidget) {
-		return this.range.from === other.range.from && this.range.to === other.range.to;
-	}
-	// is there a way to pass new props to component instead of re-mounting every time range changes?
-	toDOM(): HTMLElement {
-		const container = document.createElement('span');
-		container.style.cssText = `position: relative;`;
-		mount(QualifierRemove, {
-			props: {
-				range: this.range,
-				url: new URL(window?.location.href)
-			},
-			target: container
-		});
-		return container;
-	}
-}
-
-class QualifierKeyWidget extends WidgetType {
-	constructor(
-		readonly key: string,
-		readonly operator: string,
-		readonly label: string | undefined,
-		readonly keyType: string | undefined,
-		readonly operatorType: string | undefined,
-		readonly atomic: boolean
-	) {
-		super();
-	}
-	eq(other: QualifierKeyWidget): boolean {
-		return this.key === other.key && this.operator === other.operator;
-	}
-	toDOM(): HTMLElement {
-		const container = document.createElement('span');
-		container.style.cssText = `position: relative;`;
-		mount(QualifierKey, {
-			props: {
-				key: this.key,
-				operator: this.operator,
-				label: this.label,
-				keyType: this.keyType,
-				operatorType: this.operatorType
-			},
-			target: container
-		});
-		return container;
-	}
-}
-
-function getQualifiers(view: EditorView) {
-	const widgets: Range<Decoration>[] = [];
-	const doc = view.state.doc.toString();
-
-	for (const { from, to } of view.visibleRanges) {
-		syntaxTree(view.state).iterate({
-			from,
-			to,
-			enter: (node) => {
-				if (node.name === 'Qualifier') {
-					// Mark decoration to create wrapper element, non-atomic
-					const qualifierMark = Decoration.mark({
-						class: 'lxl-qualifier',
-						inclusive: true,
-						atomic: false // invented!
-					});
-					widgets.push(qualifierMark.range(node.from, node.to));
-
-					// Remove decoration (x-button) widget - atomic
-					const removeDecoration = Decoration.widget({
-						widget: new RemoveWidget({ from: node.from, to: node.to }, true),
-						side: 1
-					});
-					widgets.push(removeDecoration.range(node.to));
-
-					// Qualifier key + operator widget - atomic
-					const keyNode = node.node.getChild('QualifierKey');
-					const operatorNode = node.node.getChild('QualifierOperator');
-
-					if (keyNode && operatorNode) {
-						const keyDecoration = Decoration.replace({
-							widget: new QualifierKeyWidget(
-								doc.slice(keyNode?.from, keyNode?.to),
-								doc.slice(operatorNode?.from, operatorNode?.to),
-								doc.slice(keyNode?.from, keyNode?.to), // label should be found using vocab
-								keyNode.firstChild?.type.name,
-								operatorNode.firstChild?.type.name,
-								true
-							)
-						});
-						widgets.push(keyDecoration.range(keyNode?.from, operatorNode?.to));
-					}
-				}
-			}
-		});
-	}
-
-	return Decoration.set(widgets, true); // true = sort
-}
-
-/**
- * filter out non-atomics using custom property 'atomic'
- */
-const filterAtomic = (from: number, to: number, decoration: Decoration) => {
-	return decoration.spec?.atomic || decoration.spec?.widget?.atomic;
+type GetLabelFunction = (
+	key: string,
+	value?: string
+) => {
+	keyLabel?: string;
+	valueLabel?: string;
+	removeLink?: string;
 };
 
-export const lxlQualifierPlugin = ViewPlugin.fromClass(
-	class {
-		qualifiers: DecorationSet;
-		constructor(view: EditorView) {
-			this.qualifiers = getQualifiers(view);
-		}
-
-		update(update: ViewUpdate) {
-			if (update.docChanged || syntaxTree(update.startState) != syntaxTree(update.state)) {
-				this.qualifiers = getQualifiers(update.view);
-			}
-		}
-	},
-	{
-		decorations: (instance) => instance.qualifiers,
-		eventHandlers: {},
-		provide: (plugin) => [
-			EditorView.atomicRanges.of((view) => {
-				const filteredRanges = view.plugin(plugin)?.qualifiers.update({ filter: filterAtomic });
-				return filteredRanges || Decoration.none;
-			}),
-			EditorState.transactionFilter.of(insertQuotes)
-		]
+class QualifierWidget extends WidgetType {
+	constructor(
+		readonly key: string,
+		readonly keyLabel: string | undefined,
+		readonly keyType: string | undefined,
+		readonly value: string | undefined,
+		readonly valueLabel: string | undefined,
+		readonly operator: string,
+		readonly operatorType: string | undefined,
+		readonly removeLink: string | undefined,
+		readonly atomic: boolean
+	) {
+		super();
 	}
-);
+	eq(other: QualifierWidget): boolean {
+		return (
+			this.key === other.key &&
+			this.keyLabel === other.keyLabel &&
+			this.operator === other.operator &&
+			this.value === other.value &&
+			this.valueLabel === other.valueLabel
+		);
+	}
+	toDOM(): HTMLElement {
+		const container = document.createElement('span');
+		container.style.cssText = `position: relative; display:inline-flex`;
+		mount(QualifierComponent, {
+			props: {
+				key: this.key,
+				keyLabel: this.keyLabel,
+				keyType: this.keyType,
+				value: this.value,
+				valueLabel: this.valueLabel,
+				operator: this.operator,
+				operatorType: this.operatorType,
+				removeLink: this.removeLink
+			},
+			target: container
+		});
+		return container;
+	}
+}
+
+function lxlQualifierPlugin(getLabelFn?: GetLabelFunction) {
+	function getQualifiers(view: EditorView) {
+		const widgets: Range<Decoration>[] = [];
+		const doc = view.state.doc.toString();
+
+		for (const { from, to } of view.visibleRanges) {
+			syntaxTree(view.state).iterate({
+				from,
+				to,
+				enter: (node) => {
+					if (node.name === 'Qualifier') {
+						const keyNode = node.node.getChild('QualifierKey');
+						const key = keyNode ? doc.slice(keyNode?.from, keyNode?.to) : '';
+						const keyType = keyNode?.firstChild?.type.name;
+
+						const operatorNode = node.node.getChild('QualifierOperator');
+						const operator = operatorNode ? doc.slice(operatorNode?.from, operatorNode?.to) : '';
+						const operatorType = operatorNode?.firstChild?.type.name;
+
+						const valueNode = node.node.getChild('QualifierValue');
+						const value = valueNode ? doc.slice(valueNode?.from, valueNode?.to) : undefined;
+
+						const { keyLabel, valueLabel, removeLink } = getLabelFn?.(key, value) || {};
+
+						// Add qualifier widget
+						if (keyLabel) {
+							const qualifierDecoration = Decoration.replace({
+								widget: new QualifierWidget(
+									key,
+									keyLabel,
+									keyType,
+									value,
+									valueLabel,
+									operator,
+									operatorType,
+									removeLink,
+									true // atomic
+								)
+							});
+							const rangeFrom = node.from;
+							const rangeTo = valueLabel ? node.to : operatorNode?.to;
+							widgets.push(qualifierDecoration.range(rangeFrom, rangeTo));
+						} else {
+							// Add invalid key mark decoration
+							const qualifierMark = Decoration.mark({
+								class: 'lxl-qualifier-key invalid',
+								inclusive: true,
+								atomic: false
+							});
+							widgets.push(qualifierMark.range(node.from, operatorNode?.to));
+						}
+
+						// add mark decoration for qualifier value if not included in widget
+						if (valueNode && value && !valueLabel) {
+							const qualifierMark = Decoration.mark({
+								class: keyLabel ? 'lxl-qualifier-value unlinked' : 'lxl-qualifier-value',
+								inclusive: true,
+								atomic: false
+							});
+							widgets.push(qualifierMark.range(valueNode.from, valueNode.to));
+						}
+					}
+				}
+			});
+		}
+		return Decoration.set(widgets, true); // true = sort
+	}
+
+	/**
+	 * filter out non-atomics using custom property 'atomic'
+	 */
+	const filterAtomic = (from: number, to: number, decoration: Decoration) => {
+		return decoration.spec?.atomic || decoration.spec?.widget?.atomic;
+	};
+
+	const qualifierPlugin = ViewPlugin.fromClass(
+		class {
+			qualifiers: DecorationSet;
+			constructor(view: EditorView) {
+				this.qualifiers = getQualifiers(view);
+			}
+
+			update(update: ViewUpdate) {
+				if (update.docChanged || syntaxTree(update.startState) != syntaxTree(update.state)) {
+					this.qualifiers = getQualifiers(update.view);
+				}
+			}
+		},
+		{
+			decorations: (instance) => instance.qualifiers,
+			eventHandlers: {},
+			provide: (plugin) => [
+				EditorView.atomicRanges.of((view) => {
+					const filteredRanges = view.plugin(plugin)?.qualifiers.update({ filter: filterAtomic });
+					return filteredRanges || Decoration.none;
+				}),
+				EditorState.transactionFilter.of(insertQuotes)
+			]
+		}
+	);
+	return qualifierPlugin;
+}
 
 export default lxlQualifierPlugin;
