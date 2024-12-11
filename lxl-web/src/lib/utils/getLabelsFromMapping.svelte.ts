a35bf94144d343bde636d4bcbaee71685952db4c
Jesper Engström <jesper.engstrom@kb.se>
2024-12-11 10:39:33 +0100

    Fix all labels disappearing efter entering invalid qualifier

--- lxl-web/src/lib/utils/getLabelsFromMapping.svelte.ts
+++ lxl-web/src/lib/utils/getLabelsFromMapping.svelte.ts
@@ -1,5 +1,7 @@
 import type { DisplayMapping } from '$lib/types/search';
 
+let prevSuggestMapping: DisplayMapping[] | undefined;
+
 function getLabelFromMappings(
 	key: string,
 	value?: string,
@@ -7,12 +9,18 @@ function getLabelFromMappings(
 	suggestMapping?: DisplayMapping[]
 ) {
 	const pageLabels = iterateMapping(key, value, pageMapping);
-	const suggestLabels = iterateMapping(key, value, suggestMapping);
+	const suggestLabels = iterateMapping(key, value, suggestMapping || prevSuggestMapping);
 
 	const keyLabel = suggestLabels.keyLabel || pageLabels.keyLabel;
 	const valueLabel = suggestLabels.valueLabel || pageLabels.valueLabel;
 	// only page data have 'up' links we can use
 	const removeLink = pageLabels.keyLabel ? pageLabels.removeLink : undefined;
+
+	if (suggestMapping) {
+		// TODO remove when invalid qualifier no longer result in empty error response
+		// until when we need to save latest 'successful' suggest mapping
+		prevSuggestMapping = suggestMapping;
+	}
 
 	return { keyLabel, valueLabel, removeLink };
 }
