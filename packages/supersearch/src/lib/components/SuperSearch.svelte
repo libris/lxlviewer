79e2a2ae737e20a7915b80904eefa8b146b3aa88
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2024-12-20 22:23:40 +0100

    Add keyboard handling for rows and cells in expanded search
    Adheres to the Combobox Pattern in W3Cs ARIA Authoring Practice Guide (see: https://www.w3.org/WAI/ARIA/apg/patterns/combobox/)

--- packages/supersearch/src/lib/components/SuperSearch.svelte
+++ packages/supersearch/src/lib/components/SuperSearch.svelte
@@ -18,6 +18,7 @@
 	import { standardKeymap } from '@codemirror/commands';
 
 	interface Props {
+		id?: string;
 		name: string;
 		value?: string;
 		form?: string;
@@ -28,11 +29,15 @@
 		paginationQueryFn?: PaginationQueryFunction;
 		transformFn?: TransformFunction;
 		extensions?: Extension[];
-		resultItem?: Snippet<[ResultItem]>;
+		resultItem?: Snippet<
+			[ResultItem, (cellIndex: number) => string, (cellIndex: number) => boolean, number]
+		>;
+		defaultRow?: number;
 		toggleWithKeyboardShortcut?: boolean;
 	}
 
 	let {
+		id = 'supersearch',
 		name,
 		value = $bindable(''),
 		form,
@@ -44,16 +49,23 @@
 		transformFn,
 		extensions = [],
 		resultItem = fallbackResultItem,
-		toggleWithKeyboardShortcut = false
+		toggleWithKeyboardShortcut = false,
+		defaultRow = 0
 	}: Props = $props();
 
 	let collapsedEditorView: EditorView | undefined = $state();
 	let expandedEditorView: EditorView | undefined = $state();
 	let dialog: HTMLDialogElement | undefined = $state();
+	let expanded = $state(false);
 	let cursor: number = $state(0);
+	let activeRowIndex: number = $state(0);
+	let activeColIndex: number = $state(0);
 
 	let placeholderCompartment = new Compartment();
 	let prevPlaceholder = placeholder;
+
+	let collapsedContentAttributesCompartment = new Compartment();
+	let expandedContentAttributesCompartment = new Compartment();
 
 	let search = useSearchRequest({
 		endpoint,
@@ -89,6 +101,43 @@
 		...extensions
 	];
 
+	let collapsedContentAttributes = $derived(
+		EditorView.contentAttributes.of({
+			'aria-haspopup': 'dialog', // indicates the availability and type of interactive popup element that can be triggered by the element
+			'aria-controls': `${id}-dialog`, // identifies the popup element
+			'aria-expanded': expanded.toString() // indicates if the popup element is open
+		})
+	);
+
+	let expandedContentAttributes = $derived(
+		EditorView.contentAttributes.of({
+			id: `${id}-content`,
+			role: 'combobox', // identifies the element as a combobox
+			'aria-haspopup': 'grid', // indicates that the combobox can popup a grid to suggest values
+			'aria-expanded': 'true', // indicates that the popup element is displayed
+			'aria-autocomplete': 'list', // indicates that the autocomplete behavior of the input is to suggest a list of possible values in a popup
+			'aria-controls': `${id}-grid`, // identifies the popup element that lists suggested values
+			...(activeRowIndex >= 0 && {
+				'aria-activedescendant': `${id}-result-item-${activeRowIndex}x${activeColIndex}` // enables assistive technologies to know which element the application regards as focused while DOM focus remains on the input element
+			})
+		})
+	);
+
+	/* svelte-ignore state_referenced_locally */
+	const initialCollapsedContentAttributes = collapsedContentAttributes; // initial value is needed to prevent unnecessary updates when using compartments
+	// svelte-ignore state_referenced_locally
+	const initialExpandedContentAttributes = expandedContentAttributes;
+
+	let collapsedExtensions = $derived([
+		...extensionsWithDefaults,
+		collapsedContentAttributesCompartment.of(initialCollapsedContentAttributes)
+	]);
+
+	let expandedExtensions = $derived([
+		...extensionsWithDefaults,
+		expandedContentAttributesCompartment.of(initialExpandedContentAttributes)
+	]);
+
 	function handleClickCollapsed() {
 		if (!dialog?.open) showExpandedSearch();
 	}
@@ -99,6 +148,8 @@
 		}
 		value = event.value;
 		cursor = event.cursor;
+		activeRowIndex = defaultRow;
+		activeColIndex = 0;
 	}
 
 	function showExpandedSearch() {
@@ -107,6 +158,7 @@
 		});
 		dialog?.showModal();
 		expandedEditorView?.focus();
+		expanded = true;
 	}
 
 	function hideExpandedSearch() {
@@ -115,11 +167,89 @@
 		});
 		dialog?.close();
 		collapsedEditorView?.focus();
+		expanded = false;
 	}
 
 	function handleKeyDown(event: KeyboardEvent) {
 		if (event.key === 'Escape') {
 			hideExpandedSearch();
+		}
+
+		/**
+		 * Handle keyboard navigation between focusable elements in expanded search
+		 */
+		if (
+			event.key === 'ArrowUp' ||
+			event.key === 'ArrowDown' ||
+			event.key === 'ArrowLeft' ||
+			event.key === 'ArrowRight' ||
+			event.key === 'Tab'
+		) {
+			const rows = Array.from(
+				dialog?.querySelectorAll(':scope nav:first-of-type [role=row]') || []
+			);
+
+			const getColsInActiveRow = (activeRowIndex: number) =>
+				rows[activeRowIndex].querySelectorAll(':scope button, :scope a');
+
+			if (rows.length) {
+				switch (event.key) {
+					case 'ArrowUp':
+						if (activeRowIndex > 0) {
+							activeRowIndex--;
+							activeColIndex = Math.min(
+								activeColIndex,
+								getColsInActiveRow(activeRowIndex).length - 1
+							);
+						}
+						break;
+					case 'ArrowDown':
+						if (activeRowIndex < rows.length - 1) {
+							activeRowIndex++;
+							activeColIndex = Math.min(
+								activeColIndex,
+								getColsInActiveRow(activeRowIndex).length - 1
+							);
+						}
+						break;
+					case 'ArrowLeft':
+						if (activeColIndex > 0) {
+							activeColIndex--;
+						}
+						break;
+					case 'ArrowRight':
+						if (activeColIndex < getColsInActiveRow(activeRowIndex).length - 1) {
+							activeColIndex++;
+						}
+						break;
+					case 'Tab':
+						if (event.shiftKey) {
+							if (activeColIndex == 0) {
+								activeRowIndex--;
+								activeColIndex = getColsInActiveRow(activeRowIndex).length - 1;
+							} else {
+								activeColIndex--;
+							}
+						} else {
+							if (activeColIndex < getColsInActiveRow(activeRowIndex).length - 1) {
+								activeColIndex++;
+							} else {
+								activeRowIndex++;
+								activeColIndex = 0;
+							}
+						}
+						break;
+				}
+
+				/**
+				 * TODO: Ensure the input is in view
+				 * const activeCellElement = document.getElementById(`${id}-result-item-${activeRowIndex}x${activeColIndex}`);
+				 *
+				 * if (!isElementInView(activeCellElement)) {
+				 *		activeCellElement.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+				 * }
+				 */
+			}
 		}
 	}
 
@@ -165,6 +295,18 @@
 			prevPlaceholder = placeholder;
 		}
 	});
+
+	$effect(() => {
+		collapsedEditorView?.dispatch({
+			effects: collapsedContentAttributesCompartment.reconfigure(collapsedContentAttributes)
+		});
+	});
+
+	$effect(() => {
+		expandedEditorView?.dispatch({
+			effects: expandedContentAttributesCompartment.reconfigure(expandedContentAttributes)
+		});
+	});
 </script>
 
 {#snippet fallbackResultItem(item: ResultItem)}
@@ -173,53 +315,64 @@
 
 <CodeMirror
 	{value}
-	extensions={extensionsWithDefaults}
+	extensions={collapsedExtensions}
 	onclick={handleClickCollapsed}
 	onchange={handleChangeCodeMirror}
 	bind:editorView={collapsedEditorView}
 	syncedEditorView={expandedEditorView}
 />
 <textarea {value} {name} {form} hidden readonly></textarea>
-<dialog bind:this={dialog} onclose={hideExpandedSearch} onkeydowncapture={handleKeyDown}>
-	<CodeMirror
-		{value}
-		extensions={extensionsWithDefaults}
-		onchange={handleChangeCodeMirror}
-		bind:editorView={expandedEditorView}
-		syncedEditorView={collapsedEditorView}
-	/>
-	<nav>
-		{#if search.data}
-			{@const resultItems =
-				(Array.isArray(search.paginatedData) &&
-					search.paginatedData.map((page) => page.items).flat()) ||
-				search.data?.items}
-			<ul>
-				{#each resultItems as item}
-					<li>
-						{@render resultItem?.(item)}
-					</li>
-				{/each}
-			</ul>
-		{/if}
-		{#if search.isLoading}
-			Loading...
-		{:else if search.hasMorePaginatedData}
-			<button type="button" class="supersearch-show-more" onclick={search.fetchMoreData}>
-				Load more
-			</button>
-		{/if}
-	</nav>
+<dialog id={`${id}-dialog`} bind:this={dialog} onclose={hideExpandedSearch}>
+	<div role="presentation" onkeydown={handleKeyDown}>
+		<CodeMirror
+			{value}
+			extensions={expandedExtensions}
+			onchange={handleChangeCodeMirror}
+			bind:editorView={expandedEditorView}
+			syncedEditorView={collapsedEditorView}
+		/>
+		<nav>
+			{#if search.data}
+				{@const resultItems =
+					(Array.isArray(search.paginatedData) &&
+						search.paginatedData.map((page) => page.items).flat()) ||
+					search.data?.items}
+				<div id={`${id}-grid`} role="grid">
+					{#each resultItems as item, rowIndex}
+						<div role="row" class:focused={activeRowIndex === rowIndex}>
+							{@render resultItem?.(
+								item,
+								(colIndex: number) => `${id}-result-item-${rowIndex}x${colIndex}`,
+								(colIndex: number) => activeRowIndex === rowIndex && colIndex === activeColIndex,
+								rowIndex
+							)}
+						</div>
+					{/each}
+				</div>
+			{/if}
+			{#if search.isLoading}
+				Loading...
+			{:else if search.hasMorePaginatedData}
+				<li>
+					<button type="button" class="supersearch-show-more" onclick={search.fetchMoreData}>
+						Load more
+					</button>
+				</li>
+			{/if}
+		</nav>
+	</div>
 </dialog>
 
 <style>
-	ul {
-		margin: 0;
-		padding: 0;
-		list-style-type: none;
-	}
-
 	dialog {
 		padding: 0;
+	}
+
+	.focused {
+		background: #ebebeb;
+
+		& :global(.focused-cell) {
+			background: lightgreen;
+		}
 	}
 </style>
