d059681650afbea33f444bc16bb42efba3952d0f
Jesper Engström <jesper.engstrom@kb.se>
2025-01-13 11:52:55 +0100

    Handle space input before qualifier

--- packages/supersearch/src/lib/extensions/lxlQualifierPlugin/insertSpace.ts
+++ packages/supersearch/src/lib/extensions/lxlQualifierPlugin/insertSpace.ts
@@ -6,20 +6,31 @@ const insertSpaceBeforeQualifier = (getRanges: () => RangeSet<RangeValue>) => {
 			return tr;
 		} else {
 			let insert = {};
+			let changes = {};
 			const atomicRanges = getRanges();
 			const oldCursorPos = tr.startState.selection.main.head;
 			const newCursorPos = tr.state.selection.main.head;
 
-			atomicRanges.between(oldCursorPos, oldCursorPos, () => {
-				insert = {
-					changes: {
-						from: newCursorPos,
-						to: newCursorPos,
-						insert: ' '
-					},
-					sequential: true,
-					selection: { anchor: newCursorPos }
-				};
+			atomicRanges.between(oldCursorPos, oldCursorPos, (from) => {
+				if (oldCursorPos === from) {
+					// overlap is at atomic range start
+					const input = !!tr.newDoc.slice(oldCursorPos, newCursorPos).toString().trim();
+					const isDelete = tr.isUserEvent('delete');
+					// don't add space if input is space
+					// instead, move cursor back to old pos
+					if (input || isDelete) {
+						changes = {
+							from: newCursorPos,
+							to: newCursorPos,
+							insert: ' '
+						};
+					}
+					insert = {
+						changes,
+						sequential: true,
+						selection: { anchor: input || isDelete ? newCursorPos : oldCursorPos }
+					};
+				}
 				return false;
 			});
 			return [tr, insert];
