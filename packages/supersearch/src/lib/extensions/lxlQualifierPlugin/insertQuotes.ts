72948a04a28adeb9c72e65f0eab118be7c481614
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2025-02-24 13:21:54 +0100

    Add qualifiers using dispatchChange
    Uses input.complete userEvent to prevent insertQuotes to be triggered

--- packages/supersearch/src/lib/extensions/lxlQualifierPlugin/insertQuotes.ts
+++ packages/supersearch/src/lib/extensions/lxlQualifierPlugin/insertQuotes.ts
@@ -5,12 +5,13 @@ import { syntaxTree } from '@codemirror/language';
  * Moves cursor into an empty quote after typing the QualifierOperator
  */
 const insertQuotes = (tr: Transaction) => {
-	if (!tr.isUserEvent('input')) {
+	if (!tr.isUserEvent('input') || tr.isUserEvent('input.complete')) {
 		return tr;
 	}
 	let changes = null;
 	syntaxTree(tr.state).iterate({
 		enter: (node) => {
+			/** TODO: We can probably optimize this as it currently traverses the entire syntax tree (when it should be enough to just check the edited/added part) */
 			if (node.name === 'QualifierOperator') {
 				const operatorEnd = node.to;
 				const oldCursorPos = tr.startState.selection.main.head;
