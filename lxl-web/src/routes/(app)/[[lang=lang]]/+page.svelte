09507d2d8e75dc57a42d7dc61b4fdd9fcc104fac
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2025-02-25 15:04:41 +0100

    Fix layout on start page

--- lxl-web/src/routes/(app)/[[lang=lang]]/+page.svelte
+++ lxl-web/src/routes/(app)/[[lang=lang]]/+page.svelte
@@ -10,6 +10,16 @@
 </svelte:head>
 
 <article class="container-fluid my-8 max-w-3xl">
+	<div class="landing flex flex-col items-center gap-2 pb-3">
+		<h1 class="flex text-3xl font-extrabold text-primary sm:text-[5.5rem] sm:font-bold">
+			Libris
+			<sup
+				class="self-center rounded-sm bg-positive-dark/16 px-2 uppercase text-2-cond-bold sm:text-3-cond-bold"
+				>Beta</sup
+			>
+		</h1>
+	</div>
+
 	{#if $page.data.locale === 'en'}
 		<EnContent />
 	{:else}
