92dc32c5f593f7541f0f9d96e7ce926fbccba3c9
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2025-02-25 14:57:15 +0100

    Fix tests

--- lxl-web/src/lib/components/SuperSearchWrapper.svelte
+++ lxl-web/src/lib/components/SuperSearchWrapper.svelte
@@ -143,7 +143,7 @@
 	</div>
 {/snippet}
 
-<form class="relative w-full" action="find" onsubmit={handleSubmit}>
+<form class="relative w-full" action="find" onsubmit={handleSubmit} data-testid="main-search">
 	<SuperSearch
 		name="_q"
 		bind:this={superSearch}
@@ -449,5 +449,9 @@
 		padding-top: 0.6125rem;
 		padding-bottom: 0.6125rem;
 		outline: none;
+	}
+
+	:global(.codemirror-container .cm-placeholder) {
+		color: #757575;
 	}
 </style>
