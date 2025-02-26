d756854d8bc75e5c7593fa5b692a06fc4a599f3e
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2025-02-26 10:32:52 +0100

    Fix mapping visiblity

--- lxl-web/src/routes/(app)/[[lang=lang]]/find/+page.svelte
+++ lxl-web/src/routes/(app)/[[lang=lang]]/find/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import { env } from '$env/dynamic/public';
 	import { page } from '$app/stores';
 	import SearchResult from '$lib/components/find/SearchResult.svelte';
 	import getPageTitle from '$lib/utils/getPageTitle';
+
+	let usingSuperSearch = $derived(
+		env?.PUBLIC_USE_SUPERSEARCH === 'true' || $page.url.searchParams.get('_x') === 'supersearch'
+	);
 </script>
 
 <svelte:head>
@@ -10,5 +15,5 @@
 <h1 class="sr-only">{$page.data.t('search.searchResults')}</h1>
 <SearchResult
 	searchResult={$page.data.searchResult}
-	showMapping={$page.url.searchParams.get('_x') === 'mappings'}
+	showMapping={$page.url.searchParams.get('_x') === 'mappings' || !usingSuperSearch}
 />
