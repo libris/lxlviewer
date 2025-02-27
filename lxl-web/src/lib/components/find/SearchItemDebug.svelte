5722aa9912375058fa580e42f029d5094dbbd072
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2025-02-27 19:32:34 +0100

    feat(lxl-web): Display haystack values when _debug=esScore

--- lxl-web/src/lib/components/find/SearchItemDebug.svelte
+++ lxl-web/src/lib/components/find/SearchItemDebug.svelte
@@ -23,12 +23,22 @@
 				<td>{fmtPercent(score.totalPercent)}%</td>
 				<td class="text-3-cond-bold">{fmt(score.total)}</td>
 			</tr>
+			<tr class="italic">
+				<td>field</td>
+				<td>needle</td>
+				<td>this page</td>
+				<td>score</td>
+			</tr>
 		</thead>
 		<tbody>
 			{#each score.perField as field}
-				<tr>
+				<tr
+					title="haystack: {field.haystack.slice(0, 1)}{field.haystack.length > 1
+						? ` + ${field.haystack.length - 1}`
+						: ''}"
+				>
 					<td>{field.name}</td>
-					<td>{field.searchString}</td>
+					<td>{field.needle}</td>
 					<td>{fmtPercent(field.scorePercent)}%</td>
 					<td>{fmt(field.score)}</td>
 				</tr>
