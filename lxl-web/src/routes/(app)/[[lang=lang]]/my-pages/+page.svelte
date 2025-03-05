adaefff28c9798fbefd570b0364b254d9748f590
Lars Rosenström <lars.rosenstrom@kb.se>
2025-03-05 14:55:37 +0100

    Working use of supersearch component

--- lxl-web/src/routes/(app)/[[lang=lang]]/my-pages/+page.svelte
+++ lxl-web/src/routes/(app)/[[lang=lang]]/my-pages/+page.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
 	import getPageTitle from '$lib/utils/getPageTitle';
+	import { SuperSearch } from 'supersearch';
 	import { page } from '$app/stores';
 	import BiSearch from '~icons/bi/search';
-	let searchPhrase = '';
+	import { lxlQuery } from 'codemirror-lang-lxlquery';
+	import BiXLg from '~icons/bi/x-lg';
+	import BiArrowLeft from '~icons/bi/arrow-left';
+	import SuggestionCard from '$lib/components/SuggestionCard.svelte';
+
+	let searchPhrase = $state('');
+	let value = $state('');
+	let superSearch = $state<ReturnType<typeof SuperSearch>>();
+
+	let isLoading: boolean | undefined = $state();
+	let hasData: boolean | undefined = $state();
+	let placeholder = $state('Search');
+	let useFormAttribute = $state(false);
 </script>
 
 <svelte:head>
@@ -23,8 +36,127 @@
 				class="mt-3 w-full pl-8"
 				type="search"
 			/>
+			<SuperSearch
+				name="q"
+				bind:this={superSearch}
+				bind:value
+				bind:isLoading
+				bind:hasData
+				{placeholder}
+				endpoint={`/api/my-pages`}
+				queryFn={(query) =>
+					new URLSearchParams({
+						_q: query,
+						_limit: '10'
+					})}
+				language={lxlQuery}
+				toggleWithKeyboardShortcut
+				defaultInputCol={2}
+				defaultResultRow={1}
+				form={useFormAttribute ? 'form-outside' : undefined}
+			>
+				{#snippet loadingIndicator()}
+					<div class="flex min-h-11 w-full items-center px-4 text-left">
+						{$page.data.t('search.loading')}
+					</div>
+				{/snippet}
+				{#snippet inputRow({
+					expanded,
+					inputField,
+					getCellId,
+					isFocusedCell,
+					onclickSubmit,
+					onclickClear,
+					onclickClose
+				})}
+					<div class="supersearch-input">
+						{#if expanded}
+							<button
+								type="button"
+								id={getCellId(0)}
+								class:focused-cell={isFocusedCell(0)}
+								aria-label={$page.data.t('general.close')}
+								class="button-ghost min-h-12 min-w-11 rounded-none border-none hover:bg-main sm:hidden"
+								onclick={onclickClose}
+							>
+								<BiArrowLeft />
+							</button>
+						{/if}
+						<div class="flex-1 overflow-hidden">
+							{@render inputField()}
+						</div>
+						{#if searchPhrase}
+							<button
+								type="reset"
+								id={getCellId(1)}
+								class:focused-cell={isFocusedCell(1)}
+								class="button-ghost min-h-12 rounded-none border-none hover:bg-main"
+								aria-label={$page.data.t('search.clearFilters')}
+								onclick={onclickClear}
+							>
+								<BiXLg />
+							</button>
+						{/if}
+						<button
+							type="submit"
+							id={getCellId(2)}
+							class:focused-cell={isFocusedCell(2)}
+							class="submit-action button-primary min-h-12 rounded-none"
+							enterkeyhint="search"
+							onclick={onclickSubmit}
+						>
+							{$page.data.t('search.search')}
+						</button>
+					</div>
+				{/snippet}
+				{#snippet resultItemRow({ resultItem, getCellId, isFocusedCell })}
+					{#if resultItem}
+						<div class="suggestion-card-container flex text-xs md:text-sm">
+							<SuggestionCard
+								item={resultItem}
+								cellId={getCellId(0)}
+								isFocused={isFocusedCell(0)}
+							/>
+						</div>
+					{/if}
+				{/snippet}
+			</SuperSearch>
+
+			<!--			return new URLSearchParams({-->
+			<!--			_q: query,-->
+			<!--			_limit: '8',-->
+			<!--			cursor: cursor.toString()-->
 			<!--			https://libris.kb.se/find.jsonld?q=kung&_lens=chips&_suggest=sv&_limit=7&@type=Library-->
 			<!--			https://beta.libris-qa.kb.se/find?_i=&_q=typ:Library&_limit=20&_spell=true-->
 		</div>
 	</div>
 </div>
+
+<style lang="postcss">
+	/* codemirror elements */
+
+	:global(.codemirror-container) {
+		@apply block flex-1;
+	}
+
+	:global(.codemirror-container .cm-scroller) {
+		@apply min-h-12 font-sans outline-none text-3-regular;
+		scrollbar-width: none;
+	}
+
+	:global(.supersearch-input .cm-line) {
+		@apply px-4;
+		min-height: 28px;
+		line-height: 28px;
+	}
+
+	:global(.supersearch-input .cm-focused) {
+		outline: none;
+	}
+
+	:global(.codemirror-container .cm-content) {
+		padding-top: 0.6125rem;
+		padding-bottom: 0.6125rem;
+		outline: none;
+	}
+</style>
