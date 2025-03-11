bfdcac2e6c8e7567eacba510e9776c7b7b29398c
Jesper Engström <jesper.engstrom@kb.se>
2025-03-11 15:42:21 +0100

    fix(lxlweb): remove show search mappings condition

--- lxl-web/src/routes/(app)/[[lang=lang]]/find/+page.svelte
+++ lxl-web/src/routes/(app)/[[lang=lang]]/find/+page.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
-	import { env } from '$env/dynamic/public';
 	import { page } from '$app/stores';
 	import SearchResult from '$lib/components/find/SearchResult.svelte';
 	import getPageTitle from '$lib/utils/getPageTitle';
-
-	let usingSuperSearch = $derived(
-		env?.PUBLIC_USE_SUPERSEARCH === 'true' || $page.url.searchParams.get('_x') === 'supersearch'
-	);
 </script>
 
 <svelte:head>
@@ -15,5 +10,5 @@
 <h1 class="sr-only">{$page.data.t('search.searchResults')}</h1>
 <SearchResult
 	searchResult={$page.data.searchResult}
-	showMapping={$page.url.searchParams.get('_x') === 'mappings' || !usingSuperSearch}
+	showMapping={$page.url.searchParams.get('_x') === 'mappings'}
 />
