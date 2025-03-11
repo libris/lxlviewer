833529724d4e3242468ede487960439ae1c9182d
Lars Rosenström <lars.rosenstrom@kb.se>
2025-03-11 14:48:22 +0100

    Working simplified request with debounce but without supersearch/codemirror

---
+++ lxl-web/src/lib/components/my-pages/Libraries.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import BiSearch from '~icons/bi/search';
+	import { useSearchRequest } from 'supersearch';
+
+	let searchPhrase = $state('');
+	let endpoint = '/api/my-pages';
+	let queryFn = (query: string) =>
+		new URLSearchParams({
+			_q: `${query} typ:Library`,
+			_limit: '10',
+			'@type': 'Library'
+		});
+	const debouncedWait = 300;
+
+	let search = useSearchRequest({
+		endpoint,
+		queryFn,
+		debouncedWait
+	});
+
+	function handleInputChange() {
+		search.debouncedFetchData(searchPhrase);
+		console.log(search.data);
+	}
+</script>
+
+<div class="container-fluid mb-12 mt-8 max-w-3xl page-padding">
+	<h1 class="mb-6 pl-2 text-6-cond-extrabold">Mina sidor</h1>
+	<h1 class="mb-2 pl-2 text-3-cond-bold">Bibliotek</h1>
+	<div class="container-fluid max-w-3xl rounded-md bg-primary/4 page-padding">
+		Hitta och lägg till favoritbibliotek
+		<div class="relative">
+			<BiSearch class="absolute left-2.5 top-6 text-sm text-icon" />
+			<input
+				bind:value={searchPhrase}
+				placeholder={$page.data.t('myPages.findLibrary')}
+				aria-label={$page.data.t('myPages.findLibrary')}
+				class="mt-3 w-full pl-8"
+				oninput={handleInputChange}
+				type="search"
+			/>
+			<div></div>
+			<!--			return new URLSearchParams({-->
+			<!--			_q: query,-->
+			<!--			_limit: '8',-->
+			<!--			cursor: cursor.toString()-->
+			<!--			https://libris.kb.se/find.jsonld?q=kung&_lens=chips&_suggest=sv&_limit=7&@type=Library-->
+			<!--			https://beta.libris-qa.kb.se/find?_i=&_q=typ:Library&_limit=20&_spell=true-->
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+</style>
