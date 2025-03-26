f9fede93122c7cf9e6e05c8833487168b08a5d74
Lars Rosenström <lars.rosenstrom@kb.se>
2025-03-26 14:14:32 +0100

    Make custom asResult function local

--- lxl-web/src/routes/api/[[lang=lang]]/my-pages/+server.ts
+++ lxl-web/src/routes/api/[[lang=lang]]/my-pages/+server.ts
@@ -1,8 +1,10 @@
 import { env } from '$env/dynamic/private';
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types.ts';
-import { getSupportedLocale } from '$lib/i18n/locales.js';
-import { asLibraryResult } from '$lib/utils/search.js';
+import { getSupportedLocale, type LocaleCode as LangCode } from '$lib/i18n/locales.js';
+import type { LibraryResult, PartialCollectionView } from '$lib/types/search';
+import { DisplayUtil, toString } from '$lib/utils/xl';
+import { JsonLd, LensType } from '$lib/types/xl';
 
 export const GET: RequestHandler = async ({ url, params, locals }) => {
 	const displayUtil = locals.display;
@@ -18,7 +20,23 @@ export const GET: RequestHandler = async ({ url, params, locals }) => {
 	const newSearchParams = new URLSearchParams([...Array.from(url.searchParams.entries())]);
 	const findRes = await fetch(`${env.API_URL}/find?${newSearchParams.toString()}`);
 	const data = await findRes.json();
-	const result = await asLibraryResult(data, displayUtil, locale);
+	const result = await asResult(data, displayUtil, locale);
 
 	return json(result);
 };
+
+async function asResult(
+	view: PartialCollectionView,
+	displayUtil: DisplayUtil,
+	locale: LangCode
+): Promise<LibraryResult> {
+	return {
+		totalItems: view.totalItems,
+		maxItems: view.maxItems,
+		items: view.items?.map((i) => ({
+			[JsonLd.ID]: i.meta[JsonLd.ID] as string,
+			label: toString(displayUtil.lensAndFormat(i, LensType.Chip, locale)) as string,
+			sigel: i.sigel as string
+		}))
+	};
+}
