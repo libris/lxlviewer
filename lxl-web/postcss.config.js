9f5a8a1c1a13025a5f5557988dc8b7eface8347a
Jesper Engström <jesper.engstrom@kb.se>
2025-04-10 14:45:51 +0200

    run update tool again in lxlweb

--- lxl-web/postcss.config.js
+++ lxl-web/postcss.config.js
@@ -1,9 +1,6 @@
 export default {
 	plugins: {
-		'postcss-import': {},
-		'tailwindcss/nesting': {},
-		tailwindcss: {},
-		autoprefixer: {}
+		'@tailwindcss/postcss': {}
 		// ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}) // we have currently disabled cssnano as it doesn't play along with container queries. This is however fixed in Svelte 5 (see: https://github.com/sveltejs/svelte/issues/10369#issuecomment-1983169720)
 	}
 };
