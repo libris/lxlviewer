fe5de6d269fee25e3edca8e5cc5228fcdedaeb27
Jesper Engström <jesper.engstrom@kb.se>
2025-04-10 15:18:52 +0200

    Add vite plugin

--- lxl-web/postcss.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-	plugins: {
-		'@tailwindcss/postcss': {}
-		// ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}) // we have currently disabled cssnano as it doesn't play along with container queries. This is however fixed in Svelte 5 (see: https://github.com/sveltejs/svelte/issues/10369#issuecomment-1983169720)
-	}
-};
