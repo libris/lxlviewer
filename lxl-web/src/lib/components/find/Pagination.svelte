b1a44eb10566a98e9af1181fa1fd5fa12bca8770
Jesper Engström <jesper.engstrom@kb.se>
2025-05-23 22:22:01 +0200

    Render sequence depending on screen size

--- lxl-web/src/lib/components/find/Pagination.svelte
+++ lxl-web/src/lib/components/find/Pagination.svelte
@@ -9,18 +9,28 @@
 	};
 
 	const { data }: PaginationProps = $props();
-
-	const numberOfPages = 7;
-
 	const { first, last, next, previous, totalItems, itemsPerPage, itemOffset, maxItems } =
 		$derived(data);
+
+	const numberOfPagesSm = 3;
+	const numberOfPagesMd = 7;
+
 	const showPagination = $derived(data.items.length > 0 && totalItems > itemsPerPage);
 	const currentPage = $derived(Math.floor(itemOffset / itemsPerPage) + 1);
 	const lastItem = $derived(totalItems > maxItems ? maxItems : totalItems);
 	const lastPage = $derived(Math.ceil(lastItem / itemsPerPage));
-	const sequenceSize = $derived(numberOfPages > lastPage ? lastPage : numberOfPages);
 
-	const pageSequence = $derived.by(() => {
+	const pageSequenceSm = $derived.by(() => {
+		let pagesSm = numberOfPagesSm > lastPage ? lastPage : numberOfPagesSm;
+		return getPages(pagesSm);
+	});
+
+	const pageSequenceMd = $derived.by(() => {
+		let pagesMd = numberOfPagesMd > lastPage ? lastPage : numberOfPagesMd;
+		return getPages(pagesMd);
+	});
+
+	function getPages(sequenceSize: number) {
 		let pages = [];
 		let halfSequence = Math.floor(sequenceSize / 2);
 
@@ -46,7 +56,7 @@
 			pages.push({ page: i, offset: itemsPerPage * (i - 1) });
 		}
 		return pages;
-	});
+	}
 
 	function getOffsetLink(offset: number) {
 		let o = offset < 0 ? 0 : offset;
@@ -54,82 +64,98 @@
 	}
 </script>
 
+{#snippet pagination(
+	pages: {
+		page: number;
+		offset: number;
+	}[]
+)}
+	<!-- prev -->
+	{#if previous}
+		<li>
+			<a
+				href={previous['@id']}
+				aria-label={page.data.t('search.previous')}
+				class="btn btn-primary border-0"><BiChevronLeft class="text-base" aria-hidden="true" /></a
+			>
+		</li>
+	{/if}
+	<!-- first -->
+	<li>
+		<a
+			aria-label="{page.data.t('search.page')} 1"
+			class={['btn btn-primary', currentPage === 1 ? 'bg-accent-50' : 'border-0']}
+			href={first['@id']}>1</a
+		>
+	</li>
+	{#if pages[0].page > 2}
+		<li class="text-2xs flex size-8 items-end justify-center pb-2 sm:size-9"><span>...</span></li>
+	{/if}
+	<!-- page sequence -->
+	{#each pages as p (p.offset)}
+		{#if p.page !== 1 && p.page !== lastPage}
+			<li>
+				<a
+					class={['btn btn-primary', p.page === currentPage ? 'bg-accent-50' : 'border-0']}
+					href={getOffsetLink(p.offset)}
+					aria-label="{page.data.t('search.page')} {p}"
+					aria-current={p.page === currentPage ? 'page' : null}
+					>{p.page.toLocaleString(page.data.locale)}</a
+				>
+			</li>
+		{/if}
+	{/each}
+	{#if lastPage - pages[pages.length - 1].page > 1}
+		<li class="text-2xs flex size-8 items-end justify-center pb-2 sm:size-9"><span>...</span></li>
+	{/if}
+	<!-- last -->
+	<li>
+		<a
+			aria-label="{page.data.t('search.page')} {lastPage}"
+			class={['btn btn-primary', currentPage === lastPage ? 'bg-accent-50' : 'border-0']}
+			href={last['@id']}>{lastPage.toLocaleString(page.data.locale)}</a
+		>
+	</li>
+	<!-- next -->
+	{#if next}
+		<li>
+			<a href={next['@id']} aria-label={page.data.t('search.next')} class="btn btn-primary border-0"
+				><BiChevronRight class="text-base" aria-hidden="true" /></a
+			>
+		</li>
+	{/if}
+{/snippet}
+
 {#if showPagination}
-	<nav aria-label={page.data.t('search.pagination')} data-testid="pagination">
-		<ul class="flex justify-center overflow-hidden p-4 sm:px-6">
-			<!-- prev -->
-			{#if previous}
-				<li>
-					<a
-						href={previous['@id']}
-						aria-label={page.data.t('search.previous')}
-						class="btn btn-primary border-0"
-						><BiChevronLeft class="text-base" aria-hidden="true" /></a
-					>
-				</li>
-			{/if}
-			<!-- first -->
-			<li>
-				<a
-					aria-label="{page.data.t('search.page')} 1"
-					class={['btn btn-primary', currentPage === 1 ? 'bg-accent-50' : 'border-0']}
-					href={first['@id']}>1</a
-				>
-			</li>
-			{#if pageSequence[0].page > 2}
-				<li class="hidden h-9 w-6 items-end pb-1 sm:flex"><span>...</span></li>
-			{/if}
-			<!-- page sequence -->
-			{#each pageSequence as p (p.offset)}
-				{#if p.page !== 1 && p.page !== lastPage}
-					<li>
-						<a
-							class={[
-								'btn btn-primary',
-								p.page === currentPage ? 'bg-accent-50' : 'hidden border-0 sm:flex'
-							]}
-							href={getOffsetLink(p.offset)}
-							aria-label="{page.data.t('search.page')} {p}"
-							aria-current={p.page === currentPage ? 'page' : null}
-							>{p.page.toLocaleString(page.data.locale)}</a
-						>
-					</li>
-				{/if}
-			{/each}
-			{#if lastPage - pageSequence[pageSequence.length - 1].page > 1}
-				<li class="hidden h-9 w-6 items-end pb-1 sm:flex"><span>...</span></li>
-			{/if}
-			<!-- last -->
-			<li>
-				<a
-					aria-label="{page.data.t('search.page')} {lastPage}"
-					class={['btn btn-primary', currentPage === lastPage ? 'bg-accent-50' : 'border-0']}
-					href={last['@id']}>{lastPage.toLocaleString(page.data.locale)}</a
-				>
-			</li>
-			<!-- next -->
-			{#if next}
-				<li>
-					<a
-						href={next['@id']}
-						aria-label={page.data.t('search.next')}
-						class="btn btn-primary border-0"
-						><BiChevronRight class="text-base" aria-hidden="true" /></a
-					>
-				</li>
-			{/if}
+	<nav
+		aria-label={page.data.t('search.pagination')}
+		data-testid="pagination"
+		class="mt-4 overflow-x-hidden py-4"
+	>
+		<!-- sm -->
+		<ul class="sm-pagination flex justify-center sm:hidden">
+			{@render pagination(pageSequenceSm)}
+		</ul>
+		<!-- md -->
+		<ul class="md-pagination hidden justify-center sm:flex">
+			{@render pagination(pageSequenceMd)}
 		</ul>
 	</nav>
 {/if}
 
 <style>
-	nav li > * {
+	.md-pagination li > * {
 		margin-inline: calc(var(--spacing) * 0.5);
 	}
 
-	nav li > a {
+	.md-pagination li > a {
 		min-height: calc(var(--spacing) * 9);
 		min-width: calc(var(--spacing) * 9);
 		padding-inline: calc(var(--spacing) * 2);
+	}
+
+	.sm-pagination li > a {
+		min-height: calc(var(--spacing) * 8);
+		min-width: calc(var(--spacing) * 8);
 	}
 </style>
