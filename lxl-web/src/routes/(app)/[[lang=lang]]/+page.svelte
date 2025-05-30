ca18d336eaee11c1b334e5c62444db833da8e668
Jesper Engström <jesperengstrom@users.noreply.github.com>
2025-05-30 12:26:54 +0200

    feat(lxlweb): reinstate search input for mobile find page (#1311)
    * Remove supersearch start 'show more', 'load more', re-add mobile input for find page
    * Improve display of titles in suggestions
    * fix facet badget shrinking too much with ellipsis
    * Hide free text operator in search mapping, get rid of apply

--- lxl-web/src/routes/(app)/[[lang=lang]]/+page.svelte
+++ lxl-web/src/routes/(app)/[[lang=lang]]/+page.svelte
@@ -9,7 +9,7 @@
 	<title>{getPageTitle()}</title>
 </svelte:head>
 
-<article class="mx-auto my-8 max-w-3xl p-6">
+<article class="mx-auto max-w-3xl p-6">
 	<div class="landing flex flex-col items-center gap-2">
 		<h1 class="font-heading flex text-5xl/tight font-[600] sm:text-6xl/tight">Libris</h1>
 	</div>
