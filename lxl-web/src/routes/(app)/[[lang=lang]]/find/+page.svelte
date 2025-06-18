378944a29957ad7d8583296c10639f415f591cf2
Jesper Engström <jesper.engstrom@kb.se>
2025-06-18 11:57:11 +0200

    Remove outcommented code

--- lxl-web/src/routes/(app)/[[lang=lang]]/find/+page.svelte
+++ lxl-web/src/routes/(app)/[[lang=lang]]/find/+page.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import SearchResult_2 from '$lib/components/find/SearchResult-2.svelte';
-	// import SearchResult from '$lib/components/find/SearchResult.svelte';
 	import getPageTitle from '$lib/utils/getPageTitle';
 </script>
 
 <svelte:head>
 	<title>{getPageTitle(page.url.searchParams.get('_q')?.trim())}</title>
 </svelte:head>
-<!-- <SearchResult
-	searchResult={page.data.searchResult}
-	showMapping={page.url.searchParams.get('_x') === 'mappings'}
-/> -->
 <SearchResult_2 searchResult={page.data.searchResult} />
