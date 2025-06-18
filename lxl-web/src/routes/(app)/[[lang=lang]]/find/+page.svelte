32689c5f62afdfb389cf6cbb99c73908be0b5888
Jesper Engström <jesper.engstrom@kb.se>
2025-06-18 13:45:47 +0200

    Move SearchResult-2 content to find/page.svelte, delete component

--- lxl-web/src/routes/(app)/[[lang=lang]]/find/+page.svelte
+++ lxl-web/src/routes/(app)/[[lang=lang]]/find/+page.svelte
@@ -1,10 +1,58 @@
 <script lang="ts">
 	import { page } from '$app/state';
-	import SearchResult_2 from '$lib/components/find/SearchResult-2.svelte';
+	import type { SearchResult } from '$lib/types/search';
 	import getPageTitle from '$lib/utils/getPageTitle';
+	import Toolbar from '$lib/components/Toolbar.svelte';
+	import LeadingPane from '$lib/components/find/LeadingPane.svelte';
+	import Filters from '$lib/components/find/Filters.svelte';
+	import SearchResultToolbar from '$lib/components/find/SearchResultToolbar.svelte';
+	import SearchResultInfo from '$lib/components/find/SearchResultInfo.svelte';
+	import SearchCard from '$lib/components/find/SearchCard.svelte';
+	import TrailingPanes from '$lib/components/find/TrailingPanes.svelte';
+	import Pagination from '$lib/components/find/Pagination.svelte';
+	import SiteFooter from '../SiteFooter.svelte';
+
+	const searchResult: SearchResult = $derived(page.data.searchResult);
 </script>
 
 <svelte:head>
 	<title>{getPageTitle(page.url.searchParams.get('_q')?.trim())}</title>
 </svelte:head>
-<SearchResult_2 searchResult={page.data.searchResult} />
+{#if searchResult}
+	<div class="search-result flex w-full flex-1">
+		<LeadingPane>
+			<Filters facets={searchResult.facetGroups || []} />
+		</LeadingPane>
+		<div class="@container/content flex flex-1 flex-col">
+			<div class="flex flex-1 flex-col @5xl/content:flex-row">
+				<main class="flex-1">
+					<h1 class="sr-only">{page.data.t('search.searchResults')}</h1>
+					<SearchResultToolbar {searchResult} />
+					<SearchResultInfo {searchResult} />
+					<ol class="flex flex-col px-4">
+						{#each searchResult.items as item (item['@id'])}
+							<li>
+								<SearchCard {item} />
+							</li>
+						{/each}
+					</ol>
+					<Pagination data={searchResult} />
+				</main>
+				<aside class="min-w-[300px]">
+					<div class="hidden @5xl/content:block">
+						<Toolbar />
+					</div>
+					<div class="aside-content sticky h-px"></div>
+				</aside>
+			</div>
+			<SiteFooter />
+		</div>
+		<TrailingPanes />
+	</div>
+{/if}
+
+<style>
+	.aside-content {
+		top: calc(var(--app-bar-height) + (var(--spacing) * 4));
+	}
+</style>
