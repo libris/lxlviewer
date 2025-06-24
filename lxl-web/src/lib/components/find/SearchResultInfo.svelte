5e880dcde0d5ef09ac2beeb458308906112b91e9
Jesper Engström <jesper.engstrom@kb.se>
2025-06-24 10:42:28 +0200

    Display info when mylibraries filter applied without fav lib

--- lxl-web/src/lib/components/find/SearchResultInfo.svelte
+++ lxl-web/src/lib/components/find/SearchResultInfo.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import type { SearchResult } from '$lib/types/search';
+	import { getUserSettings } from '$lib/contexts/userSettings';
+	import { MY_LIBRARIES_FILTER_ALIAS } from '$lib/constants/facets';
+	import BiInfo from '~icons/bi/info-circle';
 
 	type SearchResultInfoProps = {
 		searchResult: SearchResult;
 	};
 
 	const { searchResult }: SearchResultInfoProps = $props();
+	const userSettings = getUserSettings();
 	const numHits = $derived(searchResult.totalItems);
 </script>
 
@@ -16,7 +20,7 @@
 	data-testid="result-info"
 >
 	{#if numHits && numHits > 0}
-		<span class="hits-count">
+		<p class="hits-count">
 			{#if numHits > searchResult.itemsPerPage}
 				<span class="font-medium">
 					{(searchResult.itemOffset + 1).toLocaleString(page.data.locale)}
@@ -34,12 +38,12 @@
 				{numHits == 1 ? page.data.t('search.relatedOne') : page.data.t('search.related')}
 			{/if}
 			{numHits == 1 ? page.data.t('search.hitsOne') : page.data.t('search.hits')}
-		</span>
+		</p>
 	{:else}
-		<span class="hits-count">{page.data.t('search.noResults')}</span>
+		<p class="hits-count">{page.data.t('search.noResults')}</p>
 	{/if}
 	{#if searchResult._spell.length}
-		<span class="suggest">
+		<p class="suggest">
 			{#each searchResult._spell as suggestion (suggestion.label)}
 				{page.data.t('search.didYouMean')}
 				<a href={suggestion.view['@id'].replace('_spell=true', '_spell=false')} class="link-subtle">
@@ -47,9 +51,17 @@
 					{@html suggestion.labelHtml}</a
 				>?
 			{/each}
-		</span>
+		</p>
 	{/if}
 </div>
+<!-- no fav libraries + myLibraries filter warning -->
+{#if numHits === 0 && !userSettings.myLibraries?.length && page.url.search.includes(MY_LIBRARIES_FILTER_ALIAS)}
+	<div class="text-2xs mb-4 px-4" role="alert">
+		<BiInfo aria-hidden="true" class="text-subtle mb-0.5 inline align-middle" />
+		<p class="inline">{page.data.t('search.noAddedLibrariesText')}</p>
+		<a class="link inline" href="/my-pages">{page.data.t('search.addLibraries')}</a>
+	</div>
+{/if}
 
 <style>
 	.search-result-info {
