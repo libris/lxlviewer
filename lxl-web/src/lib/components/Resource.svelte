00e7395de847a723a8ccb7c57342e51275ef18fb
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2025-06-26 09:40:32 +0200

    Remove child paddings

--- lxl-web/src/lib/components/Resource.svelte
+++ lxl-web/src/lib/components/Resource.svelte
@@ -22,16 +22,14 @@
 		<TableOfContents items={tocItems} mobile />
 	</div>
 	<div
-		class="max-w-10xl mx-auto my-3 flex flex-col gap-3 sm:my-6 sm:gap-6 @3xl:grid @3xl:grid-cols-(--two-grid-cols) @7xl:grid-cols-(--three-grid-cols)"
+		class="max-w-10xl mx-auto flex flex-col gap-3 p-3 sm:gap-6 sm:p-6 @3xl:grid @3xl:grid-cols-(--two-grid-cols) @7xl:grid-cols-(--three-grid-cols)"
 	>
-		<div class="px-3 sm:px-6 @3xl:pr-0">
-			<div
-				class="border-neutral aspect-3/2 max-h-96 w-full border-b bg-neutral-50 @3xl:aspect-square @3xl:border-none"
-			>
-				Image
-			</div>
+		<div
+			class="border-neutral aspect-3/2 max-h-96 w-full border-b bg-neutral-50 @3xl:aspect-square @3xl:border-none"
+		>
+			Image
 		</div>
-		<div class="mx-auto w-full max-w-4xl px-3 sm:px-6 @3xl:pl-0">
+		<div class="mx-auto w-full max-w-4xl">
 			{#each { length: 5 }}
 				<p class="not-first:mt-6">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare ut tortor ac
@@ -46,11 +44,9 @@
 				</p>
 			{/each}
 		</div>
-		<div class=" pr-3 sm:pr-6">
-			<aside class="sticky top-0 hidden @7xl:block">
-				<TableOfContents items={tocItems} />
-			</aside>
-		</div>
+		<aside class="sticky top-0 hidden @7xl:block">
+			<TableOfContents items={tocItems} />
+		</aside>
 	</div>
 </article>
 
