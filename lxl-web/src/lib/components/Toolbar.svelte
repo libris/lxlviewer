25852a651e1ccd320d6e04d82509c608e8a03c20
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2025-07-10 09:32:41 +0200

    Add flex + gap to leading and trailing actions

--- lxl-web/src/lib/components/Toolbar.svelte
+++ lxl-web/src/lib/components/Toolbar.svelte
@@ -14,14 +14,14 @@
 	class="flex h-[var(--toolbar-height)] place-content-between items-center gap-2 overflow-hidden border-b border-b-neutral-200 px-3"
 >
 	{#if leadingActions}
-		<div>
+		<div class="flex gap-1">
 			{@render leadingActions()}
 		</div>
 	{/if}
 	{@render children?.()}
 
 	{#if trailingActions}
-		<div class="ml-auto">
+		<div class="ml-auto flex gap-1">
 			{@render trailingActions()}
 		</div>
 	{/if}
