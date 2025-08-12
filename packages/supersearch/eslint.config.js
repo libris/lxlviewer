d3167707d57e4d0e5a54f6014fcdfbfdd6de45e1
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2025-08-12 11:07:31 +0200

    Add comment from sveltekit starter template

--- packages/supersearch/eslint.config.js
+++ packages/supersearch/eslint.config.js
@@ -23,7 +23,11 @@ export default ts.config(
 				...globals.node
 			}
 		},
-		rules: { 'no-undef': 'off' }
+		rules: {
+			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
+			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors,
+			'no-undef': 'off'
+		}
 	},
 	{
 		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
