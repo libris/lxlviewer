578816806a5eddb190945556ebd134da77519b9f
Jesper Engström <jesper.engstrom@kb.se>
2025-08-18 15:50:30 +0200

    Manually attach onload handler to not violate csp

--- lxl-web/src/lib/components/Matomo.svelte
+++ lxl-web/src/lib/components/Matomo.svelte
@@ -2,6 +2,8 @@
 	import { env } from '$env/dynamic/public';
 	import { afterNavigate } from '$app/navigation';
 	import { setMatomoTracker, getMatomoTracker, setMatomoContext } from '$lib/contexts/matomo';
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
 	interface Props {
 		children?: import('svelte').Snippet;
 	}
@@ -9,6 +11,7 @@
 	let { children }: Props = $props();
 
 	const URL: string = env.PUBLIC_MATOMO_URL;
+	let scriptTag: HTMLScriptElement | undefined = $state();
 
 	setMatomoContext();
 	const tracker = getMatomoTracker();
@@ -33,11 +36,20 @@
 			}
 		}
 	});
+
+	onMount(() => {
+		if (browser) {
+			scriptTag?.addEventListener('load', onMatomoScriptLoad);
+		}
+		return () => {
+			scriptTag?.removeEventListener('load', onMatomoScriptLoad);
+		};
+	});
 </script>
 
 <svelte:head>
 	{#if URL}
-		<script async defer src={`${URL}/matomo.js`} on:load={onMatomoScriptLoad}></script>
+		<script bind:this={scriptTag} async defer src={`${URL}/matomo.js`}></script>
 	{/if}
 </svelte:head>
 {@render children?.()}
