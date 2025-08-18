fe6376a2667f0704663f452fc8827850ad2f3661
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2025-08-18 14:31:54 +0200

    Remove redundant search params from tests

--- lxl-web/src/lib/utils/addDefaultSearchParams.test.ts
+++ lxl-web/src/lib/utils/addDefaultSearchParams.test.ts
@@ -4,34 +4,19 @@ import addDefaultSearchParams from './addDefaultSearchParams';
 describe('addDefaultSearchParams', () => {
 	it('adds default search params if they are missing', () => {
 		expect(addDefaultSearchParams(new URLSearchParams())).toStrictEqual(
-			new URLSearchParams([
-				['_q', '*'],
-				['_limit', '20'],
-				['_offset', '0'],
-				['_sort', ''],
-				['_spell', 'true']
-			])
+			new URLSearchParams([['_q', '*']])
 		);
 	});
 	it("doesn't change initial params if they are already set", () => {
 		expect(addDefaultSearchParams(new URLSearchParams([['_q', 'test']]))).toStrictEqual(
-			new URLSearchParams([
-				['_q', 'test'],
-				['_limit', '20'],
-				['_offset', '0'],
-				['_sort', ''],
-				['_spell', 'true']
-			])
+			new URLSearchParams([['_q', 'test']])
 		);
 	});
 	it("don't reset the offset if set", () => {
 		expect(addDefaultSearchParams(new URLSearchParams([['_offset', '30']]))).toStrictEqual(
 			new URLSearchParams([
 				['_offset', '30'],
-				['_q', '*'],
-				['_limit', '20'],
-				['_sort', ''],
-				['_spell', 'true']
+				['_q', '*']
 			])
 		);
 	});
