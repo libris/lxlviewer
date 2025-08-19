0e7d15c4c5f89c9fd5c4fd9373fe66bded0e6a49
Johan Bisse Mattsson <johan@johanbissemattsson.se>
2025-08-19 11:21:58 +0200

    chore(lxl-web): Add test more tests (#1351)
    * Add test ensuring qualifier value has styling
    
    * Test if access filters can be added and removed

--- lxl-web/tests/supersearch-lxlweb.spec.ts
+++ lxl-web/tests/supersearch-lxlweb.spec.ts
@@ -93,6 +93,13 @@ test('qualifier keys can be added using the user interface', async ({ page }) =>
 		'buttons for adding qualifier keys is hidden after selecting one of them'
 	).toBeHidden();
 	await expect(
+		page.getByRole('dialog').getByRole('combobox').locator('.lxl-qualifier-value')
+	).toBeVisible();
+	await expect(
+		page.getByRole('dialog').getByRole('combobox').locator('.lxl-qualifier-value'),
+		'qualifier value is initially empty but has styling'
+	).toContainText('""');
+	await expect(
 		page.getByRole('dialog').getByLabel('Förslag').getByRole('link').filter({ hasText: 'Person' }),
 		'all suggestions are persons'
 	).toHaveCount(5);
@@ -186,4 +193,27 @@ test('clear button clears input field', async ({ page }) => {
 	await expect(page.getByRole('combobox').last(), 'Can type again after clear').toContainText(
 		'hello'
 	);
+});
+
+test('access filters can be added/removed', async ({ page }) => {
+	await page.getByRole('combobox').fill('hej');
+	await page.keyboard.press('Enter');
+	await page.getByText('Fritt online').click();
+	await expect(page, 'user can add access filters').toHaveURL('/find?_q=hej+freeOnline');
+	await page.getByLabel('Rensa').first().click();
+	await expect(page, 'user can remove access filters by pressing clear icon').toHaveURL(
+		'/find?_q=hej+freeOnline'
+	);
+	await page.getByText('Fritt online').first().click();
+	await expect(page).toHaveURL('/find?_q=hej+freeOnline');
+	await expect(page.getByRole('combobox').first()).toContainText('hej Fritt online');
+
+	await page.getByTestId('main-search').click();
+	await page.keyboard.press('Backspace');
+	await page.keyboard.press('Backspace');
+	await expect(
+		page.getByRole('dialog').getByRole('combobox'),
+		'user can remove access filters by pressing backspace to remove pill'
+	).not.toContainText('Fritt online');
+	await expect(page.getByRole('dialog').getByRole('combobox')).toContainText('hej');
 });
