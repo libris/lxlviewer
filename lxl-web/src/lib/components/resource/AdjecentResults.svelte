f770766f1699ca7e591282c46d8a094408b469e9
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2025-09-01 15:12:32 +0200

    Shrink height of adjecent results nav (and table of contents)

--- lxl-web/src/lib/components/resource/AdjecentResults.svelte
+++ lxl-web/src/lib/components/resource/AdjecentResults.svelte
@@ -110,7 +110,7 @@
 {/snippet}
 
 {#if currentSearchResult}
-	<div class="text-2xs flex min-h-12 items-center gap-2 px-3 sm:px-6 @7xl:text-xs">
+	<div class="text-2xs flex min-h-10 items-center gap-2 px-3 sm:px-6 @7xl:min-h-11 @7xl:text-xs">
 		{#if typeof indexOfTotalSearchResults === 'number'}
 			<a href={relativizeUrl(currentSearchResult['@id'])} class="link whitespace-nowrap">
 				<span class="@xl:hidden">{page.data.t('resource.showInSearchResultsShort')}</span>
