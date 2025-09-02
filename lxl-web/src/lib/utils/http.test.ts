f6dbaae9d9a4cf0e2e6ee659e45c2c0d7e603318
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2025-09-02 13:23:30 +0200

    Fix test

--- lxl-web/src/lib/utils/http.test.ts
+++ lxl-web/src/lib/utils/http.test.ts
@@ -4,12 +4,7 @@ import { relativizeUrl } from './http';
 describe('relativize', () => {
 	it('formats an absolute URL to a relative path', () => {
 		expect(relativizeUrl('https://libris.kb.se/grz79wcldbmgz6jr')).toStrictEqual(
-			'grz79wcldbmgz6jr'
-		);
-	});
-	it('removes the opening slash from a relative path', () => {
-		expect(relativizeUrl('/find?q=*&@type=Work&_limit=20')).toStrictEqual(
-			'find?q=*&@type=Work&_limit=20'
+			'/grz79wcldbmgz6jr'
 		);
 	});
 });
