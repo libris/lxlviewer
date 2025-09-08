ba1809cb5d95de2019f54d1d0fdd3f1e0cdd6af8
Lars Rosenström <lars.rosenstrom@kb.se>
2025-09-08 16:48:22 +0200

    Migrate to ESLint 9, (.eslintrc.js -> eslint.config.js)

---
+++ cataloging/eslint.config.js
@@ -0,0 +1,64 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginVue from "eslint-plugin-vue";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ignores: ["**/node_modules/**"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    plugins: {js},
+    extends: [
+      "js/recommended"
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        runtimeConfig: 'readonly'
+      }
+    },
+    rules: {
+      'no-use-before-define': 'off',
+      'no-plusplus': 'off',
+      'no-restricted-syntax': 'off',
+      'no-console': 'off',
+      'no-prototype-builtins': 'off',
+      'prefer-destructuring': 'off',
+      'prefer-object-spread': 'off',
+      'no-underscore-dangle': 'off',
+      'prefer-promise-reject-errors': 'off',
+      'no-cond-assign': 'off',
+      'object-shorthand': 'off',
+      'import/no-cycle': 'off',
+      'import/extensions': 'off',
+      'no-param-reassign': 'off',
+      'import/prefer-default-export': 'off',
+      'import/no-dynamic-require': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'object-curly-newline': [
+        'error',
+        {
+          ImportDeclaration: 'never',
+        },
+      ],
+      'global-require': 'off',
+      radix: 'off',
+      'no-alert': 'off',
+      'no-continue': 'off',
+      'vue/max-len': ['error', {
+        code: 150,
+        template: 150,
+        comments: 150,
+      }],
+      'vue/multi-word-component-names': ['error', {
+        ignores: ['field', 'breadcrumb', 'spinner', 'notification', 'facet', 'sort', 'Find', 'Inspector', 'Login'],
+      }],
+    }
+  },
+  pluginVue.configs["flat/essential"],
+]);
+
+
+
