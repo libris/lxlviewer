ceb1f81ee4f98664f14f90cb4f7bcd47203f7a43
Jesper Engström <jesperengstrom@users.noreply.github.com>
2025-10-13 15:19:49 +0200

    fix(lxlweb): Fix tests after type normalization (#1387)

--- lxl-web/src/lib/components/find/FacetGroup.svelte
+++ lxl-web/src/lib/components/find/FacetGroup.svelte
@@ -134,13 +134,14 @@
 			<span class="flex-1 whitespace-nowrap">{group.label}</span>
 		</summary>
 		<!-- sorting -->
-		<div class="facet-sort absolute top-0 right-2 size-8" data-testid="facet-sort">
+		<div class="facet-sort absolute top-0 right-2 size-8">
 			<select
 				name={group.dimension}
 				bind:value={currentSort}
 				onchange={saveUserSort}
 				class="btn btn-primary size-full appearance-none border-0 text-transparent"
 				aria-label={page.data.t('sort.sort') + ' ' + page.data.t('search.filters')}
+				data-testid={`facet-sort-${group.dimension}`}
 			>
 				{#each sortOptions as option (option.value)}
 					<option selected={option.value == currentSort} value={option.value}>{option.label}</option
--- lxl-web/tests/find.spec.ts
+++ lxl-web/tests/find.spec.ts
@@ -55,14 +55,15 @@ test('expanded filters have no detectable a11y issues', async ({ page }) => {
 	await expect.soft(accessibilityScanResults.violations).toEqual([]);
 });
 
-test('sorting the facet sets a cookie', async ({ page, context }) => {
+test('opening and sorting the facet sets a cookie', async ({ page, context }) => {
 	const beforeCookies = await context.cookies();
 	await expect(beforeCookies).toEqual([]);
-	await page.getByTestId('facet-sort').nth(1).getByRole('combobox').selectOption('alpha.asc');
+	await page.locator('summary').filter({ hasText: 'Språk' }).click();
+	await page.getByTestId('facet-sort-language').selectOption('alpha.asc');
 	const afterCookies = await context.cookies();
 	await expect(afterCookies[0].name).toEqual('userSettings');
 	await expect(afterCookies[0].value).toEqual(
-		'{%22leadingPane%22:{%22open%22:true}%2C%22facetSort%22:{%22rdf:type%22:%22alpha.asc%22}}'
+		'{%22leadingPane%22:{%22open%22:true}%2C%22facetExpanded%22:{%22language%22:%22OPEN%22}%2C%22facetSort%22:{%22language%22:%22alpha.asc%22}}'
 	);
 });
 
@@ -103,20 +104,17 @@ test('myLibraries without favourite libraries shows a message', async ({ page })
 	await expect(page.getByTestId('my-libraries-warning')).toBeVisible();
 });
 
-// Comment out test that fails in CI
+test('user sorting is persisted after navigating', async ({ page }) => {
+	await page.locator('summary').filter({ hasText: 'Språk' }).click();
+	const langFacetSort = await page.getByTestId('facet-sort-language');
+	await expect(langFacetSort).toHaveValue('hits.desc');
+	await langFacetSort.selectOption('alpha.asc');
+	await page.goto('/find?_q=a&_limit=20&_offset=0&_sort=&_i=f');
 
-// test('user sorting is persisted after navigating', async ({ page }) => {
-// 	const firstSelect = await page
-// 		.getByTestId('facet-sort')
-// 		.first()
-// 		.getByRole('combobox')
-// 		.inputValue();
-// 	expect(firstSelect).toBe('hits.desc');
-// 	await page.getByTestId('facet-sort').first().getByRole('combobox').selectOption('alpha.asc');
-// 	await page.goto('/find?_q=a&_limit=20&_offset=0&_sort=&_i=f');
-// 	const newSelect = await page.getByTestId('facet-sort').first().getByRole('combobox').inputValue();
-// 	expect(newSelect).toBe('alpha.asc');
-// });
+	await page.locator('summary').filter({ hasText: 'Språk' }).click();
+	const langFacetSortNew = await page.getByTestId('facet-sort-language');
+	await expect(langFacetSortNew).toHaveValue('alpha.asc');
+});
 
 test('displays hits info', async ({ page }) => {
 	await expect(page.getByTestId('result-info')).toBeVisible();
