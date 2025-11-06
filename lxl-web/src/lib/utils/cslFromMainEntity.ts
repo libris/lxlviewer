43e72796ccd2ff80f341efef839b5f9aa2c526d1
Jesper Engström <jesper.engstrom@kb.se>
2025-11-06 17:31:27 +0100

    Assign a role to every contributor, add performer etc, map to article-journal

--- lxl-web/src/lib/utils/cslFromMainEntity.ts
+++ lxl-web/src/lib/utils/cslFromMainEntity.ts
@@ -1,12 +1,10 @@
 import jmespath from 'jmespath';
-import type { CSLJSON, CSLName, CSLType } from '$lib/types/citation';
+import type { CSLJSON, CSLName, CSLRoles, CSLType } from '$lib/types/citation';
 import { JsonLd, type FramedData } from '$lib/types/xl';
 import getTypeLike, { slug, type TypeLike } from './getTypeLike';
 import type { VocabUtil } from './xl';
 
-const agentPicker = `{family: agent.familyName, given: agent.givenName, literal: agent.name}`;
-
-const CSL_KBV_MAPPING: Record<keyof CSLJSON, string> = {
+const CSL_KBV_MAPPING: Partial<Record<keyof CSLJSON, string>> = {
 	id: `"@id"`,
 	type: `instanceOf."@type"`,
 	title: `hasTitle[0].computedLabel`,
@@ -25,19 +23,23 @@ const CSL_KBV_MAPPING: Record<keyof CSLJSON, string> = {
 	abstract: `join(', ', summary[].computedLabel)`,
 	keyword: `join(', ', instanceOf.subject[].computedLabel)`,
 	issue: `part`,
-	volume: `join(', ', [?"@type"=='Serial'].part[?"@type"=='Monograph'].mainEntity.hasTitle[].hasPart[].partNumber)`,
-
-	author: `instanceOf.contribution[?contains((role[]."@id" || [role."@id"]), 'https://id.kb.se/relator/author')].${agentPicker}`,
-	editor: `instanceOf.contribution[?contains((role[]."@id" || [role."@id"]), 'https://id.kb.se/relator/editor')].${agentPicker}`,
-	composer: `instanceOf.contribution[?contains((role[]."@id" || [role."@id"]), 'https://id.kb.se/relator/composer')].${agentPicker}`,
-	director: `instanceOf.contribution[?(contains((role[]."@id" || [role."@id"]),'https://id.kb.se/relator/filmDirector') || contains((role[]."@id" || [role."@id"]),'https://id.kb.se/relator/director') || contains((role[]."@id" || [role."@id"]),'https://id.kb.se/relator/televisionDirector'))].${agentPicker}`,
-	illustrator: `instanceOf.contribution[?contains((role[]."@id" || [role."@id"]), 'https://id.kb.se/relator/illustrator')].${agentPicker}`,
-	interviewer: `instanceOf.contribution[?contains((role[]."@id" || [role."@id"]), 'https://id.kb.se/relator/interviewer')].${agentPicker}`,
-	translator: `instanceOf.contribution[?contains((role[]."@id" || [role."@id"]), 'https://id.kb.se/relator/translator')].${agentPicker}`
+	volume: `join(', ', [?"@type"=='Serial'].part[?"@type"=='Monograph'].mainEntity.hasTitle[].hasPart[].partNumber)`
 };
 
+interface Contributor {
+	'@type'?: string;
+	role?: { '@id'?: string }[];
+	agent: Agent | Agent[];
+}
+
+interface Agent {
+	familyName?: string;
+	givenName?: string;
+	name?: string;
+}
+
 export function cslFromMainEntity(mainEntity: FramedData, vocabUtil: VocabUtil): CSLJSON[] {
-	const result: Partial<CSLJSON> = {};
+	let result: Partial<CSLJSON> = {};
 
 	for (const [key, expr] of Object.entries(CSL_KBV_MAPPING) as [keyof CSLJSON, string][]) {
 		try {
@@ -50,29 +52,109 @@ export function cslFromMainEntity(mainEntity: FramedData, vocabUtil: VocabUtil):
 		}
 	}
 
-	// Put other contributors as author fallback
-	if (!result?.author?.length) {
-		let constributors: CSLName[] | undefined;
-		try {
-			constributors = jmespath.search(
-				mainEntity,
-				`instanceOf.contribution[?"@type"=='PrimaryContribution'].${agentPicker}`
-			);
-			if (!constributors?.length) {
-				constributors = jmespath.search(
-					mainEntity,
-					`instanceOf.contribution[?"@type"=='Contribution'].${agentPicker}`
-				);
-				result.author = constributors;
-			}
-		} catch {
-			// do nothing
-		}
-	}
-
+	const contributors = mapContribution(mainEntity.instanceOf?.contribution as Contributor[]);
+	result = { ...result, ...contributors };
 	result.type = getCslType(mainEntity, vocabUtil);
 
 	return [result as CSLJSON];
+}
+
+function mapContribution(contribution: Contributor[]): Partial<CSLJSON> {
+	const result: Partial<CSLJSON> = {};
+
+	if (contribution && Array.isArray(contribution)) {
+		for (const contributor of contribution) {
+			const roleEntry = getRole(contributor);
+			if (roleEntry) {
+				for (const [role, name] of Object.entries(roleEntry) as [keyof CSLRoles, CSLName[]][]) {
+					addContributor(result, role, name);
+				}
+			} else {
+				const fallbackRole = getFallbackRole(contributor);
+				if (fallbackRole) {
+					addContributor(result, 'author', fallbackRole);
+				}
+			}
+		}
+	}
+	return result;
+}
+
+function addContributor(result: Partial<CSLJSON>, role: keyof CSLRoles, names: CSLName[]) {
+	if (Array.isArray(result[role])) {
+		result[role].push(...names);
+	} else {
+		result[role] = names;
+	}
+}
+
+function getRole(contributor: Contributor): Partial<CSLRoles> | false {
+	if (contributor.role) {
+		let role: string | undefined = contributor.role?.[0]?.['@id'];
+		if (role) {
+			role = role.replace('https://id.kb.se/relator/', '');
+			const name = getName(contributor.agent);
+			switch (role) {
+				case 'author':
+					return { author: name };
+				case 'editor':
+					return { editor: name };
+				case 'composer':
+					return { composer: name };
+				case 'filmDirector':
+				case 'director':
+					return { director: name };
+				case 'illustrator':
+					return { illustrator: name };
+				case 'interviewer':
+					return { interviewer: name };
+				case 'translator':
+					return { translator: name };
+				case 'producer':
+				case 'filmProducer':
+					return { producer: name };
+				case 'narrator':
+					return { narrator: name };
+				case 'performer':
+				case 'musician':
+				case 'singer':
+				case 'arranger':
+					return { performer: name };
+				case 'compiler':
+					return { compiler: name };
+				case 'curator':
+					return { curator: name };
+				default:
+					return false;
+			}
+		}
+	}
+	return false;
+}
+
+function getFallbackRole(contributor: Contributor) {
+	if (contributor['@type'] === 'PrimaryContribution' || contributor['@type'] === 'Contribution') {
+		const name = getName(contributor.agent);
+		if (name) {
+			return name;
+		}
+	}
+	return false;
+}
+
+function getName(agent: Agent | Agent[]): CSLName[] {
+	const flattenedAgent = Array.isArray(agent) ? agent[0] : agent;
+	const name: Partial<CSLName> = {};
+	if (flattenedAgent.familyName) {
+		name.family = flattenedAgent.familyName;
+	}
+	if (flattenedAgent.givenName) {
+		name.given = flattenedAgent.givenName;
+	}
+	if (flattenedAgent.name) {
+		name.literal = flattenedAgent.name;
+	}
+	return [name as CSLName];
 }
 
 // see https://github.com/citation-style-language/schema/blob/master/schemas/input/csl-data.json
@@ -88,7 +170,7 @@ function getCslType(mainEntity: FramedData, vocabUtil: VocabUtil): CSLType {
 	if (allTypes.includes('Monograph')) {
 		if (allTypes.includes('Text')) {
 			if (allTypes.includes('ComponentPart')) {
-				return 'article';
+				return 'article-journal';
 			}
 			if (allTypes.includes('Thesis')) {
 				return 'thesis';
