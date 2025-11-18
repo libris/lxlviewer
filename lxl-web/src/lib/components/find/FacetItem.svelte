9c3b65840cd9e9c143aff25506312b34c7d9581d
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2025-11-18 16:51:58 +0100

    Add checkboxes

--- lxl-web/src/lib/components/find/FacetItem.svelte
+++ lxl-web/src/lib/components/find/FacetItem.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
 	import { page } from '$app/state';
+	import { goto } from '$app/navigation';
 	import DecoratedDataLite from '$lib/components/DecoratedDataLite.svelte';
 
 	let { data, level }: { data: unknown; level: number } = $props();
+
 	const href = $derived(data.view ? page.data.localizeHref(data.view['@id']) : undefined);
+	let selected = $derived(data?.selected);
+	const hasCheckbox = $derived(
+		data.parentFacet?.operator === 'OR' /* || data?.facets || (data.selected && level > 1) */
+	);
+
+	function handleToggleSelected() {
+		if (hasCheckbox) {
+			selected = !selected;
+			goto(href);
+		}
+	}
 </script>
 
 {#snippet label()}
@@ -27,32 +40,39 @@
 	class={[
 		'flex min-h-9 w-full items-stretch justify-between text-sm',
 		level === 1 && 'text-subtle hover:text-body min-h-10 font-medium',
-		level > 1 && 'text-subtle/85 hover:text-subtle focus-within:text-subtle pl-4',
-		data?.selected && 'underline'
+		level > 1 && 'text-subtle/85 hover:text-subtle focus-within:text-subtle pl-4'
 	]}
 >
 	{#if data.view}
-		<a
-			{href}
-			class={[
-				'hover:bg-accent-50/50 flex w-full min-w-0 items-center justify-between gap-2 rounded-md px-3'
-			]}
-			data-sveltekit-preload-data="false"
-		>
-			{@render label()}
-			{#if data.totalItems && (data.parentFacet?.operator === 'OR' || (data.parentFacet?.operator === 'AND' && !data.selected))}
-				<span class="badge">
-					{data.totalItems.toLocaleString(page.data.locale)}
-					<span class="sr-only">
-						{#if data.totalItems === 1}
-							{page.data.t('search.hitsOne')}
-						{:else}
-							{page.data.t('search.hits')}
-						{/if}
-					</span>
-				</span>
+		<div class="flex w-full items-center">
+			{#if hasCheckbox}
+				<input type="checkbox" bind:checked={selected} onclick={handleToggleSelected} />
 			{/if}
-		</a>
+			<a
+				{href}
+				class={[
+					'hover:bg-accent-50/50 flex w-full min-w-0 items-center justify-between gap-2 rounded-md px-3',
+					selected && 'bg-accent-50'
+				]}
+				tabindex={hasCheckbox ? -1 : undefined}
+				onclick={handleToggleSelected}
+				data-sveltekit-preload-data="false"
+			>
+				{@render label()}
+				{#if data.totalItems && (data.parentFacet?.operator === 'OR' || (data.parentFacet?.operator === 'AND' && !selected))}
+					<span class="badge">
+						{data.totalItems.toLocaleString(page.data.locale)}
+						<span class="sr-only">
+							{#if data.totalItems === 1}
+								{page.data.t('search.hitsOne')}
+							{:else}
+								{page.data.t('search.hits')}
+							{/if}
+						</span>
+					</span>
+				{/if}
+			</a>
+		</div>
 	{:else}
 		<div class={['flex items-center justify-between px-3']}>
 			{@render label()}
