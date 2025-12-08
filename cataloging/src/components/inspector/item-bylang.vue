f7412a2b3030fa4c78b1dc1d2748a33d0348d090
Lars Rosenström <lars.rosenstrom@kb.se>
2025-12-08 10:36:05 +0100

    fix(cataloging/enrich): Fix regression where field properties are selectable in the old detailed enrichment function

--- cataloging/src/components/inspector/item-bylang.vue
+++ cataloging/src/components/inspector/item-bylang.vue
@@ -33,7 +33,7 @@ export default {
     },
     isEnrichmentSource: {
       type: Boolean,
-      default: true,
+      default: false,
     }
   },
   components: {
--- cataloging/src/components/inspector/item-entity.vue
+++ cataloging/src/components/inspector/item-entity.vue
@@ -30,7 +30,7 @@ export default {
     },
     isEnrichmentSource: {
       type: Boolean,
-      default: true,
+      default: false,
     }
   },
   data() {
