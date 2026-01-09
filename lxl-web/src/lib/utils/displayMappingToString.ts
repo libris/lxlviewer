1ce0ca386646429ea4e40a68df184a335cae0275
Jesper Engström <jesper.engstrom@kb.se>
2026-01-09 13:40:13 +0100

    display invalid keys in search mapping pills

--- lxl-web/src/lib/utils/displayMappingToString.ts
+++ lxl-web/src/lib/utils/displayMappingToString.ts
@@ -31,7 +31,7 @@ export function displayMappingToString(mapping: DisplayMapping[]): string {
 		}
 		return result
 			.filter((m) => !!m)
-			.join(', ')
+			.join(' ')
 			.trim();
 	}
 	return '';
