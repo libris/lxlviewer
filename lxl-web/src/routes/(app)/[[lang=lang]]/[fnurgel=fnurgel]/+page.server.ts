818ecc084925e9ee046f045f87de3130116c19a2
Jesper Engström <jesper.engstrom@kb.se>
2026-01-13 10:10:34 +0100

    fix(lxlweb): work-centered popovers

--- lxl-web/src/routes/(app)/[[lang=lang]]/[fnurgel=fnurgel]/+page.server.ts
+++ lxl-web/src/routes/(app)/[[lang=lang]]/[fnurgel=fnurgel]/+page.server.ts
@@ -65,6 +65,16 @@ export const load = async ({ params, locals, fetch, url }) => {
 	const mainEntity = { ...centerOnWork(resource['mainEntity'] as FramedData) };
 	copyMediaLinksToWork(mainEntity);
 
+	const workCard = asSearchResultItem(
+		[mainEntity],
+		displayUtil,
+		vocabUtil,
+		locale,
+		env.AUXD_SECRET,
+		myLibraries,
+		undefined
+	)[0];
+
 	resourceId = resource.mainEntity['@id'];
 
 	const typeLike = getTypeLike(mainEntity, vocabUtil);
@@ -272,16 +282,6 @@ export const load = async ({ params, locals, fetch, url }) => {
 		bibIdData: getBibIdsByInstanceId(mainEntity, displayUtil, resource, locale),
 		holdingLibraries: getHoldingLibraries(byType)
 	};
-
-	const workCard = asSearchResultItem(
-		[resource.mainEntity],
-		displayUtil,
-		vocabUtil,
-		locale,
-		env.AUXD_SECRET,
-		myLibraries,
-		undefined
-	)[0];
 
 	const subsetMapping = locals?.subsetMapping;
 	const refinedOrgs = getRefinedOrgs(myLibraries, [subsetMapping, searchResult?.mapping]);
