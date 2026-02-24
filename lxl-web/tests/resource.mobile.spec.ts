d930dd3ba9e6fa2201c1ee10ec1d42c048e75291
Johan Bisse Mattsson <bisse.mattsson@kb.se>
2026-02-24 10:17:29 +0100

    Bump codemirror packages to get placeholder fix
    An excluded element rule is needed as adding aria-placeholder on comboboxes raises a false alarm in the a11y tests

--- lxl-web/tests/resource.mobile.spec.ts
+++ lxl-web/tests/resource.mobile.spec.ts
@@ -4,7 +4,9 @@ test.use({ ...devices['iPhone 13'] });
 
 test('should not have any detectable a11y issues', async ({ page }) => {
 	await page.goto('/h08ndxddfg5v2pjf');
-	const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+	const accessibilityScanResults = await new AxeBuilder({ page })
+		.exclude('[role="combobox"][aria-placeholder]') // aria-placeholder issue in supersearch is a false alarm (see https://github.com/w3c/aria/issues/2689)
+		.analyze();
 	expect.soft(accessibilityScanResults.violations).toEqual([]);
 });
 
