00b117bc78a78f44b0cd1d608a95267ce49255b2
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2026-03-13 16:52:04 +0100

    Add default query

--- lxl-web/src/routes/(app)/[[lang=lang]]/sparql2/+page.svelte
+++ lxl-web/src/routes/(app)/[[lang=lang]]/sparql2/+page.svelte
@@ -1,4 +1,20 @@
 <script lang="ts">
+	import { page } from '$app/state';
+
+	const DEFAULT_QUERY = `BASE <${page.data.sparqlBaseUrl}/>
+PREFIX ktg: <https://id.kb.se/term/ktg/>
+PREFIX marc: <https://id.kb.se/marc/>
+PREFIX saogf: <https://id.kb.se/term/saogf/>
+PREFIX saobf: <https://id.kb.se/term/saobf/>
+PREFIX : <https://id.kb.se/vocab/>
+
+SELECT ?type (count(?s) as ?count) WHERE {
+  ?s a ?type.
+  FILTER isIRI(?s)
+}
+ORDER BY DESC(?count)
+      `;
+
 	import { onMount } from 'svelte';
 	import '@triply/yasgui/build/yasgui.min.css';
 
@@ -9,8 +25,13 @@
 		const Yasgui = module.default;
 
 		new Yasgui(document.getElementById('yasgui'), {
-			requestConfig: { endpoint: 'https://libris-qa.kb.se/sparql' },
-			copyEndpointOnNewTab: true
+			requestConfig: {
+				endpoint: `${page.data.sparqlBaseUrl}/sparql`
+			},
+			copyEndpointOnNewTab: true,
+			yasqe: {
+				value: DEFAULT_QUERY
+			}
 		});
 	});
 </script>
