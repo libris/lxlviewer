9d530737c35f826cc1b70bdc13455cde7b2885e0
Jesper Engström <jesper.engstrom@kb.se>
2026-03-18 14:55:05 +0100

    Fix blue buttons for subset

--- lxl-web/src/lib/components/ResourceHoldings.svelte
+++ lxl-web/src/lib/components/ResourceHoldings.svelte
@@ -5,9 +5,10 @@
 	import type { HoldingsData } from '$lib/types/holdings';
 	import type { SearchResultItem } from '$lib/types/search';
 	import type { ResourceData } from '$lib/types/resourceData';
+	import { LxlLens } from '$lib/types/display';
+	import { getLibraryIdsFromMapping } from '$lib/utils/getLibraryIdsFromMapping';
 	import { getHoldingsLink, getLibsFromHoldings, handleClickHoldings } from '$lib/utils/holdings';
 	import MyLibsHoldingIndicator from '$lib/components/MyLibsHoldingIndicator.svelte';
-	import { LxlLens } from '$lib/types/display';
 
 	interface Props {
 		instances: SearchResultItem[] | ResourceData[];
@@ -16,6 +17,9 @@
 
 	let { holdings, instances }: Props = $props();
 	const { myLibraries } = getUserSettings();
+	const subsetLibraries = $derived(
+		getLibraryIdsFromMapping([page.data.subsetMapping]) || undefined
+	);
 
 	function getLocalizedType(type: string) {
 		const found = instances.find((instanceItem) => type === instanceItem[JsonLd.TYPE]);
@@ -32,9 +36,20 @@
 			holdings.byType[type],
 			page.data.refinedOrgs
 		)}
+		{@const subsetHoldingByType = getLibsFromHoldings(
+			subsetLibraries,
+			holdings.byType[type],
+			page.data.refinedOrgs
+		)}
 		<li class="@md:self-center">
+			<!-- use regular btn if subset library but no holding for type -->
 			<a
-				class="btn btn-cta @md:max-w-sm"
+				class={[
+					'btn @md:max-w-sm',
+					!subsetLibraries || (subsetLibraries && subsetHoldingByType)
+						? 'btn-cta'
+						: 'btn-primary h-10 rounded-full text-sm'
+				]}
 				href={page.data.localizeHref(getHoldingsLink(page.url, type))}
 				data-sveltekit-preload-data="false"
 				data-testid="holding-link"
@@ -48,10 +63,16 @@
 				<span class="text-nowrap">{getLocalizedType(type)}</span>
 				<span class="truncate font-normal opacity-90">
 					{' · '}
-					{holdings.byType[type].length}
-					{holdings.byType[type].length === 1
-						? page.data.t('holdings.library')
-						: page.data.t('holdings.libraries')}
+					{#if subsetLibraries && subsetHoldingByType}
+						<span>{page.data.t('holdings.findTitle')}</span>
+					{:else}
+						<span>
+							{holdings.byType[type].length}
+							{holdings.byType[type].length === 1
+								? page.data.t('holdings.library')
+								: page.data.t('holdings.libraries')}
+						</span>
+					{/if}
 				</span>
 			</a>
 		</li>
