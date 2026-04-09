b2ed2b53953bbedf876f4f9045447d883b26ad8f
Jesper Engström <jesperengstrom@users.noreply.github.com>
2026-04-09 16:09:42 +0200

    fix(lxlweb): fix Zotero tag (#1544)
    * Fix <abbr> tag
    * Write tests

---
+++ lxl-web/tests/cite.spec.ts
@@ -0,0 +1,18 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('Zotero support', async () => {
+	const id = 'z7p0lb61wvsdl3x7';
+	test('A resource page should have a link tag to unapi server', async ({ page }) => {
+		await page.goto(`/${id}`);
+		const link = page.locator('link[rel="unapi-server"]');
+		await expect(link).toHaveAttribute('type', 'application/xml');
+		await expect(link).toHaveAttribute('href', '/api/sv/cite');
+	});
+
+	test('A resource page should have a <abbr> tag containing the resource id', async ({ page }) => {
+		await page.goto(`/${id}`);
+		const abbr = page.locator('abbr.unapi-id');
+		await expect(abbr).toHaveCount(1);
+		await expect(abbr).toHaveAttribute('title', new RegExp(`.+/${id}$`));
+	});
+});
