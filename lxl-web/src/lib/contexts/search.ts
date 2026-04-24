57a3382ed689b3e44eae0c2cedfd5e9349de93a2
Johan Bisse Mattsson <johan@johanbissemattsson.se>
2026-04-24 14:36:02 +0200

    feat(lxl-web): Prevent lost value and selection when loading supersearch (#1564)
    * Use searchContext to control SuperSearch from outside
    
    * Move fallback search input to separate component
    
    * Pass along value and selection when mounting SuperSearchWrapper
    
    * Use same focus styling on collapsed and expanded state
    
    * Fix test
    
    * Another try at fixing tests
    
    * Yet another test fix...
    
    * Another try gaaah
    
    * Fix teeeests
    
    * Revert changes to test file
    
    * Fix reactive context

--- lxl-web/src/lib/contexts/search.ts
+++ lxl-web/src/lib/contexts/search.ts
@@ -1,6 +1,14 @@
 import { createContext } from 'svelte';
+import type { ShowExpandedSearchOptions } from 'supersearch';
 import type { ChangeQueryParams } from '$lib/components/supersearch/SuperSearchWrapper.svelte';
 
 export const [getSearchContext, setSearchContext] = createContext<{
-	changeQuery?: (params: ChangeQueryParams) => void;
+	showExpandedSearch: (options: ShowExpandedSearchOptions) => void;
+	hideExpandedSearch: () => void;
+	changeQuery: (params: ChangeQueryParams) => void;
+	initialStateBeforeMount?: {
+		value: string;
+		selection?: { anchor: number | null | undefined; head: number | null | undefined };
+	};
+	isMounted: boolean;
 }>();
