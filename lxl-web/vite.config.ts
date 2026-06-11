2a02b36655678f4e968e673c8138169c1470124d
Johan Bisse Mattsson <johan@johanbissemattsson.se>
2026-06-11 23:14:25 +0200

    Install @sveltejs/enhanced-img

--- lxl-web/vite.config.ts
+++ lxl-web/vite.config.ts
@@ -1,11 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vitest/config';
 import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
+		enhancedImages(),
 		sveltekit(),
 		Icons({
 			compiler: 'svelte',
