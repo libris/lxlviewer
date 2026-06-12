d2b7458409957d900735774b86a1d336b2a62dd1
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2026-06-12 16:59:13 +0200

    feat(lxl-web/decorated-data): Use ul for bullet lists

---
+++ lxl-web/src/lib/components/Wrapper.svelte
@@ -0,0 +1,11 @@
+<script>
+	let { condition, withElement, children } = $props();
+</script>
+
+{#if condition}
+	<svelte:element this={withElement}>
+		{@render children()}
+	</svelte:element>
+{:else}
+	{@render children()}
+{/if}
