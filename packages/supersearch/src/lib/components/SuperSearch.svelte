b88206dbf2592c48b9e6299aa9c893bde1aba060
Johan Bisse Mattsson <johan@johanbissemattsson.se>
2026-08-06 17:00:19 +0200

    Simplify code

--- packages/supersearch/src/lib/components/SuperSearch.svelte
+++ packages/supersearch/src/lib/components/SuperSearch.svelte
@@ -967,16 +967,13 @@
 
 	$effect(() => {
 		if (defaultResultRow) {
-			moveToActiveRowIfDefaultHasMoved();
+			moveToActiveRowAfterDefaultHasMoved();
 		}
 	});
 
-	async function moveToActiveRowIfDefaultHasMoved() {
+	async function moveToActiveRowAfterDefaultHasMoved() {
 		await tick();
-		if (defaultResultRow > activeRowIndex) {
-			activeRowIndex = defaultResultRow;
-			activeColIndex = defaultResultCol;
-		}
+		setDefaultRowAndCols();
 	}
 </script>
 
