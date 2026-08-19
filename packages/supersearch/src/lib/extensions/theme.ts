a6637d46c2c1c912aef720b04acac0ceba9e5052
Jesper Engström <jesper.engstrom@kb.se>
2026-08-19 17:10:41 +0200

    style selection

--- packages/supersearch/src/lib/extensions/theme.ts
+++ packages/supersearch/src/lib/extensions/theme.ts
@@ -7,5 +7,11 @@ export const theme = EditorView.theme({
 	'&.cm-focused .cm-cursor': {
 		borderLeft: '1.2px solid #000',
 		marginLeft: '0px'
+	},
+	'&.cm-editor .cm-selectionLayer': {
+		zIndex: '1!important'
+	},
+	'&.cm-focused .cm-selectionBackground': {
+		background: '#3b82f640!important'
 	}
 });
