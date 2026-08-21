e54ab8078f55338dcd6c8e1e82207ada7934c54d
Jesper Engström <jesper.engstrom@kb.se>
2026-08-21 15:26:52 +0200

    fix(lxlweb): temporarily disable svelte/no-navigation-without-resolve lint rule

--- lxl-web/eslint.config.js
+++ lxl-web/eslint.config.js
@@ -30,7 +30,9 @@ export default ts.config(
 			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
 			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
 			'no-undef': 'off',
-			'svelte/no-useless-mustaches': 'warn'
+			'svelte/no-useless-mustaches': 'warn',
+			// TODO: Fix all files. temp disabled to not be flooded with errors.
+			'svelte/no-navigation-without-resolve': 'warn'
 		}
 	},
 	{
