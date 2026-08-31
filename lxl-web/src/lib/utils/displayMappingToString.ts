1eb26f9028f22bff80d2632b1735878e45c4fc16
Jesper Engström <jesper.engstrom@kb.se>
2026-08-31 14:22:39 +0200

    Adjust displayMappingToString + test cases

--- lxl-web/src/lib/utils/displayMappingToString.ts
+++ lxl-web/src/lib/utils/displayMappingToString.ts
@@ -2,50 +2,57 @@ import { MAPPING_IGNORE_VARIABLE } from '$lib/constants/mapping';
 import type { DisplayMapping } from '$lib/types/search';
 
 export function displayMappingToString(mapping: DisplayMapping[]): string {
-	if (mapping) {
-		const result: string[] = [];
-		mapping.forEach((m) => _iterate(m));
+	if (!mapping) return '';
 
-		function _iterate(mapping: DisplayMapping) {
-			if (MAPPING_IGNORE_VARIABLE.some((v) => v === mapping.variable)) return;
-			const { children, operator, variable, displayStr, label, _key, _value, isRedundantKeyLabel } =
-				mapping;
-			if (displayStr || label) {
-				const isFreeText = !_key && !_value;
-				if ((isFreeText || isRedundantKeyLabel) && displayStr) {
-					result.push(displayStr);
-				} else {
-					result.push(
-						`${label || _key || ''}${operator !== 'none' ? ':' : ''} ${displayStr || _value}`
-					);
-				}
-			} else if (children) {
-				if (children.length > 1 && !variable) {
-					result.push('(');
-				}
-				result.push(formatBooleanOperator(operator));
-				children.forEach((m) => {
-					_iterate(m);
-				});
-				if (children.length > 1 && !variable) {
-					result.push(')');
-				}
+	function _iterate(mapping: DisplayMapping): string {
+		if (MAPPING_IGNORE_VARIABLE.some((v) => v === mapping.variable)) return '';
+
+		const { children, operator, variable, displayStr, label, _key, _value, isRedundantKeyLabel } =
+			mapping;
+
+		if (displayStr || label) {
+			const isFreeText = !_key && !_value;
+
+			let _op = '';
+			switch (operator) {
+				case 'none':
+					break;
+				case 'like':
+					_op = '~';
+					break;
+				default:
+					_op = ':';
+					break;
 			}
-		}
-		return result
-			.filter((m) => !!m)
-			.join(' ')
-			.trim();
-	}
-	return '';
-}
 
-function formatBooleanOperator(operator: string) {
-	switch (operator) {
-		case 'and':
-		case 'none':
-			return '';
-		default:
-			return ` ${operator.toUpperCase()} `;
+			if ((isFreeText || isRedundantKeyLabel) && displayStr) {
+				return `${_op === '~' ? `${_op} ` : ''}${displayStr}`;
+			}
+
+			return `${label || _key || ''}${_op} ${displayStr || _value}`;
+		}
+
+		if (children) {
+			const renderedChildren = children.map(_iterate).filter(Boolean);
+
+			let result: string;
+
+			if (operator === 'not') {
+				result = `${operator.toUpperCase()} ${renderedChildren.join(' ')}`;
+			} else {
+				const separator = operator === 'or' ? ' OR ' : ' ';
+				result = renderedChildren.join(separator);
+			}
+
+			if (children.length > 1 && !variable) {
+				result = `(${result.trim()})`;
+			}
+
+			return result;
+		}
+
+		return '';
 	}
+
+	return mapping.map(_iterate).filter(Boolean).join(' ').trim();
 }
