1d77bddf02f6fc159fe6101b7de07e22be257bd0
Jesper Engström <jesper.engstrom@kb.se>
2026-09-03 21:48:08 +0200

    parent prop, render <p>

--- lxl-web/src/lib/components/Resource.svelte
+++ lxl-web/src/lib/components/Resource.svelte
@@ -221,6 +221,7 @@
 								<DecoratedData2
 									data={decoratedData.headingTop}
 									showLabels={ShowLabelsOptions.Never}
+									parent="p"
 								/>
 							</p>
 							<h1 class="decorated-heading mt-2 mb-1 text-3xl @3xl:text-3xl">
@@ -229,6 +230,7 @@
 									showLabels={ShowLabelsOptions.Never}
 									allowLinks={false}
 									allowPopovers={false}
+									parent="h"
 								/>
 							</h1>
 							<p
@@ -237,6 +239,7 @@
 								<DecoratedData2
 									data={decoratedData.headingExtra}
 									showLabels={ShowLabelsOptions.DefaultOn}
+									parent="p"
 								/>
 							</p>
 							{#if decoratedData['_workTitle2']?._display}
@@ -246,6 +249,7 @@
 									<DecoratedData2
 										data={decoratedData._workTitle2}
 										showLabels={ShowLabelsOptions.DefaultOff}
+										parent="p"
 									/>
 								</p>
 							{/if}
@@ -254,7 +258,7 @@
 				</div>
 				<div class="decorated-data-section decorated-compact">
 					{#each decoratedData.overview as overview (overview)}
-						<div class="compact mb-2">
+						<div class="mb-2">
 							<DecoratedData2
 								data={overview}
 								showLabels={ShowLabelsOptions.DefaultOff}
@@ -281,7 +285,7 @@
 						<div class="border-b-neutral mb-2 border-b"></div>
 					{/if}
 					{#each decoratedData.overview2 as overview2 (overview2)}
-						<div class="compact mb-2">
+						<div class="mb-2">
 							<DecoratedData2
 								data={overview2}
 								showLabels={ShowLabelsOptions.DefaultOn}
