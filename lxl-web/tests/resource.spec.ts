d14fc88a96df4bc95d3e13049992ccfeda1c3841
Jesper Engström <jesper.engstrom@kb.se>
2026-09-25 16:48:17 +0200

    Add tests

--- lxl-web/tests/resource.spec.ts
+++ lxl-web/tests/resource.spec.ts
@@ -18,6 +18,11 @@ test('first heading on page should be h1', async ({ page }) => {
 	await expect(firstHeading).toHaveJSProperty('tagName', 'H1');
 });
 
+test('page displays the correct title', async ({ page }) => {
+	await page.goto('/h08ndxddfg5v2pjf');
+	await expect(page).toHaveTitle('Pappaklausulen. — Jonas Hassen Khemiri, 1978- | Libris');
+});
+
 test('Open holdings panel should not have any detectable a11y issues', async ({ page }) => {
 	await page.goto('/h08ndxddfg5v2pjf?holdings=Electronic');
 	const accessibilityScanResults = await new AxeBuilder({ page })
